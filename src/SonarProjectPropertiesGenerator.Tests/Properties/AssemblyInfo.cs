14f029059f31c04c9f7e91d38eff65c774c9ecf2
Dinesh Bolkensteyn <dinesh@dinsoft.net>
2015-02-13 10:58:28 +0100

    Initial project structure

---
+++ src/SonarProjectPropertiesGenerator.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SonarProjectPropertiesGenerator.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SonarSource")]
+[assembly: AssemblyProduct("SonarProjectPropertiesGenerator.Tests")]
+[assembly: AssemblyCopyright("Copyright © SonarSource 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("21fb22df-4de3-429d-bd9d-1f36edb53a91")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("999.999.999.0")]
+[assembly: AssemblyFileVersion("999.999.999.0")]
