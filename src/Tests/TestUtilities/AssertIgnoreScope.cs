a713f74b172de47a8cf48eed0d627a86ea4e3303
jdcp <jdcp@live.co.uk>
2015-03-12 04:38:33 +0000

    Fixed FxCop issues

--- src/Tests/TestUtilities/AssertIgnoreScope.cs
+++ src/Tests/TestUtilities/AssertIgnoreScope.cs
@@ -49,7 +49,7 @@ namespace TestUtilities
         }
 
         // This code added to correctly implement the disposable pattern.
-        void IDisposable.Dispose()
+        public void Dispose()
         {
             // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
             Dispose(true);
