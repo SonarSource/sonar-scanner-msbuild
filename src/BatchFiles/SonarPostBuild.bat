a2d4a744651b3636e1845c47ac2c9c9fb12c520e
Dinesh Bolkensteyn <dinesh@dinsoft.net>
2015-03-19 15:01:21 +0100

    Support spaces in the output folder

--- src/BatchFiles/SonarPostBuild.bat
+++ src/BatchFiles/SonarPostBuild.bat
@@ -6,14 +6,14 @@
 
 @REM Set the output folder to the command line parameter, if supplied
 @set OutputFolder=%4
-@if "%OutputFolder%"=="" set OutputFolder=%TF_BUILD_BUILDDIRECTORY%\SonarTemp\Output\
+@if "%OutputFolder%"=="" set OutputFolder=%TF_BUILD_BUILDDIRECTORY%\SonarTemp\Output
 @echo Sonar output folder = %OutputFolder%
 
 @echo Performing Sonar post-processing...
 @%~dp0\Sonar.TeamBuild.PostProcessor.exe
 
 @echo Generating Sonar properties file to %OutputFolder% ...
-@%~dp0\SonarProjectPropertiesGenerator.exe %ProjectKey% %ProjectName% %ProjectVersion% %OutputFolder%
+@%~dp0\SonarProjectPropertiesGenerator.exe %ProjectKey% %ProjectName% %ProjectVersion% "%OutputFolder%"
 @echo ...done.
 
 @echo Calling SonarRunner...
