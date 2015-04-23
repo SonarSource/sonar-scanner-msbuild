128b0e0ee67b62040cc922dcfdfc8030b34d508d
Dinesh Bolkensteyn <dinesh@dinsoft.net>
2015-04-23 16:47:50 +0200

    Set version as 0.9.0.0 and fix some branding issues in the AssemblyInfo.cs files

---
+++ src/AssemblyInfo.Shared.cs
@@ -0,0 +1,20 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="AssemblyInfo.Shared.cs" company="SonarSource SA and Microsoft Corporation">
+//   Copyright (c) SonarSource SA and Microsoft Corporation.  All rights reserved.
+//   Licensed under the MIT License. See License.txt in the project root for license information.
+// </copyright>
+//-----------------------------------------------------------------------
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyVersion("0.9.0.0")]
+[assembly: AssemblyFileVersion("0.9.0.0")]
+
+[assembly: AssemblyConfiguration("")]
+
+[assembly: AssemblyCompany("SonarSource and Microsoft")]
+[assembly: AssemblyCopyright("Copyright © SonarSource and Microsoft 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
--- src/Tests/SonarRunner.Shim.Tests/Properties/AssemblyInfo.cs
+++ src/Tests/SonarRunner.Shim.Tests/Properties/AssemblyInfo.cs
@@ -1,36 +1,14 @@
-﻿using System.Reflection;
+﻿//-----------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="SonarSource SA and Microsoft Corporation">
+//   Copyright (c) SonarSource SA and Microsoft Corporation.  All rights reserved.
+//   Licensed under the MIT License. See License.txt in the project root for license information.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("SonarProjectPropertiesGenerator.Tests")]
+[assembly: AssemblyTitle("SonarRunner.Shim.Tests")]
+[assembly: AssemblyProduct("SonarRunner.Shim.Tests")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("SonarSource")]
-[assembly: AssemblyProduct("SonarProjectPropertiesGenerator.Tests")]
-[assembly: AssemblyCopyright("Copyright © SonarSource 2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("21fb22df-4de3-429d-bd9d-1f36edb53a91")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("999.999.999.0")]
-[assembly: AssemblyFileVersion("999.999.999.0")]
