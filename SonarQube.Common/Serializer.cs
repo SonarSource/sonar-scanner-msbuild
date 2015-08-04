53a4ea9fa407d0cb759be96f1385810338201d31
Mike <github@barry.io>
2015-08-04 11:53:01 -0400

    [SONARMSBRU-120] Add the FileShare.Read to the .Load command to allow multiple readers.

--- SonarQube.Common/Serializer.cs
+++ SonarQube.Common/Serializer.cs
@@ -66,7 +66,7 @@ namespace SonarQube.Common
             XmlSerializer ser = new XmlSerializer(typeof(T));
 
             object o = null;
-            using (FileStream fs = File.Open(fileName, FileMode.Open, FileAccess.Read))
+            using (FileStream fs = File.Open(fileName, FileMode.Open, FileAccess.Read, FileShare.Read))
             {
                 o = ser.Deserialize(fs);
             }
