b17970c2acebe64a3c25ef1fb2261df29db30056
duncanp <duncanp@microsoft.com>
2015-08-12 18:09:27 +0100

    [Technical] Re-instating single end-to-end pre-processor exe test

---
+++ Tests/SonarQube.TeamBuild.PreProcessor.Tests/PreProcessorExeTests.cs
@@ -0,0 +1,62 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="PreProcessorExeTests.cs" company="SonarSource SA and Microsoft Corporation">
+//   Copyright (c) SonarSource SA and Microsoft Corporation.  All rights reserved.
+//   Licensed under the MIT License. See License.txt in the project root for license information.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Diagnostics;
+
+namespace SonarQube.TeamBuild.PreProcessor.Tests
+{
+    [TestClass]
+    public class PreProcessorExeTests
+    {
+        public TestContext TestContext { get; set; }
+
+        #region Tests
+
+        [TestMethod]
+        public void PreProc_ErrorCodeReturnedForInvalidCommandLine()
+        {
+            CheckExecutionFails("invalid command line");
+        }
+
+        #endregion
+
+        #region Checks
+
+        private static void CheckExecutionFails(params string[] args)
+        {
+            Process p = Execute(args);
+
+            Assert.AreNotEqual(0, p.ExitCode, "Expecting a non-zero exit code");
+            string errorOutput = p.StandardError.ReadToEnd();
+            Console.WriteLine("Error output: {0}", errorOutput);
+
+            Assert.IsFalse(string.IsNullOrWhiteSpace(errorOutput), "Expecting error output if the process fails");
+        }
+
+        private static Process Execute(string[] args)
+        {
+            ProcessStartInfo psi = new ProcessStartInfo()
+            {
+                FileName = typeof(SonarQube.TeamBuild.PreProcessor.Program).Assembly.Location,
+                RedirectStandardError = true,
+                UseShellExecute = false, // required if we want to capture the error output
+                ErrorDialog = false,
+                CreateNoWindow = true,
+                Arguments = string.Join(" ", args)
+            };
+
+            Process p = Process.Start(psi);
+            p.WaitForExit(1000);
+            Assert.IsTrue(p.HasExited, "Timed out waiting for the process to exit");
+            return p;
+        }
+
+        #endregion
+    }
+}
