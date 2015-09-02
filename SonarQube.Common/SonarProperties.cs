8feab3a8a9f30d269b7e4c06812d227479552d39
Bogdan Gavril <bgavril@microsoft.com>
2015-09-02 08:20:30 +0100

    SONARMSBRU-121: Change default regex that recognises test projects so that only 'test' in the filename is detected.

--- SonarQube.Common/SonarProperties.cs
+++ SonarQube.Common/SonarProperties.cs
@@ -14,25 +14,40 @@ namespace SonarQube.Common
     {
         // SonarQube server settings
         public const string HostUrl = "sonar.host.url";
+
         public const string SonarUserName = "sonar.login";
         public const string SonarPassword = "sonar.password";
 
         // Database settings
         public const string DbConnectionString = "sonar.jdbc.url";
+
         public const string DbUserName = "sonar.jdbc.username";
         public const string DbPassword = "sonar.jdbc.password";
 
         // SonarQube project settings
         public const string ProjectKey = "sonar.projectKey";
+
         public const string ProjectName = "sonar.projectName";
         public const string ProjectVersion = "sonar.projectVersion";
 
         // Miscellaneous
         public const string SourceEncoding = "sonar.sourceEncoding";
+
         public const string ProjectBaseDir = "sonar.projectBaseDir";
         public const string WorkingDirectory = "sonar.working.directory";
         public const string Verbose = "sonar.verbose";
         public const string LogLevel = "sonar.log.level";
 
+        // Default property values
+
+        /// <summary>
+        /// Regex that determines if a project is a test project or not based on its path. 
+        /// This regular expression matches paths where the filename contains the 'test' token. 
+        /// Regex breakdown: 
+        /// [^\\]*  - everything except \
+        /// test    - that contains 'test'
+        /// [^\\]*$ - and it doesn't end in \
+        /// </summary>
+        public const string DefaultTestProjectPattern = @"[^\\]*test[^\\]*$";
     }
 }
