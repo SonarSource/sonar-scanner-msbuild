a2f3a812d73b5fd0218bd1b4af0254b4102e53c0
duncanp <duncanp@microsoft.com>
2015-10-19 13:59:22 +0100

    Update version number to 1.0.2

--- AssemblyInfo.Shared.cs
+++ AssemblyInfo.Shared.cs
@@ -7,8 +7,8 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
 
 [assembly: AssemblyConfiguration("")]
 
