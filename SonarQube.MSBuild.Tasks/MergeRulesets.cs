2c69768d117460d6cd00a4d9f3b5d079abaef583
duncanp <duncanp@microsoft.com>
2015-11-26 19:18:55 +0000

    [SONARMSBRU-183] Fixed merge ruleset to resolve relative rulesets using the project directory

--- SonarQube.MSBuild.Tasks/MergeRulesets.cs
+++ SonarQube.MSBuild.Tasks/MergeRulesets.cs
@@ -8,6 +8,7 @@
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
 using System;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Xml.Linq;
@@ -34,6 +35,13 @@ namespace SonarQube.MSBuild.Tasks
         public string PrimaryRulesetFilePath { get; set; }
 
         /// <summary>
+        /// Full path to the directory containing the project file
+        /// </summary>
+        /// <remarks>Required to resolve relative ruleset paths</remarks>
+        [Required]
+        public string ProjectDirectoryPath { get; set; }
+
+        /// <summary>
         /// The full file paths of any rulesets to include. Can be empty, in which case the task is a no-op.
         /// </summary>
         public string[] IncludedRulesetFilePaths { get; set; }
@@ -44,6 +52,9 @@ namespace SonarQube.MSBuild.Tasks
 
         public override bool Execute()
         {
+            Debug.Assert(this.PrimaryRulesetFilePath != null, "[Required] property PrimaryRulesetFilePath should not be null when the task is called from MSBuild");
+            Debug.Assert(this.ProjectDirectoryPath != null, "[Required] property ProjectDirectoryPath should not be null when the task is called from MSBuild");
+
             this.Log.LogMessage(MessageImportance.Low, Resources.MergeRulesets_MergingRulesets);
 
             if (!File.Exists(this.PrimaryRulesetFilePath))
@@ -59,7 +70,11 @@ namespace SonarQube.MSBuild.Tasks
             XDocument ruleset = XDocument.Load(PrimaryRulesetFilePath);
             foreach (string includePath in this.IncludedRulesetFilePaths)
             {
-                EnsureIncludeExists(ruleset, includePath);
+                string resolvedIncludePath = this.TryResolveIncludedRuleset(includePath);
+                if (resolvedIncludePath != null)
+                {
+                    EnsureIncludeExists(ruleset, resolvedIncludePath);
+                }
             }
             this.Log.LogMessage(MessageImportance.Low, Resources.MergeRuleset_SavingUpdatedRuleset, this.PrimaryRulesetFilePath);
             ruleset.Save(this.PrimaryRulesetFilePath);
@@ -70,6 +85,32 @@ namespace SonarQube.MSBuild.Tasks
         #endregion Overrides
 
         #region Private methods
+
+        private string TryResolveIncludedRuleset(string includePath)
+        {
+            string resolvedPath;
+
+            if (Path.IsPathRooted(includePath))
+            {
+                resolvedPath = includePath; // assume rooted paths are correctly set
+            }
+            else
+            {
+                this.Log.LogMessage(MessageImportance.Low, Resources.MergeRulesets_ResolvingRuleset, includePath);
+                resolvedPath = Path.GetFullPath(Path.Combine(this.ProjectDirectoryPath, includePath));
+                if (File.Exists(resolvedPath))
+                {
+                    this.Log.LogMessage(MessageImportance.Low, Resources.MergeRulesets_ResolvedRuleset, resolvedPath);
+                }
+                else
+                {
+                    this.Log.LogMessage(MessageImportance.Low, Resources.MergeRulesets_FailedToResolveRuleset, includePath);
+                    resolvedPath = null;
+                }
+            }
+
+            return resolvedPath;
+        }
 
         private void EnsureIncludeExists(XDocument ruleset, string includePath)
         {
