a8e0ee1fc97315335b01cc16017424c6373f2d61
Bogdan Gavril <bgavril@microsoft.com>
2015-12-17 17:15:08 +0000

    Add integration test scripts and project under test

---
+++ IntegrationTestProject/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.IO;
+using System.Net.Http;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Upload("http://localhost.:9000/api/profiles/restore", @"c:\temp\TestQualityProfile.xml");
+            string s = "14.0";
+            decimal ss = System.Convert.ToDecimal(s);
+
+        }
+
+        private static System.IO.Stream Upload(string url, string filename)
+        {
+            // Convert each of the three inputs into HttpContent objects
+
+            var content = File.ReadAllText(filename);
+            StringContent stringContent = new StringContent(content);
+
+
+            using (var client = new HttpClient())
+            using (var formData = new MultipartFormDataContent())
+            {
+                // Add the HttpContent objects to the form data
+                client.DefaultRequestHeaders.Add("Authorization", "Basic YWRtaW46YWRtaW4 =");
+
+                // <input type="text" name="filename" />
+                formData.Add(stringContent, "backup", "TestQualityProfile.xml");
+
+
+                // Actually invoke the request to the server
+
+                // equivalent to (action="{url}" method="post")
+                var response = client.PostAsync(url, formData).Result;
+
+                // equivalent of pressing the submit button on the form
+                if (!response.IsSuccessStatusCode)
+                {
+                    return null;
+                }
+                return response.Content.ReadAsStreamAsync().Result;
+            }
+        }
+    }
+}
