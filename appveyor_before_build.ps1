c75468b6c0ace57fd308741a651a4e3115a746c1
Bogdan Gavril <bgavrilMS@users.noreply.github.com>
2015-12-22 11:08:32 +0000

    Update appveyor_before_build.ps1

--- appveyor_before_build.ps1
+++ appveyor_before_build.ps1
@@ -1,5 +1,3 @@
-. .\appveyor_helpers.ps1
-
 #
 # Copies the C# plugin so that the SonarQube MSBuild Scanner packaging projects will patch it. The packaging project is invoked by Appveyor as part of the regular build.
 #
@@ -18,8 +16,9 @@ if (!$env:APPVEYOR)
     $LOCAL_DEBUG_RUN = 1
 }
 
-Add-AppveyorMessage -Message "Building the latest working C# plugin"
+echo "Building the latest working C# plugin"
 DownloadAndMavenBuildFromGitHub "SonarSource/sonar-csharp" "master"
 
 CopyCsharpPluginForPatching
+
 
