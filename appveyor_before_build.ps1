a2821b423d11c31bbcd4390d13e40d2dfcb4985a
Bogdan Gavril <bgavrilMS@users.noreply.github.com>
2015-12-22 11:15:17 +0000

    Update appveyor_before_build.ps1

--- appveyor_before_build.ps1
+++ appveyor_before_build.ps1
@@ -1,3 +1,5 @@
+. ./appveyor_helpers.ps1
+
 #
 # Copies the C# plugin so that the SonarQube MSBuild Scanner packaging projects will patch it. The packaging project is invoked by Appveyor as part of the regular build.
 #
@@ -9,11 +11,6 @@ function CopyCsharpPluginForPatching
    
     $destinationFile = [System.IO.Path]::Combine($env:APPVEYOR_BUILD_FOLDER, "PackagingProjects\CSharpPluginPayload", "csharp_plugin.jar");
     [System.IO.File]::Copy($csPluginCleanJar, $destinationFile)
-}
-
-if (!$env:APPVEYOR)
-{    
-    $LOCAL_DEBUG_RUN = 1
 }
 
 echo "Building the latest working C# plugin"
