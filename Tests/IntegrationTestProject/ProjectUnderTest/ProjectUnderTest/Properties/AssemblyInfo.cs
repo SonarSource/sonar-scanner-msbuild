d011233f547c67203f4759fac9f97cece772fe0f
Bogdan Gavril <bgavril@microsoft.com>
2015-12-22 11:29:54 +0000

    project under test

--- Tests/IntegrationTestProject/ProjectUnderTest/ProjectUnderTest/Properties/AssemblyInfo.cs
+++ Tests/IntegrationTestProject/ProjectUnderTest/ProjectUnderTest/Properties/AssemblyInfo.cs
@@ -1,5 +1,12 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿//-----------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="SonarSource SA and Microsoft Corporation">
+//   Copyright (c) SonarSource SA and Microsoft Corporation.  All rights reserved.
+//   Licensed under the MIT License. See License.txt in the project root for license information.
+// </copyright>
+//-----------------------------------------------------------------------
+
+
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
