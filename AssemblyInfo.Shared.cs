f3533b12e72e4c609963cea583ffb63bbd926916
duncanp <duncanp@microsoft.com>
2016-02-11 13:43:48 +0000

    [SONARMSBRU-217] Updated version number to 2.0

--- AssemblyInfo.Shared.cs
+++ AssemblyInfo.Shared.cs
@@ -7,8 +7,8 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
 
 [assembly: AssemblyConfiguration("")]
 
