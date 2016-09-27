924bccf32e00eab09180a1d70e16ba35b8d26adc
Duarte Meneses <duarte.meneses@sonarsource.com>
2016-09-27 17:48:51 +0200

    Echo cix script

--- cix.sh
+++ cix.sh
@@ -1,4 +1,6 @@
+#!/bin/sh
+
+set -x
+
 cd its
-
-
 mvn verify -Prun-its -Dsonar.runtimeVersion=$SQ_VERSION -DscannerForMSBuild.version=$SCANNER_VERSION -DscannerForMSBuildPayload.version=$SCANNER_PAYLOAD_VERSION
