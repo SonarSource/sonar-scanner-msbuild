125a16a6a60b3ea8d5c4c992fc44d000017e9be9
Duarte Meneses <duarte.meneses@sonarsource.com>
2016-09-28 10:04:39 +0200

    Output cmds

--- cix.sh
+++ cix.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 set -euo pipefail
-
+set -x
 cd its
 mvn verify -Dsonar.runtimeVersion=$SQ_VERSION -DscannerForMSBuild.version=$SCANNER_VERSION -DscannerForMSBuildPayload.version=$SCANNER_PAYLOAD_VERSION
