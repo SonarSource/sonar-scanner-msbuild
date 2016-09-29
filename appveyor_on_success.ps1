181501fb9de796aae376f6e8ad96af2d560ba6fe
Tom <thomas.verin@sonarsource.com>
2016-09-29 14:25:16 +0200

    QA: clearer logs

--- appveyor_on_success.ps1
+++ appveyor_on_success.ps1
@@ -15,18 +15,18 @@ if ($env:APPVEYOR_REPO_BRANCH -eq "master" -and -not $env:APPVEYOR_PULL_REQUEST_
     #DeployOnRepox $implZipPath "impl" $FinalVersion
     $scannerZipPath = FindSingleFile ([System.IO.Path]::Combine($PSScriptRoot, "DeploymentArtifacts", "BuildAgentPayload", "Release")) "SonarQube.Scanner.MSBuild.zip"
     #DeployOnRepox $scannerZipPath "" $FinalVersion
-    echo "replace zip filenames in pom.xml"
+    write-host -f green  "replace zip filenames in pom.xml"
     (Get-Content .\pom.xml) -replace 'implZipPath', "$implZipPath" | Set-Content .\pom.xml
     (Get-Content .\pom.xml) -replace 'scannerZipPath', "$scannerZipPath" | Set-Content .\pom.xml
         
-    echo "set version $FinalVersion in pom.xml"
+    write-host -f green  "set version $FinalVersion in pom.xml"
     $command = "mvn versions:set -DgenerateBackupPoms=false -DnewVersion='$FinalVersion'"
     iex $command
-    echo "set version $FinalVersion in env VAR PROJECT_VERSION for artifactory buildinfo metadata"
+    write-host -f green  "set version $FinalVersion in env VAR PROJECT_VERSION for artifactory buildinfo metadata"
     $env:PROJECT_VERSION=$FinalVersion
-    echo "set the buildnumber to this job build number"
+    write-host -f green  "set the buildnumber to this job build number"
     $env:BUILD_ID=$env:APPVEYOR_BUILD_NUMBER
-    echo "Deploy to repox with $FinalVersion"    
+    write-host -f green  "Deploy to repox with $FinalVersion"    
     $command = 'mvn deploy -Pdeploy-sonarsource -B -e -V'
     iex $command
 }
