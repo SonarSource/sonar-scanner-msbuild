c058e227f83d4b18fa7623d5f8d5fb6b53684067
Tom <thomas.verin@sonarsource.com>
2017-01-31 16:19:57 +0100

    BUILD: run QA on PR

--- build/build.ps1
+++ build/build.ps1
@@ -7,15 +7,57 @@ function testExitCode(){
     }
 }
 
+#download MSBuild
+$url = "https://github.com/SonarSource-VisualStudio/sonar-msbuild-runner/releases/download/2.0/MSBuild.SonarQube.Runner-2.0.zip"
+$output = ".\MSBuild.SonarQube.Runner.zip"    
+Invoke-WebRequest -Uri $url -OutFile $output
+unzip -o .\MSBuild.SonarQube.Runner.zip
+testExitCode
+
+#generate build version from the build number
+$buildversion="$env:BUILD_NUMBER"
+$branchName = "$env:GITHUB_BRANCH"
+$sha1 = "$env:GIT_SHA1"
+#Append build number to the versions
+(Get-Content .\build\Version.props) -replace '<AssemblyFileVersion>\$\(MainVersion\)\.0</AssemblyFileVersion>', "<AssemblyFileVersion>`$(MainVersion).$buildversion</AssemblyFileVersion>" | Set-Content .\build\Version.props
+(Get-Content .\build\Version.props) -replace '<AssemblyInformationalVersion>Version:\$\(AssemblyFileVersion\) Branch:not-set Sha1:not-set</AssemblyInformationalVersion>', "<AssemblyInformationalVersion>Version:`$(AssemblyFileVersion) Branch:$branchName Sha1:$sha1</AssemblyInformationalVersion>" | Set-Content .\build\Version.props
+& $env:MSBUILD_PATH  .\build\ChangeVersion.proj
+testExitCode 
+
+#get version number
+[xml]$versionProps = Get-Content .\build\Version.props
+$version  = $versionProps.Project.PropertyGroup.MainVersion+".$buildversion"
+write-host -f green "version: $version"    
+
+function deploy(
+    [string] $version
+)
+{
+    #DeployOnRepox $scannerZipPath "" $version
+    $implZipPath    = Get-Item .\DeploymentArtifacts\CSharpPluginPayload\Release\SonarQube.MSBuild.Runner.Implementation.zip
+    $scannerZipPath = Get-Item .\DeploymentArtifacts\BuildAgentPayload\Release\SonarQube.Scanner.MSBuild.zip
+    
+    write-host -f green  "replace zip filenames in pom.xml"
+    (Get-Content .\pom.xml) -replace 'implZipPath', "$implZipPath" | Set-Content .\pom.xml
+    (Get-Content .\pom.xml) -replace 'scannerZipPath', "$scannerZipPath" | Set-Content .\pom.xml
+        
+    write-host -f green  "set version $version in pom.xml"
+    $command = "mvn versions:set -DgenerateBackupPoms=false -DnewVersion='$version'"
+    iex $command
+    write-host -f green  "set version $version in env VAR PROJECT_VERSION for artifactory buildinfo metadata"
+    $env:PROJECT_VERSION=$version
+    write-host -f green  "set the buildnumber to this job build number"
+    $env:BUILD_ID=$env:BUILD_NUMBER
+    write-host -f green  "Deploy to repox with $version"    
+    $command = 'mvn deploy -Pdeploy-sonarsource -B -e -V'
+    iex $command
+
+    #create empty file to trigger qa
+    new-item -path . -name qa.properties -type "file"
+}
+
 if ($env:IS_PULLREQUEST -eq "true") { 
     write-host -f green "in a pull request"
-    
-    #download MSBuild
-    $url = "https://github.com/SonarSource-VisualStudio/sonar-msbuild-runner/releases/download/2.0/MSBuild.SonarQube.Runner-2.0.zip"
-    $output = ".\MSBuild.SonarQube.Runner.zip"    
-    Invoke-WebRequest -Uri $url -OutFile $output
-    unzip -o .\MSBuild.SonarQube.Runner.zip
-    testExitCode
 
     .\MSBuild.SonarQube.Runner begin /k:sonar-scanner-msbuild /n:"SonarQube Scanner for MSBuild" /v:latest `
         /d:sonar.host.url=$env:SONAR_HOST_URL `
@@ -35,27 +77,11 @@ if ($env:IS_PULLREQUEST -eq "true") {
     .\MSBuild.SonarQube.Runner end /d:sonar.login=$env:SONAR_TOKEN
     testExitCode
 
+    deploy -version $version
+
 } else {
     if (($env:GITHUB_BRANCH -eq "master") -or ($env:GITHUB_BRANCH -eq "refs/heads/master")) {
         write-host -f green "Building master branch"
-
-        #generate build version from the build number
-        $buildversion="$env:BUILD_NUMBER"
-
-        $branchName = "$env:GITHUB_BRANCH"
-        $sha1 = "$env:GIT_SHA1"
-
-        #Append build number to the versions
-        (Get-Content .\build\Version.props) -replace '<AssemblyFileVersion>\$\(MainVersion\)\.0</AssemblyFileVersion>', "<AssemblyFileVersion>`$(MainVersion).$buildversion</AssemblyFileVersion>" | Set-Content .\build\Version.props
-        (Get-Content .\build\Version.props) -replace '<AssemblyInformationalVersion>Version:\$\(AssemblyFileVersion\) Branch:not-set Sha1:not-set</AssemblyInformationalVersion>', "<AssemblyInformationalVersion>Version:`$(AssemblyFileVersion) Branch:$branchName Sha1:$sha1</AssemblyInformationalVersion>" | Set-Content .\build\Version.props
-        & $env:MSBUILD_PATH  .\build\ChangeVersion.proj
-        testExitCode
-
-        #get version number
-        [xml]$versionProps = Get-Content .\build\Version.props
-        $version  = $versionProps.Project.PropertyGroup.MainVersion+".$buildversion"
-        write-host -f green "version: $version"
-
 
         #start analysis
         .\MSBuild.SonarQube.Runner begin /k:sonar-scanner-msbuild /n:"SonarQube Scanner for MSBuild" /v:$version `
@@ -73,33 +99,12 @@ if ($env:IS_PULLREQUEST -eq "true") {
         .\MSBuild.SonarQube.Runner end /d:sonar.login=$env:SONAR_TOKEN
         testExitCode
        
-        #DeployOnRepox $scannerZipPath "" $version
-        $implZipPath    = Get-Item .\DeploymentArtifacts\CSharpPluginPayload\Release\SonarQube.MSBuild.Runner.Implementation.zip
-        $scannerZipPath = Get-Item .\DeploymentArtifacts\BuildAgentPayload\Release\SonarQube.Scanner.MSBuild.zip
-        
-        write-host -f green  "replace zip filenames in pom.xml"
-        (Get-Content .\pom.xml) -replace 'implZipPath', "$implZipPath" | Set-Content .\pom.xml
-        (Get-Content .\pom.xml) -replace 'scannerZipPath', "$scannerZipPath" | Set-Content .\pom.xml
-            
-        write-host -f green  "set version $version in pom.xml"
-        $command = "mvn versions:set -DgenerateBackupPoms=false -DnewVersion='$version'"
-        iex $command
-        write-host -f green  "set version $version in env VAR PROJECT_VERSION for artifactory buildinfo metadata"
-        $env:PROJECT_VERSION=$version
-        write-host -f green  "set the buildnumber to this job build number"
-        $env:BUILD_ID=$env:BUILD_NUMBER
-        write-host -f green  "Deploy to repox with $version"    
-        $command = 'mvn deploy -Pdeploy-sonarsource -B -e -V'
-        iex $command
-
-        #create empty file to trigger qa
-        new-item -path . -name qa.properties -type "file"
+       deploy -version =$version
 		
     } else {
         write-host -f green "not on master"
     }
 
 }
-
 
 
