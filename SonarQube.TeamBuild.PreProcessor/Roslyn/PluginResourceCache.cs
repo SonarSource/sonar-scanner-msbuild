0ba7bdc6d0d524eebcccb7e84bec264faca41068
Janos Gyerik <janos.gyerik+windows@sonarsource.com>
2017-04-03 10:33:18 +0200

    Add comment to explain waitHandle

--- SonarQube.TeamBuild.PreProcessor/Roslyn/PluginResourceCache.cs
+++ SonarQube.TeamBuild.PreProcessor/Roslyn/PluginResourceCache.cs
@@ -25,6 +25,7 @@ namespace SonarQube.TeamBuild.PreProcessor.Roslyn
 {
     class SubdirIndex
     {
+        // global locking, to ensure synchronized access to index file by multiple processes
         private readonly EventWaitHandle waitHandle = new EventWaitHandle(true, EventResetMode.AutoReset, "90CD3CFF-A12C-4013-A44A-199B8C26818B");
 
         private readonly string basedir;
