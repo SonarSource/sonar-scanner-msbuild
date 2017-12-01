0c2841b2538aeb60dcb8a20871046920b12ee2c3
Amaury Levé <amaury.leve@sonarsource.com>
2017-12-01 18:27:26 +0100

    SONARMSBRU-331 Add support for shared projects

---
+++ its/projects/CSharpSharedProjectType/SharedProject/TestEventInvoke.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace SonarIssues.Shared
+{
+	public abstract class TestEventInvoke : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected bool Set<T>(ref T field, T value,
+			[CallerMemberName] string propertyName = null)
+		{
+			if (!EqualityComparer<T>.Default.Equals(field, value))
+			{
+				field = value;
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+				return true;
+			}
+			return false;
+            // TODO:
+		}
+	}
+}
