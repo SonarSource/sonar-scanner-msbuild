b4ebd0dc9935751ce57f0f166dc49fd57b2ae7f7
Michal Barczyk <michal.barczyk@sonarsource.com>
2018-02-08 10:01:18 +0100

    multitarget to standard/core

--- src/SonarQube.TeamBuild.Integration/Resources.Designer.cs
+++ src/SonarQube.TeamBuild.Integration/Resources.Designer.cs
@@ -10,8 +10,8 @@
 
 namespace SonarQube.TeamBuild.Integration {
     using System;
-    
-    
+
+
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -23,15 +23,15 @@ namespace SonarQube.TeamBuild.Integration {
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
-        
+
         private static global::System.Resources.ResourceManager resourceMan;
-        
+
         private static global::System.Globalization.CultureInfo resourceCulture;
-        
+
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
         internal Resources() {
         }
-        
+
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
@@ -45,7 +45,7 @@ namespace SonarQube.TeamBuild.Integration {
                 return resourceMan;
             }
         }
-        
+
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
@@ -59,7 +59,7 @@ namespace SonarQube.TeamBuild.Integration {
                 resourceCulture = value;
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to Code coverage command line tool: {0}.
         /// </summary>
@@ -68,7 +68,7 @@ namespace SonarQube.TeamBuild.Integration {
                 return ResourceManager.GetString("CONV_DIAG_CommandLineToolInfo", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to Attempting to locate the CodeCoverage.exe tool....
         /// </summary>
@@ -77,7 +77,7 @@ namespace SonarQube.TeamBuild.Integration {
                 return ResourceManager.GetString("CONV_DIAG_LocatingCodeCoverageTool", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to Attempting to locate the CodeCoverage.exe tool using registry....
         /// </summary>
@@ -86,7 +86,7 @@ namespace SonarQube.TeamBuild.Integration {
                 return ResourceManager.GetString("CONV_DIAG_LocatingCodeCoverageToolRegistry", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to Attempting to locate the CodeCoverage.exe tool using setup configuration....
         /// </summary>
@@ -95,7 +95,7 @@ namespace SonarQube.TeamBuild.Integration {
                 return ResourceManager.GetString("CONV_DIAG_LocatingCodeCoverageToolSetupConfiguration", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to Multiple versions of VS are installed: {0}.
         /// </summary>
@@ -104,7 +104,7 @@ namespace SonarQube.TeamBuild.Integration {
                 return ResourceManager.GetString("CONV_DIAG_MultipleVsVersionsInstalled", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to Visual Studio setup configuration was not found..
         /// </summary>
@@ -113,7 +113,7 @@ namespace SonarQube.TeamBuild.Integration {
                 return ResourceManager.GetString("CONV_DIAG_SetupConfigurationNotSupported", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to Failed to convert the downloaded code coverage tool to XML. No code coverage information will be uploaded to SonarQube.
         ///Check that the downloaded code coverage file ({0}) is valid by opening it in Visual Studio. If it is not, check that the internet security settings on the build machine allow files to be downloaded from the Team Foundation Server machine..
@@ -123,7 +123,7 @@ namespace SonarQube.TeamBuild.Integration {
                 return ResourceManager.GetString("CONV_ERROR_ConversionToolFailed", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to Failed to convert the binary coverage file to XML. The expected output file was not found: {0}.
         /// </summary>
@@ -132,7 +132,7 @@ namespace SonarQube.TeamBuild.Integration {
                 return ResourceManager.GetString("CONV_ERROR_OutputFileNotFound", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to Failed to find the code coverage command line tool. Possible cause: Visual Studio is not installed, or the installed version does not support code coverage..
         /// </summary>
@@ -141,16 +141,16 @@ namespace SonarQube.TeamBuild.Integration {
                 return ResourceManager.GetString("CONV_WARN_FailToFindConversionTool", resourceCulture);
             }
         }
-        
+
         /// <summary>
-        ///   Looks up a localized string similar to The Coverage Report Processor was not initialized before use..
+        ///   Looks up a localized string similar to The Coverage Report Processor was not initialised before use..
         /// </summary>
         internal static string EX_CoverageReportProcessorNotInitialised {
             get {
                 return ResourceManager.GetString("EX_CoverageReportProcessorNotInitialised", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to Fetching code coverage report information from TFS....
         /// </summary>
@@ -159,7 +159,7 @@ namespace SonarQube.TeamBuild.Integration {
                 return ResourceManager.GetString("PROC_DIAG_FetchingCoverageReportInfoFromServer", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to Updating project info files with code coverage information....
         /// </summary>
@@ -168,7 +168,7 @@ namespace SonarQube.TeamBuild.Integration {
                 return ResourceManager.GetString("PROC_DIAG_UpdatingProjectInfoFiles", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to Absolute path to coverage file: {0}.
         /// </summary>
@@ -177,7 +177,7 @@ namespace SonarQube.TeamBuild.Integration {
                 return ResourceManager.GetString("TRX_DIAG_AbsoluteTrxPath", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to Looking for TRX files in: {0}.
         /// </summary>
@@ -186,7 +186,7 @@ namespace SonarQube.TeamBuild.Integration {
                 return ResourceManager.GetString("TRX_DIAG_FolderPaths", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to Attempting to locate a test results (.trx) file....
         /// </summary>
@@ -195,7 +195,7 @@ namespace SonarQube.TeamBuild.Integration {
                 return ResourceManager.GetString("TRX_DIAG_LocatingTrx", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to No code coverage attachments were found in the trx file.
         /// </summary>
@@ -204,7 +204,7 @@ namespace SonarQube.TeamBuild.Integration {
                 return ResourceManager.GetString("TRX_DIAG_NoCodeCoverageInfo", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to No test results files found.
         /// </summary>
@@ -213,7 +213,7 @@ namespace SonarQube.TeamBuild.Integration {
                 return ResourceManager.GetString("TRX_DIAG_NoTestResultsFound", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to One code coverage attachment was found in the trx file: {0}.
         /// </summary>
@@ -222,7 +222,7 @@ namespace SonarQube.TeamBuild.Integration {
                 return ResourceManager.GetString("TRX_DIAG_SingleCodeCoverageAttachmentFound", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to Located a test results file: {0}.
         /// </summary>
@@ -231,7 +231,7 @@ namespace SonarQube.TeamBuild.Integration {
                 return ResourceManager.GetString("TRX_DIAG_SingleTrxFileFound", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to Test results directory does not exist in {0}.
         /// </summary>
@@ -240,7 +240,7 @@ namespace SonarQube.TeamBuild.Integration {
                 return ResourceManager.GetString("TRX_DIAG_TestResultsDirectoryNotFound", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to Coverage attachment could not be found. Trx file: {0}. Attachment name: {1}.
         /// </summary>
@@ -249,7 +249,7 @@ namespace SonarQube.TeamBuild.Integration {
                 return ResourceManager.GetString("TRX_WARN_InvalidConstructedCoveragePath", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to Located trx file is not a valid xml file. File: {0}. File load error: {1}.
         /// </summary>
@@ -258,7 +258,7 @@ namespace SonarQube.TeamBuild.Integration {
                 return ResourceManager.GetString("TRX_WARN_InvalidTrx", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to Expecting to find at most one code coverage attachment in the trx file, but multiple attachments were found. Code coverage information will not be uploaded to SonarQube. Attachments: {0}.
         /// </summary>
@@ -267,7 +267,7 @@ namespace SonarQube.TeamBuild.Integration {
                 return ResourceManager.GetString("TRX_WARN_MultipleCodeCoverageAttachmentsFound", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to More than one test result file was found: expecting to find only one. Results files: {0}.
         /// </summary>
