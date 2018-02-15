ab15535a6e569682f681fdb11cbe023ddf4f5a24
michalb-sonar <michal.barczyk@sonarsource.com>
2018-02-15 09:22:25 +0100

    Use mutex instead of EventWaitHandle (#381)

---
+++ src/SonarQube.Common/MutexWrapper.cs
@@ -0,0 +1,57 @@
+﻿/*
+ * SonarQube Scanner for MSBuild
+ * Copyright (C) 2016-2018 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using System.Threading;
+
+namespace SonarQube.Common
+{
+    public sealed class MutexWrapper : IDisposable
+    {
+        private Mutex mutex;
+
+        public MutexWrapper(string name)
+            : this(name, TimeSpan.FromSeconds(5))
+        {
+        }
+
+        public MutexWrapper(string name, TimeSpan acquireTimeout)
+        {
+            mutex = new Mutex(false, name);
+
+            try
+            {
+                mutex.WaitOne(acquireTimeout);
+            }
+            catch (AbandonedMutexException)
+            {
+                // This exception means the mutex was abandoned,
+                // but was now acquired successfully.
+            }
+        }
+
+        public void Dispose()
+        {
+            mutex?.ReleaseMutex();
+            mutex?.Dispose();
+            mutex = null;
+        }
+    }
+}
