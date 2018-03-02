56786db624d7e388dc63f3acf61d278cfe8a8fb9
duncanp-sonar <duncan.pocklington@sonarsource.com>
2018-03-02 16:04:34 +0000

    Added warning that XAML builds are deprecated (#412)
    Added warning that XAML builds are deprecated

--- src/SonarQube.TeamBuild.PostProcessor/Resources.Designer.cs
+++ src/SonarQube.TeamBuild.PostProcessor/Resources.Designer.cs
@@ -294,5 +294,14 @@ namespace SonarQube.TeamBuild.PostProcessor {
                 return ResourceManager.GetString("WARN_CannotProcessCoverage", resourceCulture);
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to ** WARNING: Support for XAML builds is deprecated since version 4.1 and will be removed in version 5.0 of the Scanner for MSBuild **.
+        /// </summary>
+        internal static string WARN_XamlBuildDeprecated {
+            get {
+                return ResourceManager.GetString("WARN_XamlBuildDeprecated", resourceCulture);
+            }
+        }
     }
 }
