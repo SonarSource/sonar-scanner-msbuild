c8411430db2953057d020f0bfe35c9278c331665
michalb-sonar <michal.barczyk@sonarsource.com>
2018-03-14 16:33:02 +0100

    Embed Scanner Cli 3.1.0 (#441)

--- src/SonarScanner.MSBuild.Shim/SonarScanner.Wrapper.cs
+++ src/SonarScanner.MSBuild.Shim/SonarScanner.Wrapper.cs
@@ -54,7 +54,8 @@ namespace SonarScanner.MSBuild.Shim
 
         private const string CmdLineArgPrefix = "-D";
 
-        private const string SonarScannerVersion = "3.0.3.778";
+        // This version needs to be in sync with version in src/Packaging/Directory.Build.props.
+        private const string SonarScannerVersion = "3.1.0.1141";
 
         #region ISonarScanner interface
 
