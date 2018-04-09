ff16235e580a6e3b5fb674e650d3edd3e5d11741
Amaury Levé <amaury.leve@sonarsource.com>
2018-04-09 16:03:31 +0200

    Update Version.props and build script

--- build/build.ps1
+++ build/build.ps1
@@ -26,8 +26,12 @@ $buildversion="$env:BUILD_NUMBER"
 $branchName = "$env:GITHUB_BRANCH"
 $sha1 = "$env:GIT_SHA1"
 #Append build number to the versions
-(Get-Content .\build\Version.props) -replace '<AssemblyFileVersion>\$\(MainVersion\)\.0</AssemblyFileVersion>', "<AssemblyFileVersion>`$(MainVersion).$buildversion</AssemblyFileVersion>" | Set-Content .\build\Version.props
-(Get-Content .\build\Version.props) -replace '<AssemblyInformationalVersion>Version:\$\(AssemblyFileVersion\) Branch:not-set Sha1:not-set</AssemblyInformationalVersion>', "<AssemblyInformationalVersion>Version:`$(AssemblyFileVersion) Branch:$branchName Sha1:$sha1</AssemblyInformationalVersion>" | Set-Content .\build\Version.props
+$versionProperties = "build\Version.props"
+(Get-Content $versionProperties) `
+        -Replace '<Sha1>.*</Sha1>', "<Sha1>$sha1</Sha1>" `
+        -Replace '<BuildNumber>\d+</BuildNumber>', "<BuildNumber>$buildversion</BuildNumber>" `
+        -Replace '<BranchName>.*</BranchName>', "<BranchName>$branchName</BranchName>" `
+    | Set-Content $versionProperties
 & $env:MSBUILD_PATH  .\build\ChangeVersion.proj
 testExitCode 
 
