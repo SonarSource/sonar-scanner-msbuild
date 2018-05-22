9509dbd9c07ee8fbfcb165064c80be62eee02a56
Amaury Levé <amaury.leve@sonarsource.com>
2018-05-22 15:30:04 +0200

    Update license files

--- Tests/SonarScanner.MSBuild.Tests/AssemblyFriends.cs
+++ Tests/SonarScanner.MSBuild.Tests/AssemblyFriends.cs
@@ -1,7 +1,7 @@
 ﻿//-----------------------------------------------------------------------
 // <copyright file="AssemblyFriends.cs" company="SonarSource SA and Microsoft Corporation">
 //   Copyright (c) SonarSource SA and Microsoft Corporation.  All rights reserved.
-//   Licensed under the MIT License. See License.txt in the project root for license information.
+//   Licensed under the MIT License. See License in the project root for license information.
 // </copyright>
 //-----------------------------------------------------------------------
 
