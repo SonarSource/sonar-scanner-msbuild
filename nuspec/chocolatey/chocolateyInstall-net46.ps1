7f742dc0bad5f80202f83e3427f3432f923b719b
Amaury Levé <amaury.leve@sonarsource.com>
2018-05-22 13:56:09 +0200

    Provide chocolatey packages

---
+++ nuspec/chocolatey/chocolateyInstall-net46.ps1
@@ -0,0 +1,7 @@
+﻿$version = "4.2.0.1214"
+
+Install-ChocolateyZipPackage "sonarscanner-msbuild-net46" `
+    -Url "https://github.com/SonarSource/sonar-scanner-msbuild/releases/download/${version}/sonar-scanner-msbuild-${version}-net46.zip" `
+    -UnzipLocation "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" `
+    -ChecksumType 'sha1' `
+    -Checksum "39fd602faa1887a04b1187e8e2db994f82587e9b"
