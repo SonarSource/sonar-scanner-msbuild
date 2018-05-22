74617764ea56d1d4a0e839a841542ba5ae478b63
Amaury Levé <amaury.leve@sonarsource.com>
2018-05-22 17:31:50 +0200

    Provide chocolatey packages for the two artifacts we produce (#505)

---
+++ nuspec/chocolatey/chocolateyInstall-netcoreapp2.0.ps1
@@ -0,0 +1,7 @@
+﻿$version = "4.2.0.1214"
+
+Install-ChocolateyZipPackage "sonarscanner-msbuild-netcoreapp2.0" `
+    -Url "https://github.com/SonarSource/sonar-scanner-msbuild/releases/download/${version}/sonar-scanner-msbuild-${version}-netcoreapp2.0.zip" `
+    -UnzipLocation "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" `
+    -ChecksumType 'sha1' `
+    -Checksum "f0cdd999f298100485c3e25fe9380cf2f244cfb4"
