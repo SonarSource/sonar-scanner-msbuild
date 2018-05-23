cbbf52bb97d96d8e918a689043b8edfbfc86d5eb
Amaury Levé <amaury.leve@sonarsource.com>
2018-05-23 12:20:14 +0200

    Update the embedded version of the scanner cli

--- src/SonarScanner.MSBuild.Shim/SonarScanner.Wrapper.cs
+++ src/SonarScanner.MSBuild.Shim/SonarScanner.Wrapper.cs
@@ -55,7 +55,7 @@ namespace SonarScanner.MSBuild.Shim
         private const string CmdLineArgPrefix = "-D";
 
         // This version needs to be in sync with version in src/Packaging/Directory.Build.props.
-        private const string SonarScannerVersion = "3.1.0.1141";
+        private const string SonarScannerVersion = "3.2.0.1227";
 
         private readonly ILogger logger;
 
