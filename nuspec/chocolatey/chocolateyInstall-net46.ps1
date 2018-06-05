4939dd29c3080a70e552bcc513093b72a87eae79
Amaury Levé <amaury.leve@sonarsource.com>
2018-06-05 10:19:09 +0200

    Upate the data inside the chocolatey powershell scripts

--- nuspec/chocolatey/chocolateyInstall-net46.ps1
+++ nuspec/chocolatey/chocolateyInstall-net46.ps1
@@ -1,7 +1,7 @@
-﻿$version = "4.2.0.1214"
+﻿$version = "4.3.0.1333"
 
 Install-ChocolateyZipPackage "sonarscanner-msbuild-net46" `
     -Url "https://github.com/SonarSource/sonar-scanner-msbuild/releases/download/${version}/sonar-scanner-msbuild-${version}-net46.zip" `
     -UnzipLocation "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" `
     -ChecksumType 'sha1' `
-    -Checksum "39fd602faa1887a04b1187e8e2db994f82587e9b"
+    -Checksum "63ccb278629d3c787dae20b4c69016ecb346d39b"
--- nuspec/chocolatey/chocolateyInstall-netcoreapp2.0.ps1
+++ nuspec/chocolatey/chocolateyInstall-netcoreapp2.0.ps1
@@ -1,7 +1,7 @@
-﻿$version = "4.2.0.1214"
+﻿$version = "4.3.0.1333"
 
 Install-ChocolateyZipPackage "sonarscanner-msbuild-netcoreapp2.0" `
     -Url "https://github.com/SonarSource/sonar-scanner-msbuild/releases/download/${version}/sonar-scanner-msbuild-${version}-netcoreapp2.0.zip" `
     -UnzipLocation "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" `
     -ChecksumType 'sha1' `
-    -Checksum "f0cdd999f298100485c3e25fe9380cf2f244cfb4"
+    -Checksum "4d54469c716b56fba9c8a0ccc561293e4cdb54ee"
