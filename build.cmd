180a5de517cefee47893d4234b1579400aa029ff
Amaury Levé <amaury.leve@sonarsource.com>
2018-06-05 13:44:22 +0200

    Fix relative path

--- build.cmd
+++ build.cmd
@@ -1,3 +1,3 @@
-@PowerShell -NonInteractive -NoProfile -ExecutionPolicy Unrestricted -Command "& {.\scripts\build\build.ps1; exit $LastExitCode }"
+@PowerShell -NonInteractive -NoProfile -ExecutionPolicy Unrestricted -Command "& {.\scripts\build\ci-build.ps1; exit $LastExitCode }"
 @echo From Cmd.exe: build.ps1 exited with exit code %errorlevel%
 exit %errorlevel%
