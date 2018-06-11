bbaa1443e1c1854f025a75a3c6119516130c7252
Amaury Levé <amaury.leve@sonarsource.com>
2018-06-11 17:26:16 +0200

    Update chocolatey packages to use SHA256

--- nuspec/chocolatey/chocolateyInstall-net46.ps1
+++ nuspec/chocolatey/chocolateyInstall-net46.ps1
@@ -1,7 +1,5 @@
-﻿$sha1 = "63ccb278629d3c787dae20b4c69016ecb346d39b"
-
-Install-ChocolateyZipPackage "sonarscanner-msbuild-net46" `
+﻿Install-ChocolateyZipPackage "sonarscanner-msbuild-net46" `
     -Url "https://github.com/SonarSource/sonar-scanner-msbuild/releases/download/$env:ChocolateyPackageVersion/sonar-scanner-msbuild-$env:ChocolateyPackageVersion-net46.zip" `
     -UnzipLocation "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" `
-    -ChecksumType 'sha1' `
-    -Checksum $sha1
+    -ChecksumType 'sha256' `
+    -Checksum "81541CB41357EDAF0D375017B3AD3E67C228B365C9EA85B29A2879A2B5C84890"
--- nuspec/chocolatey/chocolateyInstall-netcoreapp2.0.ps1
+++ nuspec/chocolatey/chocolateyInstall-netcoreapp2.0.ps1
@@ -1,7 +1,5 @@
-﻿$sha1 = "4d54469c716b56fba9c8a0ccc561293e4cdb54ee"
-
-Install-ChocolateyZipPackage "sonarscanner-msbuild-netcoreapp2.0" `
+﻿Install-ChocolateyZipPackage "sonarscanner-msbuild-netcoreapp2.0" `
     -Url "https://github.com/SonarSource/sonar-scanner-msbuild/releases/download/$env:ChocolateyPackageVersion/sonar-scanner-msbuild-$env:ChocolateyPackageVersion-netcoreapp2.0.zip" `
     -UnzipLocation "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" `
-    -ChecksumType 'sha1' `
-    -Checksum $sha1
+    -ChecksumType 'sha256' `
+    -Checksum "35571D22D3A1A24E63A3227933CFADAB22929E4C5EA717B027C98E1AA1A7ED30"
