af51235352b1c2255b782a88ec0af242b57db27d
Amaury Levé <amaury.leve@sonarsource.com>
2018-06-14 15:05:30 +0200

    Update chocolatey metadata linked to lastest release

--- nuspec/chocolatey/chocolateyInstall-net46.ps1
+++ nuspec/chocolatey/chocolateyInstall-net46.ps1
@@ -2,4 +2,4 @@
     -Url "https://github.com/SonarSource/sonar-scanner-msbuild/releases/download/$env:ChocolateyPackageVersion/sonar-scanner-msbuild-$env:ChocolateyPackageVersion-net46.zip" `
     -UnzipLocation "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" `
     -ChecksumType 'sha256' `
-    -Checksum "81541CB41357EDAF0D375017B3AD3E67C228B365C9EA85B29A2879A2B5C84890"
+    -Checksum "6AE9ECF7A4B963167AB1DDC5ADB289A1883261827244F4F59102C9EEA5B47A3C"
--- nuspec/chocolatey/chocolateyInstall-netcoreapp2.0.ps1
+++ nuspec/chocolatey/chocolateyInstall-netcoreapp2.0.ps1
@@ -2,4 +2,4 @@
     -Url "https://github.com/SonarSource/sonar-scanner-msbuild/releases/download/$env:ChocolateyPackageVersion/sonar-scanner-msbuild-$env:ChocolateyPackageVersion-netcoreapp2.0.zip" `
     -UnzipLocation "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" `
     -ChecksumType 'sha256' `
-    -Checksum "35571D22D3A1A24E63A3227933CFADAB22929E4C5EA717B027C98E1AA1A7ED30"
+    -Checksum "246E289E151FAF6D96972994F5D1A9F66B0CA650EFAF8FE9789657C888DA4884"
