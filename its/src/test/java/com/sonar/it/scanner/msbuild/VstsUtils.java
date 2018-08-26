8bbad784e69977194b897604995f17348c3698a0
duncanp <duncan.pocklington@sonarsource.com>
2018-08-26 22:05:31 +0100

    Added file missing header

--- its/src/test/java/com/sonar/it/scanner/msbuild/VstsUtils.java
+++ its/src/test/java/com/sonar/it/scanner/msbuild/VstsUtils.java
@@ -1,9 +1,28 @@
+/*
+ * Scanner for MSBuild :: Integration Tests
+ * Copyright (C) 2016-2018 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 package com.sonar.it.scanner.msbuild;
 
 public class VstsUtils {
 
   static Boolean isRunningUnderVsts(){
-    return System.getenv("BUILD_SOURCESDIRECTORY") != null;
+    return System.getenv("AGENT_BUILDDIRECTORY") != null;
   }
 
   static String getSourcesDirectory(){
