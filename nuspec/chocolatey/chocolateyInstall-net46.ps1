9c96cc33d45bd114b29b312374762137e3ce2a70
Amaury Levé <amaury.leve@sonarsource.com>
2018-10-03 13:16:44 +0200

    Bump versions

--- nuspec/chocolatey/chocolateyInstall-net46.ps1
+++ nuspec/chocolatey/chocolateyInstall-net46.ps1
@@ -2,4 +2,4 @@
     -Url "https://github.com/SonarSource/sonar-scanner-msbuild/releases/download/$env:ChocolateyPackageVersion/sonar-scanner-msbuild-$env:ChocolateyPackageVersion-net46.zip" `
     -UnzipLocation "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" `
     -ChecksumType 'sha256' `
-    -Checksum "6AE9ECF7A4B963167AB1DDC5ADB289A1883261827244F4F59102C9EEA5B47A3C"
+    -Checksum "73F1340BA90554C16E03C326FCE8ABAEF5C1E52F29E3CB1C5365E284FC3ED5AE"
--- nuspec/chocolatey/chocolateyInstall-netcoreapp2.0.ps1
+++ nuspec/chocolatey/chocolateyInstall-netcoreapp2.0.ps1
@@ -2,4 +2,4 @@
     -Url "https://github.com/SonarSource/sonar-scanner-msbuild/releases/download/$env:ChocolateyPackageVersion/sonar-scanner-msbuild-$env:ChocolateyPackageVersion-netcoreapp2.0.zip" `
     -UnzipLocation "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" `
     -ChecksumType 'sha256' `
-    -Checksum "246E289E151FAF6D96972994F5D1A9F66B0CA650EFAF8FE9789657C888DA4884"
+    -Checksum "831290FDA537D37F756051266C0B85D6064B068ECE6F53B445CC9DB3BAF2E0D5"
