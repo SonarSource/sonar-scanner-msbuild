16b5392423a03527025234276e0e3725570b2a59
Amaury Levé <amaury.leve@sonarsource.com>
2018-11-26 18:32:08 +0100

    Improve build scripts based on sonar-dotnet

--- scripts/version/set-version.ps1
+++ scripts/version/set-version.ps1
@@ -15,19 +15,30 @@ Param(
 Set-StrictMode -version 2.0
 $ErrorActionPreference = "Stop"
 
+function Set-VersionForDotNet() {
+    Write-Header "Updating version in .Net files"
+
+    Invoke-InLocation ".\scripts\version" {
+        $versionPropsFile = Resolve-Path "Version.props"
+        $xml = [xml](Get-Content $versionPropsFile)
+        $xml.Project.PropertyGroup.MainVersion = $version
+        $xml.Save($versionPropsFile)
+        msbuild "ChangeVersion.proj"
+        Test-ExitCode "ERROR: Change version FAILED."
+    }
+}
+
 try {
-    Push-Location "${PSScriptRoot}"
+    . (Join-Path $PSScriptRoot "..\utils.ps1")
+
+    Push-Location "${PSScriptRoot}\..\.."
 
     $fixedVersion = $version
     if ($fixedVersion.EndsWith(".0")) {
         $fixedVersion = $version.Substring(0, $version.Length - 2)
     }
-
-    $versionPropsFile = Resolve-Path "Version.props"
-    $xml = [xml](Get-Content $versionPropsFile)
-    $xml.Project.PropertyGroup.MainVersion = $version
-    $xml.Save($versionPropsFile)
-    msbuild "ChangeVersion.proj"
+    
+    Set-VersionForDotNet
 
     exit 0
 }
