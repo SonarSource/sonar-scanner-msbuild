e567edf87168d8f4628e1fe3121611cd0e61abba
Amaury Levé <amaury.leve@sonarsource.com>
2018-11-29 13:17:25 +0100

    Fix build.cmd

--- build.cmd
+++ build.cmd
@@ -1,3 +1,10 @@
-@PowerShell -NonInteractive -NoProfile -ExecutionPolicy Unrestricted -Command "& {.\scripts\build\ci-build.ps1; exit $LastExitCode }"
-@echo From Cmd.exe: build.ps1 exited with exit code %errorlevel%
-exit %errorlevel%
+@echo off
+setlocal ENABLEDELAYEDEXPANSION
+
+if "%DEBUG%" == "true" (
+    PowerShell -NonInteractive -NoProfile -ExecutionPolicy Unrestricted -Command "scripts\build\ci-build.ps1" -Verbose
+) else (
+    PowerShell -NonInteractive -NoProfile -ExecutionPolicy Unrestricted -Command "scripts\build\ci-build.ps1"
+)
+echo From Cmd.exe: sonar-scanner-msbuild ci-build.ps1 exited with exit code !errorlevel!
+exit !errorlevel!
