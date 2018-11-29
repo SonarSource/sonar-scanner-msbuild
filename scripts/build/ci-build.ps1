cafef41a34a87f1cb0aa3992a12bb4ee7d4aaff4
Amaury Levé <amaury.leve@sonarsource.com>
2018-11-29 13:21:32 +0100

    Revert "Analyze code on SonarCloud"
    This reverts commit 7722a13690332e855c1875c93af5fa0d0f306c00.

--- scripts/build/ci-build.ps1
+++ scripts/build/ci-build.ps1
@@ -19,8 +19,8 @@ param (
     [string]$githubPRTargetBranch = $env:GITHUB_TARGET_BRANCH,
 
     # SonarQube related parameters
-    [string]$sonarCloudUrl = $env:SONARCLOUD_HOST_URL,
-    [string]$sonarCloudToken = $env:SONARCLOUD_TOKEN,
+    [string]$sonarQubeUrl = $env:SONAR_HOST_URL,
+    [string]$sonarQubeToken = $env:SONAR_TOKEN,
 
     # Build related parameters
     [string]$buildNumber = $env:BUILD_NUMBER,
@@ -115,7 +115,7 @@ function Get-ScannerMsBuildPath() {
 }
 
 function Invoke-SonarBeginAnalysis([array][parameter(ValueFromRemainingArguments = $true)]$remainingArgs) {
-    Write-Header "Running SonarCloud Analysis begin step"
+    Write-Header "Running SonarQube Analysis begin step"
 
     if (Test-Debug) {
         $remainingArgs += "/d:sonar.verbose=true"
@@ -124,21 +124,20 @@ function Invoke-SonarBeginAnalysis([array][parameter(ValueFromRemainingArguments
     Exec { & (Get-ScannerMsBuildPath) begin `
         /k:sonar-scanner-msbuild `
         /n:"SonarScanner for MSBuild" `
-        /d:sonar.host.url=$sonarCloudUrl `
-        /d:sonar.login=$sonarCloudToken `
-        /o:sonarsource `
+        /d:sonar.host.url=$sonarQubeUrl `
+        /d:sonar.login=$sonarQubeToken `
         /d:sonar.cs.vstest.reportsPaths="**\*.trx" `
         /d:sonar.cs.vscoveragexml.reportsPaths="**\*.coveragexml" `
         $remainingArgs `
-    } -errorMessage "ERROR: SonarCloud Analysis begin step FAILED."
+    } -errorMessage "ERROR: SonarQube Analysis begin step FAILED."
 }
 
 function Invoke-SonarEndAnalysis() {
-    Write-Header "Running SonarCloud Analysis end step"
+    Write-Header "Running SonarQube Analysis end step"
 
     Exec { & (Get-ScannerMsBuildPath) end `
-        /d:sonar.login=$sonarCloudToken `
-    } -errorMessage "ERROR: SonarCloud Analysis end step FAILED."
+        /d:sonar.login=$sonarQubeToken `
+    } -errorMessage "ERROR: SonarQube Analysis end step FAILED."
 }
 
 function Publish-Artifacts() {
