73370269c3e3c2cd7f28f77685e1bbf8fa8401d7
Amaury Levé <amaury.leve@sonarsource.com>
2018-12-06 14:18:45 +0100

    Automatically generate chocolatey packages (#635)

--- nuspec/chocolatey/chocolateyInstall-netcoreapp2.0.ps1
+++ nuspec/chocolatey/chocolateyInstall-netcoreapp2.0.ps1
@@ -2,4 +2,4 @@
     -Url "https://github.com/SonarSource/sonar-scanner-msbuild/releases/download/$env:ChocolateyPackageVersion/sonar-scanner-msbuild-$env:ChocolateyPackageVersion-netcoreapp2.0.zip" `
     -UnzipLocation "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" `
     -ChecksumType 'sha256' `
-    -Checksum "BA8D058A88C311A0F01DBAC94B4B3041EB27713085FC2F0776FB863277127534"
+    -Checksum "not-set"
