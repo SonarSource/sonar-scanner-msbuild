082a7f7efefdb511b9137abe160f4b02dd9567d9
Mickaël <mickael.caro@sonarsource.com>
2019-08-27 16:54:56 +0200

    Add case-insensitive comparison for paths in ProjectInfo.xml files, for Windows OS only., updated / added U.T.

---
+++ src/SonarScanner.MSBuild.Shim/RuntimeInformationWrapper.cs
@@ -0,0 +1,20 @@
+﻿#if NET46
+using System.Runtime.InteropServices;
+#endif
+
+namespace SonarScanner.MSBuild.Shim
+{
+    /* Class for ease mocking in UT */
+    public class RuntimeInformationWrapper : IRuntimeInformationWrapper
+    {
+        public bool IsOS(System.Runtime.InteropServices.OSPlatform osPlatform)
+        {
+            return System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(osPlatform);
+        }
+    }
+
+    public interface IRuntimeInformationWrapper
+    {
+        bool IsOS(System.Runtime.InteropServices.OSPlatform osPlatform);
+    }
+}
