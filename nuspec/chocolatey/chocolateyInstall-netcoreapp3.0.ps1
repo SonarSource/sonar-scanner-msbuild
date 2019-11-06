38911b0c030fc9452bb1bf978c35b2b880cd386c
mickael-caro-sonarsource <mickael.caro@sonarsource.com>
2019-11-06 11:57:22 +0100

    Added .NET Core 3 package for Chocolatey + fixed incorrect package name in artifacts (#809)

---
+++ nuspec/chocolatey/chocolateyInstall-netcoreapp3.0.ps1
@@ -0,0 +1,5 @@
+﻿Install-ChocolateyZipPackage "sonarscanner-msbuild-netcoreapp3.0" `
+    -Url "https://github.com/SonarSource/sonar-scanner-msbuild/releases/download/$env:ChocolateyPackageVersion/sonar-scanner-msbuild-$env:ChocolateyPackageVersion-netcoreapp3.0.zip" `
+    -UnzipLocation "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" `
+    -ChecksumType 'sha256' `
+    -Checksum "not-set"
