109da21c2aea4295e844278dc889e72cf13a8c3f
Pascal Berger <bep@bbtsoftware.ch>
2019-12-13 20:18:54 +0100

    Use fixed version number in Chocolatey package

--- nuspec/chocolatey/chocolateyInstall-net46.ps1
+++ nuspec/chocolatey/chocolateyInstall-net46.ps1
@@ -1,5 +1,5 @@
 ﻿Install-ChocolateyZipPackage "sonarscanner-msbuild-net46" `
-    -Url "https://github.com/SonarSource/sonar-scanner-msbuild/releases/download/$env:ChocolateyPackageVersion/sonar-scanner-msbuild-$env:ChocolateyPackageVersion-net46.zip" `
+    -Url "https://github.com/SonarSource/sonar-scanner-msbuild/releases/download/__PackageVersion__/sonar-scanner-msbuild-__PackageVersion__-net46.zip" `
     -UnzipLocation "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" `
     -ChecksumType 'sha256' `
     -Checksum "not-set"
