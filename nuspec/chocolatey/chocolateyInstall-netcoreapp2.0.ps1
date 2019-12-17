3f4082e5fdbbee07259fdcd82be9e2898b5f68f2
mickael-caro-sonarsource <mickael.caro@sonarsource.com>
2019-12-17 15:51:50 +0100

    Use fixed version number in Chocolatey package (#818)

--- nuspec/chocolatey/chocolateyInstall-netcoreapp2.0.ps1
+++ nuspec/chocolatey/chocolateyInstall-netcoreapp2.0.ps1
@@ -1,5 +1,5 @@
 ﻿Install-ChocolateyZipPackage "sonarscanner-msbuild-netcoreapp2.0" `
-    -Url "https://github.com/SonarSource/sonar-scanner-msbuild/releases/download/$env:ChocolateyPackageVersion/sonar-scanner-msbuild-$env:ChocolateyPackageVersion-netcoreapp2.0.zip" `
+    -Url "https://github.com/SonarSource/sonar-scanner-msbuild/releases/download/__PackageVersion__/sonar-scanner-msbuild-__PackageVersion__-netcoreapp2.0.zip" `
     -UnzipLocation "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" `
     -ChecksumType 'sha256' `
     -Checksum "not-set"
