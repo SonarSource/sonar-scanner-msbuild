2b65bde300c2484b6abd815969d2d4b6222a00cd
Fabio Stawinski <fabio.stawinski@volvo.com>
2021-06-09 19:04:44 +0200

    Added default null for the param,cleaned the tests

--- src/SonarScanner.MSBuild.PreProcessor/PreprocessorObjectFactory.cs
+++ src/SonarScanner.MSBuild.PreProcessor/PreprocessorObjectFactory.cs
@@ -62,7 +62,7 @@ namespace SonarScanner.MSBuild.PreProcessor
             var clientCertPassword = args.GetSetting(SonarProperties.ClientCertPassword, null);
             var hostUrl = args.SonarQubeUrl;
 
-            this.server = new SonarWebService(new WebClientDownloader(username, password, clientCertPath, clientCertPassword, this.logger), hostUrl, this.logger);
+            this.server = new SonarWebService(new WebClientDownloader(username, password, this.logger, clientCertPath, clientCertPassword), hostUrl, this.logger);
             return this.server;
         }
 
