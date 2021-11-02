fbfbc00d33c178df0d25babbc60d347fa0294f50
Pavel Mikula <pavel.mikula@sonarsource.com>
2021-11-02 10:35:20 +0100

    Rename Tests to Test to align with other projects

---
+++ Tests/SonarScanner.MSBuild.Tasks.IntegrationTest/TargetsTests/SonarIntegrationTargetsTests.cs
@@ -0,0 +1,132 @@
+﻿/*
+ * SonarScanner for .NET
+ * Copyright (C) 2016-2021 SonarSource SA
+ * mailto: info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SonarScanner.Integration.Tasks.IntegrationTests.TargetsTests;
+using TestUtilities;
+
+namespace SonarScanner.MSBuild.Tasks.IntegrationTest.TargetsTests
+{
+    [TestClass]
+    public class SonarIntegrationTargetsTests
+    {
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        [Description("Checks the properties are not set if the temp folder is not set")]
+        public void IntTargets_TempFolderIsNotSet()
+        {
+            var result = CreateProjectAndLoad(null);
+            result.AssertPropertyValue(TargetProperties.SonarQubeOutputPath, null);
+            result.AssertPropertyValue(TargetProperties.SonarQubeConfigPath, null);
+        }
+
+        [TestMethod]
+        [Description("Checks the SonarQube paths are not set when the TeamBuild build directories are missing")]
+        public void IntTargets_SonarPaths_TeamBuildBuildDirNotSet()
+        {
+            var projectXml = $@"
+<PropertyGroup>
+  <TF_BUILD_BUILDDIRECTORY />
+  <AGENT_BUILDDIRECTORY />
+</PropertyGroup>";
+            var result = CreateProjectAndLoad(projectXml);
+            result.AssertPropertyValue(TargetProperties.SonarQubeOutputPath, null);
+            result.AssertPropertyValue(TargetProperties.SonarQubeConfigPath, null);
+        }
+
+        [TestMethod]
+        [Description("Checks the SonarQube paths are set correctly when the legacy TeamBuild directory is provided")]
+        public void IntTargets_SonarPaths_TeamBuildPropertySet_Legacy()
+        {
+            var projectXml = $@"
+<PropertyGroup>
+  <SonarQubeTempPath>t:\TeamBuildDir_Legacy\.sonarqube</SonarQubeTempPath>
+  <TF_BUILD_BUILDDIRECTORY>t:\TeamBuildDir_Legacy</TF_BUILD_BUILDDIRECTORY>
+  <AGENT_BUILDDIRECTORY />
+</PropertyGroup>";
+            var result = CreateProjectAndLoad(projectXml);
+            result.AssertPropertyValue(TargetProperties.SonarQubeOutputPath, @"t:\TeamBuildDir_Legacy\.sonarqube\out");
+            result.AssertPropertyValue(TargetProperties.SonarQubeConfigPath, @"t:\TeamBuildDir_Legacy\.sonarqube\conf");
+        }
+
+        [TestMethod]
+        [Description("Checks the SonarQube paths are set correctly when the new TeamBuild build directory is provided")]
+        public void IntTargets_SonarPaths_TeamBuildPropertySet_NonLegacy()
+        {
+            var projectXml = $@"
+<PropertyGroup>
+  <SonarQubeTempPath>t:\TeamBuildDir_NonLegacy\.sonarqube</SonarQubeTempPath>
+  <TF_BUILD_BUILDDIRECTORY></TF_BUILD_BUILDDIRECTORY>
+  <AGENT_BUILDDIRECTORY>t:\TeamBuildDir_NonLegacy</AGENT_BUILDDIRECTORY>
+</PropertyGroup>";
+            var result = CreateProjectAndLoad(projectXml);
+            result.AssertPropertyValue(TargetProperties.SonarQubeOutputPath, @"t:\TeamBuildDir_NonLegacy\.sonarqube\out");
+            result.AssertPropertyValue(TargetProperties.SonarQubeConfigPath, @"t:\TeamBuildDir_NonLegacy\.sonarqube\conf");
+        }
+
+        [TestMethod]
+        [Description("Checks the SonarQube paths are set correctly when the SonarQubeTempPath property is provided")]
+        public void IntTargets_SonarPaths_TempPathSet()
+        {
+            var projectXml = $@"
+<PropertyGroup>
+  <SonarQubeTempPath>c:\sonarQTemp</SonarQubeTempPath>
+
+  <!-- SonarQubeTempPath setting should take precedence -->
+  <TF_BUILD_BUILDDIRECTORY>t:\Legacy TeamBuildPath\</TF_BUILD_BUILDDIRECTORY>
+  <AGENT_BUILDDIRECTORY>x:\New Team Build Path\</AGENT_BUILDDIRECTORY>
+</PropertyGroup>";
+            var result = CreateProjectAndLoad(projectXml);
+            result.AssertPropertyValue(TargetProperties.SonarQubeOutputPath, @"c:\sonarQTemp\out");
+            result.AssertPropertyValue(TargetProperties.SonarQubeConfigPath, @"c:\sonarQTemp\conf");
+        }
+
+        [TestMethod]
+        [Description("Tests that the explicit property values for the output and config paths are used if supplied")]
+        public void IntTargets_SonarPaths_OutputAndConfigPathsAreSet()
+        {
+            // The SonarQubeTempPath and TeamBuild paths should be ignored if the output and config are set explicitly
+            var projectXml = $@"
+<PropertyGroup>
+  <SonarQubeOutputPath>c:\output</SonarQubeOutputPath>
+  <SonarQubeConfigPath>c:\config</SonarQubeConfigPath>
+  <SonarQubeTempPath>c:\sonarQTemp</SonarQubeTempPath>
+
+  <!-- SonarQubeTempPath setting should take precedence -->
+  <TF_BUILD_BUILDDIRECTORY>t:\Legacy TeamBuildPath\</TF_BUILD_BUILDDIRECTORY>
+  <AGENT_BUILDDIRECTORY>x:\New TeamBuildPath\</AGENT_BUILDDIRECTORY>
+</PropertyGroup>";
+            var result = CreateProjectAndLoad(projectXml);
+            result.AssertPropertyValue(TargetProperties.SonarQubeOutputPath, @"c:\output");
+            result.AssertPropertyValue(TargetProperties.SonarQubeConfigPath, @"c:\config");
+        }
+
+        private BuildLog CreateProjectAndLoad(string projectSnippet)
+        {
+            projectSnippet += @"<Target Name=""DoNothing"" />";
+            var projectDirectory = TestUtils.CreateTestSpecificFolderWithSubPaths(TestContext);
+            var targetTestUtils = new TargetsTestsUtils(TestContext);
+            var projectTemplate = targetTestUtils.GetProjectTemplate(null, projectDirectory, null, projectSnippet, null);
+            var projectFile = targetTestUtils.CreateProjectFile(projectDirectory, projectTemplate);
+            return BuildRunner.BuildTargets(TestContext, projectFile, "DoNothing");
+        }
+    }
+}
--- Tests/SonarScanner.MSBuild.Test/AssemblyFriends.cs
+++ Tests/SonarScanner.MSBuild.Test/AssemblyFriends.cs
@@ -1,7 +1,7 @@
 ﻿/*
- * SonarScanner for .NET
+ * SonarScanner for MSBuild
  * Copyright (C) 2016-2021 SonarSource SA
- * mailto: info AT sonarsource DOT com
+ * mailto:info AT sonarsource DOT com
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -19,9 +19,8 @@
  */
 
 using System.Runtime.CompilerServices;
-
-#if SignAssembly
-[assembly: InternalsVisibleTo("SonarScanner.MSBuild.TFS.Test,PublicKey=002400000480000094000000060200000024000052534131000400000100010081b4345a022cc0f4b42bdc795a5a7a1623c1e58dc2246645d751ad41ba98f2749dc5c4e0da3a9e09febcb2cd5b088a0f041f8ac24b20e736d8ae523061733782f9c4cd75b44f17a63714aced0b29a59cd1ce58d8e10ccdb6012c7098c39871043b7241ac4ab9f6b34f183db716082cd57c1ff648135bece256357ba735e67dc6")]
+#if CodeSigned
+[assembly: InternalsVisibleTo("SonarScanner.PreProcessor.Test, PublicKey=002400000480000094000000060200000024000052534131000400000100010029d15920332ded89851197f2ef16bfebd9cfc0acd7e0f3f5bbdc0d0ae03e7a893820e693e2ee9d886b362da373a6cd69e6041894fba4ea73b4c1ea31d1d6f2bd2b5a108f8863d0e01d52c58f29949719015b2889cc9f5057d7a802617d11f4c344dba9aae6d262b79c5220987b08ec0bfd9e39b0bb008441fa37b3f3b89814b8")]
 #else
-[assembly: InternalsVisibleTo("SonarScanner.MSBuild.TFS.Test")]
+[assembly: InternalsVisibleTo("SonarScanner.PreProcessor.Test")]
 #endif
