2ac742baea03001b6606cb6f92ef7f9205c51c4d
Čaba Šagi <csaba.sagi@sonarsource.com>
2022-01-19 12:06:36 +0100

    Fix last failing test

--- Tests/SonarScanner.MSBuild.TFS.Test/ProgramTests.cs
+++ Tests/SonarScanner.MSBuild.TFS.Test/ProgramTests.cs
@@ -58,16 +58,21 @@ namespace SonarScanner.MSBuild.TFS.Classic.Tests
         {
             var logger = new TestLogger();
             var tempDir = Directory.CreateDirectory(Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString())).FullName;
-            var text = @"<?xml version=""1.0"" encoding=""utf-8""?>
-                        <AnalysisConfig xmlns:xsd=""http://www.w3.org/2001/XMLSchema""
-                        xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns=""http://www.sonarsource.com/msbuild/integration/2015/1"">
-                        </AnalysisConfig>";
+            // Because the test is run now with VS community edition installed, the nuget package is used and the path to the .exe has to be provided.
+            var vsCoverageConverterToolPath = @$"{Environment.GetEnvironmentVariable("userprofile")}\.nuget\packages\microsoft.codecoverage\17.0.0\build\netstandard1.0\CodeCoverage\CodeCoverage.exe";
+            var text = 
+@$"<?xml version=""1.0"" encoding=""utf-8""?>
+<AnalysisConfig xmlns:xsd=""http://www.w3.org/2001/XMLSchema""
+xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns=""http://www.sonarsource.com/msbuild/integration/2015/1"">
+  <AdditionalConfig>
+    <ConfigSetting Id =""VsCoverageConverterToolPath"" Value=""{vsCoverageConverterToolPath}""/>
+  </AdditionalConfig >
+</AnalysisConfig>";
 
             File.WriteAllText(Path.Combine(tempDir, "temp.xml"), text);
             File.WriteAllText(Path.Combine(tempDir, "sonar-project.properties"), string.Empty);
 
             using var scope = new EnvironmentVariableScope();
-
             // Faking TeamBuild
             scope.SetVariable(TeamBuildSettings.EnvironmentVariables.IsInTeamFoundationBuild, "true");
             scope.SetVariable(TeamBuildSettings.EnvironmentVariables.BuildUri_TFS2015, "vsts://test/test");
