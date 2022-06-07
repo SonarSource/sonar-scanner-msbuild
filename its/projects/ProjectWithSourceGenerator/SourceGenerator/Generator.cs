de0560f9a617160903f7c45507558c00d19c2abe
Čaba Šagi <csaba.sagi@sonarsource.com>
2022-06-07 17:10:50 +0200

    IT reproducer project

---
+++ its/projects/ProjectWithSourceGenerator/SourceGenerator/Generator.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace SourceGen
+{
+    [Generator]
+    public class Generator : ISourceGenerator
+    {
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            // This has no implementation because there is nothing to do on init and
+            // the method is required to satisfy the interface.
+        }
+
+        public void Execute(GeneratorExecutionContext context) =>
+            context.AddSource($"Foo.cs", "class Foo {}");
+    }
+}
