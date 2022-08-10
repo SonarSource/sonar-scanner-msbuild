b10cd345886c53a140ac4e99b046be2ab868169e
Pavel Mikula <pavel.mikula@sonarsource.com>
2022-08-10 13:47:45 +0200

    UT cleanup

--- Tests/SonarScanner.MSBuild.Shim.Test/PathHelperTests.cs
+++ Tests/SonarScanner.MSBuild.Shim.Test/PathHelperTests.cs
@@ -29,164 +29,112 @@ namespace SonarScanner.MSBuild.Shim.Test
     public class PathHelperTests
     {
         [TestMethod]
-        public void WithTrailingSeparator_WhenNull_ThrowsArgumentNullException()
-        {
-            // Arrange & Act
-            Action act = () => PathHelper.WithTrailingDirectorySeparator(null);
-
-            // Assert
-            act.Should().Throw<ArgumentNullException>().And.ParamName.Should().Be("directory");
-        }
+        public void WithTrailingSeparator_WhenNull_ThrowsArgumentNullException() =>
+            ((Action)(() => PathHelper.WithTrailingDirectorySeparator(null))).Should().Throw<ArgumentNullException>().And.ParamName.Should().Be("directory");
 
         [TestMethod]
         public void WithTrailingSeparator_WhenEndsWithBackslash_ReturnsDirectoryFullName()
         {
-            // Arrange
-            var path = "C:\\SomeDirectory\\";
-            var directory = new DirectoryInfo(path);
-
-            // Act
+            var directory = new DirectoryInfo(@"C:\SomeDirectory\");
             var result = PathHelper.WithTrailingDirectorySeparator(directory);
 
-            // Assert
             result.Should().Be(directory.FullName);
         }
 
         [TestMethod]
         public void WithTrailingSeparator_WhenEndsWithSlash_ReturnsDirectoryFullName()
         {
-            // Arrange
-            var path = "C:/SomeDirectory/";
-            var directory = new DirectoryInfo(path);
-
-            // Act
+            var directory = new DirectoryInfo("C:/SomeDirectory/");
             var result = PathHelper.WithTrailingDirectorySeparator(directory);
 
-            // Assert
             result.Should().Be(directory.FullName);
         }
 
         [TestMethod]
         public void WithTrailingSeparator_WhenDoesNotEndWithSeparatorAndContainsDirectorySeparatorChar_ReturnsStringWithRightEnd()
         {
-            // Arrange
-            var path = "C:" + Path.DirectorySeparatorChar + "SomeDirectory" + Path.DirectorySeparatorChar + "Foo";
-            var directory = new DirectoryInfo(path);
-
-            // Act
+            var directory = new DirectoryInfo("C:" + Path.DirectorySeparatorChar + "SomeDirectory" + Path.DirectorySeparatorChar + "Foo");
             var result = PathHelper.WithTrailingDirectorySeparator(directory);
 
-            // Assert
             result.Should().Be(directory.FullName + Path.DirectorySeparatorChar);
         }
 
         [TestMethod]
         public void WithTrailingSeparator_WhenDoesNotEndWithSeparatorAndContainsAltDirectorySeparatorChar_ReturnsStringWithRightEnd()
         {
-            // Arrange
-            var path = "C:" + Path.AltDirectorySeparatorChar + "SomeDirectory" + Path.AltDirectorySeparatorChar + "Foo";
-            var directory = new DirectoryInfo(path);
-
-            // Act
+            var directory = new DirectoryInfo("C:" + Path.AltDirectorySeparatorChar + "SomeDirectory" + Path.AltDirectorySeparatorChar + "Foo");
             var result = PathHelper.WithTrailingDirectorySeparator(directory);
 
-            // Assert
             result.Should().Be(directory.FullName + Path.DirectorySeparatorChar);
         }
 
         [TestMethod]
         public void WithTrailingSeparator_WhenDoesNotEndWithSeparatorAndContainsNoSeparator_ReturnsStringWithDirectorySeparatorChar()
         {
-            // Arrange
-            var path = "SomeDirectory";
-            var directory = new DirectoryInfo(path);
-
-            // Act
+            var directory = new DirectoryInfo("SomeDirectory");
             var result = PathHelper.WithTrailingDirectorySeparator(directory);
 
-            // Assert
             result.Should().Be(directory.FullName + Path.DirectorySeparatorChar);
         }
 
         [TestMethod]
         public void IsInDirectory_ReturnsTheExpectedResults()
         {
-            PathHelper.IsInDirectory(new FileInfo("C:\\Src\\File.cs"), new DirectoryInfo("C:\\Src\\")).Should().BeTrue();
-            PathHelper.IsInDirectory(new FileInfo("C:\\Src\\File.cs"), new DirectoryInfo("C:\\Src")).Should().BeTrue();
-            PathHelper.IsInDirectory(new FileInfo("C:\\Src\\File.cs"), new DirectoryInfo("C:\\")).Should().BeTrue();
-            PathHelper.IsInDirectory(new FileInfo("C:\\SRC\\FILE.CS"), new DirectoryInfo("C:\\src")).Should().BeTrue();
-            PathHelper.IsInDirectory(new FileInfo("C:/Src/File.cs"), new DirectoryInfo("C:/Src")).Should().BeTrue();
-            PathHelper.IsInDirectory(new FileInfo("C:\\Src\\File.cs"), new DirectoryInfo("C:/Src")).Should().BeTrue();
-            PathHelper.IsInDirectory(new FileInfo("~Foo\\File.cs"), new DirectoryInfo("~Foo")).Should().BeTrue();
-            PathHelper.IsInDirectory(new FileInfo("C:\\Src\\Bar\\..\\File.cs"), new DirectoryInfo("C:\\Src")).Should().BeTrue();
-            PathHelper.IsInDirectory(new FileInfo("C:\\Src\\File.cs"), new DirectoryInfo("C:\\Src\\Bar\\..")).Should().BeTrue();
-
-            PathHelper.IsInDirectory(new FileInfo("C:\\SrcFile.cs"), new DirectoryInfo("C:\\Src")).Should().BeFalse();
-            PathHelper.IsInDirectory(new FileInfo("C:\\Src\\File.cs"), new DirectoryInfo("C:\\Src\\Bar")).Should().BeFalse();
+            PathHelper.IsInDirectory(new FileInfo(@"C:\Src\File.cs"), new DirectoryInfo(@"C:\Src\")).Should().BeTrue();
+            PathHelper.IsInDirectory(new FileInfo(@"C:\Src\File.cs"), new DirectoryInfo(@"C:\Src")).Should().BeTrue();
+            PathHelper.IsInDirectory(new FileInfo(@"C:\Src\File.cs"), new DirectoryInfo(@"C:\")).Should().BeTrue();
+            PathHelper.IsInDirectory(new FileInfo(@"C:\SRC\FILE.CS"), new DirectoryInfo(@"C:\src")).Should().BeTrue();
+            PathHelper.IsInDirectory(new FileInfo(@"C:/Src/File.cs"), new DirectoryInfo(@"C:/Src")).Should().BeTrue();
+            PathHelper.IsInDirectory(new FileInfo(@"C:\Src\File.cs"), new DirectoryInfo(@"C:/Src")).Should().BeTrue();
+            PathHelper.IsInDirectory(new FileInfo(@"~Foo\File.cs"), new DirectoryInfo("~Foo")).Should().BeTrue();
+            PathHelper.IsInDirectory(new FileInfo(@"C:\Src\Bar\..\File.cs"), new DirectoryInfo(@"C:\Src")).Should().BeTrue();
+            PathHelper.IsInDirectory(new FileInfo(@"C:\Src\File.cs"), new DirectoryInfo(@"C:\Src\Bar\..")).Should().BeTrue();
+            // False
+            PathHelper.IsInDirectory(new FileInfo(@"C:\SrcFile.cs"), new DirectoryInfo(@"C:\Src")).Should().BeFalse();
+            PathHelper.IsInDirectory(new FileInfo(@"C:\Src\File.cs"), new DirectoryInfo(@"C:\Src\Bar")).Should().BeFalse();
         }
 
         [TestMethod]
-        public void GetCommonRoot_WhenNull_ReturnsNull()
-        {
-            // Arrange
-            // Act
-            var result = PathHelper.GetCommonRoot(null);
-
-            // Assert
-            result.Should().BeNull();
-        }
+        public void BestCommonRoot_WhenNull_ReturnsNull() =>
+            PathHelper.BestCommonRoot(null).Should().BeNull();
 
         [TestMethod]
-        public void GetCommonRoot_WhenNoCommonPath_ReturnsNull()
-        {
-            PathHelper.GetCommonRoot(new[]
+        public void BestCommonRoot_WhenNoCommonPath_ReturnsNull() =>
+            PathHelper.BestCommonRoot(new[]
             {
-                new DirectoryInfo("C:\\"),
-                new DirectoryInfo("D:\\Foo"),
+                new DirectoryInfo(@"C:\"),
+                new DirectoryInfo(@"D:\Dir"),
             }).Should().BeNull();
-        }
 
         [TestMethod]
-        public void GetCommonRoot_WhenCommonPath_ReturnsTheLongestCommonPart()
-        {
-            PathHelper.GetCommonRoot(new[]
+        public void BestCommonRoot_WhenCommonPath_ReturnsTheLongestCommonPart() =>
+            PathHelper.BestCommonRoot(new[]
             {
-                new DirectoryInfo("C:\\Foo"),
-                new DirectoryInfo("C:\\Foo\\Bar"),
-                new DirectoryInfo("C:\\Foo\\FooBar"),
-            }).FullName.Should().Be("C:\\Foo");
-        }
+                new DirectoryInfo(@"C:\Common"),
+                new DirectoryInfo(@"C:\Common\SubDirA"),
+                new DirectoryInfo(@"C:\Common\SomethingElse"),
+            }).FullName.Should().Be(@"C:\Common");
 
         [TestMethod]
-        public void GetCommonRoot_WhenCommonPathOfFiles_ReturnsTheLongestCommonPart()
-        {
-            PathHelper.GetCommonRoot(new[]
+        public void BestCommonRoot_WhenCommonPathOfFiles_ReturnsTheLongestCommonPart() =>
+            PathHelper.BestCommonRoot(new[]
             {
-                new DirectoryInfo("C:\\Foo.cs"),
-                new DirectoryInfo("C:\\Foo\\Bar.cs"),
-                new DirectoryInfo("C:\\Foo\\FooBar.cs"),
-            }).FullName.Should().Be("C:\\");
-        }
+                new DirectoryInfo(@"C:\InRoot.cs"),
+                new DirectoryInfo(@"C:\SubDir\A.cs"),
+                new DirectoryInfo(@"C:\SubDir\B.cs"),
+            }).FullName.Should().Be(@"C:\");
 
         [TestMethod]
-        public void GetParts_WhenNull_ThrowsArgumentNullException()
-        {
-            // Arrange
-            // Act
-            Action act = () => PathHelper.GetParts(null);
-
-            // Assert
-            act.Should().Throw<ArgumentNullException>().And.ParamName.Should().Be("directoryInfo");
-        }
+        public void GetParts_WhenNull_ThrowsArgumentNullException() =>
+            ((Action)(() => PathHelper.GetParts(null))).Should().Throw<ArgumentNullException>().And.ParamName.Should().Be("directoryInfo");
 
         [TestMethod]
         public void GetParts_ReturnsTheExpectedValues()
         {
-            PathHelper.GetParts(new DirectoryInfo("C:\\")).Should().BeEquivalentTo("C:\\");
-            PathHelper.GetParts(new DirectoryInfo("C:\\Foo\\Bar")).Should().BeEquivalentTo("C:\\", "Foo", "Bar");
-
+            PathHelper.GetParts(new DirectoryInfo(@"C:\")).Should().BeEquivalentTo(@"C:\");
+            PathHelper.GetParts(new DirectoryInfo(@"C:\Foo\Bar")).Should().BeEquivalentTo(@"C:\", "Foo", "Bar");
             // Also work with a file path given as DirectoryInfo
-            PathHelper.GetParts(new DirectoryInfo("C:\\Foo\\Bar\\File.cs")).Should().BeEquivalentTo("C:\\", "Foo", "Bar", "File.cs");
+            PathHelper.GetParts(new DirectoryInfo(@"C:\Foo\Bar\File.cs")).Should().BeEquivalentTo(@"C:\", "Foo", "Bar", "File.cs");
         }
     }
 }
