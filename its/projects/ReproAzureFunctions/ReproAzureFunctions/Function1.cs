9ddf60cb911389f8f4f551ae0b336dbea35aefc0
Andrei Epure <andrei.epure@sonarsource.com>
2022-08-12 14:36:15 +0200

    review: simplify Azure Functions project

--- its/projects/ReproAzureFunctions/ReproAzureFunctions/Function1.cs
+++ its/projects/ReproAzureFunctions/ReproAzureFunctions/Function1.cs
@@ -16,20 +16,9 @@ namespace ReproAzureFunctions
         }
 
         [Function("Function1")]
-        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
+        public void Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
         {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
-
-            var response = req.CreateResponse(HttpStatusCode.OK);
-            if (response == null)
-            {
-                response.WriteString("boom");
-            }
-            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
-
-            response.WriteString("Welcome to Azure Functions!");
-
-            return response;
+            // FIXME: This should raise S1134
         }
     }
 }
