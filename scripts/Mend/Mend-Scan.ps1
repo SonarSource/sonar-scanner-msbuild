1c6a8f7e32b79bf997de1d0583c8d2a8f74adad9
Pavel Mikula <pavel.mikula@sonarsource.com>
2022-10-11 13:25:18 +0200

    Rename WhiteSource to Mend

--- scripts/Mend/Mend-Scan.ps1
+++ scripts/Mend/Mend-Scan.ps1
@@ -14,16 +14,16 @@ for ($num = 1 ; $num -le $NUM_RETRIES ; $num++)
 {
   try
   {
-    Write-host "Download WhiteSource tool, attempt $num/$NUM_RETRIES"
-    $whiteSourceAgentPath = "$toolsPath\wss-unified-agent.jar"
-    Invoke-WebRequest -Uri https://unified-agent.s3.amazonaws.com/wss-unified-agent.jar -OutFile $whiteSourceAgentPath
+    Write-host "Download Mend tool, attempt $num/$NUM_RETRIES"
+    $MendAgentPath = "$toolsPath\wss-unified-agent.jar"
+    Invoke-WebRequest -Uri https://unified-agent.s3.amazonaws.com/wss-unified-agent.jar -OutFile $MendAgentPath
     break
   }
   catch
   {
-    if ([System.IO.File]::Exists($whiteSourceAgentPath))
+    if ([System.IO.File]::Exists($MendAgentPath))
     {
-      Remove-Item -Path $whiteSourceAgentPath
+      Remove-Item -Path $MendAgentPath
     }
     Write-host "Download failed with error: $_"
 
@@ -35,19 +35,19 @@ for ($num = 1 ; $num -le $NUM_RETRIES ; $num++)
   }
 }
 
-Write-Host "Validating WhiteSource agent certificate signature..."
+Write-Host "Validating Mend agent certificate signature..."
 $cert = 'Signed by "CN=whitesource software inc, O=whitesource software inc, STREET=79 Madison Ave, L=New York, ST=New York, OID.2.5.4.17=10016, C=US"'
-if (-Not (& "$env:JAVA_HOME_11_X64\bin\jarsigner.exe" -verify -strict -verbose $whiteSourceAgentPath |  Select-String -Pattern $cert -CaseSensitive -Quiet)){
+if (-Not (& "$env:JAVA_HOME_11_X64\bin\jarsigner.exe" -verify -strict -verbose $MendAgentPath |  Select-String -Pattern $cert -CaseSensitive -Quiet)){
   Write-Host "wss-unified-agent.jar signature verification failed."
   exit 1
 }
 
-# WhiteSource agent needs the following environment variables:
+# Mend agent needs the following environment variables:
 # - WS_APIKEY
 # - WS_PRODUCTNAME
 # - WS_PROJECTNAME
 
 $env:WS_PROJECTNAME = "$env:WS_PRODUCTNAME $(Get-Version)"
 
-Write-Host "Running the WhiteSource unified agent for $env:WS_PROJECTNAME..."
-& "$env:JAVA_HOME_11_X64\bin\java.exe" -jar $whiteSourceAgentPath -c "$PSScriptRoot\wss-unified-agent.config"
+Write-Host "Running the Mend unified agent for $env:WS_PROJECTNAME..."
+& "$env:JAVA_HOME_11_X64\bin\java.exe" -jar $MendAgentPath -c "$PSScriptRoot\wss-unified-agent.config"
