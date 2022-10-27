56bff02c923a5f38bd8a828ee1fbcf22dfc44c9f
mary-georgiou-sonarsource <mary.georgiou@sonarsource.com>
2022-10-27 17:28:11 +0200

    Add .NET7 sample project to ITs

---
+++ its/projects/NET7Sample/NET7Sample/CSharp11Features.cs
@@ -0,0 +1,44 @@
+﻿namespace NET7Sample
+{
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+
+    namespace SampleNET7
+    {
+        class Foo
+        {
+            // Raw string literals https://learn.microsoft.com/en-us/dotnet/csharp/whats-new/csharp-11#raw-string-literals
+            readonly string rawStringLiteral1 = """test"""; // S2479, S1144
+            readonly string rawStringLiteral2 = $$""" Here are two ints: {}"""; // S1144
+
+            // Required modifier https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/required
+            public required string FooProperty { get; init; }
+
+            [SetsRequiredMembers]
+            public Foo(string fooProperty)
+            {
+                FooProperty = fooProperty;
+            }
+        }
+
+        public class FooBar
+        {
+            // Extended nameof scope: https://learn.microsoft.com/en-us/dotnet/csharp/whats-new/csharp-11#extended-nameof-scope
+            [Obsolete(nameof(argument.Trim))]
+            public void Bar(string argument)
+            {
+                // List Patterns https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/operators/patterns#list-patterns
+                int[] numbers = { 1, 2, 3 };
+                Console.WriteLine(numbers is [0 or 1, <= 2, >= 3]);
+            }
+        }
+
+        // File scoped types https://learn.microsoft.com/en-us/dotnet/csharp/whats-new/csharp-11#file-scoped-types
+        file class FileScopedType { }
+
+        // Generic attributes https://learn.microsoft.com/en-us/dotnet/csharp/whats-new/csharp-11#generic-attributes
+        public class GenericAttribute<T> : Attribute { } // S2326
+
+    }
+
+}
