7faa560f22bac4d2aac3cb994293632b5765ecb3
Pavel Mikula <57188685+pavel-mikula-sonarsource@users.noreply.github.com>
2022-11-22 17:02:01 +0100

    Incremental PR analysis: Implement the hashing (#1372)

--- Tests/SonarScanner.MSBuild.PreProcessor.Test/CacheProcessorTests.cs
+++ Tests/SonarScanner.MSBuild.PreProcessor.Test/CacheProcessorTests.cs
@@ -19,10 +19,13 @@
  */
 
 using System;
+using System.IO;
+using System.Text;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using SonarScanner.MSBuild.Common;
+using TestUtilities;
 
 namespace SonarScanner.MSBuild.PreProcessor.Test
 {
@@ -41,6 +44,52 @@ namespace SonarScanner.MSBuild.PreProcessor.Test
             ((Func<CacheProcessor>)(() => new CacheProcessor(null, settings, logger))).Should().Throw<ArgumentNullException>().Which.ParamName.Should().Be("server");
             ((Func<CacheProcessor>)(() => new CacheProcessor(server, null, logger))).Should().Throw<ArgumentNullException>().Which.ParamName.Should().Be("settings");
             ((Func<CacheProcessor>)(() => new CacheProcessor(server, settings, null))).Should().Throw<ArgumentNullException>().Which.ParamName.Should().Be("logger");
+        }
+
+        [TestMethod]
+        public void ContentHash_ComputeUniqueHash()
+        {
+            const string allNewLines = "public class Sample\n{\n\r\tint field;\n\r}\r";
+            const string diacritics = "ěščřžýáí";
+            using var sut = new CacheProcessor(Mock.Of<ISonarQubeServer>(), CreateProcessedArgs(), Mock.Of<ILogger>());
+            var root = TestUtils.CreateTestSpecificFolderWithSubPaths(TestContext);
+            var emptyWithBom = CreateFile(root, "EmptyWithBom.cs", string.Empty, Encoding.UTF8);
+            var emptyNoBom = CreateFile(root, "EmptyNoBom.cs", string.Empty, Encoding.ASCII);
+            var codeWithBom = CreateFile(root, "CodeWithBom.cs", allNewLines, Encoding.UTF8);
+            var codeNoBom = CreateFile(root, "CodeNoBom.cs", allNewLines, Encoding.ASCII);
+            var utf8 = CreateFile(root, "Utf8.cs", diacritics, Encoding.UTF8);
+            var utf16 = CreateFile(root, "Utf16.cs", diacritics, Encoding.Unicode);
+            var ansi = CreateFile(root, "Ansi.cs", diacritics, Encoding.GetEncoding(1250));
+            File.ReadAllBytes(emptyWithBom).Should().HaveCount(3, "UTF8 encoding should generate BOM");
+            File.ReadAllBytes(emptyNoBom).Should().HaveCount(0, "ASCII encoding should not generate BOM");
+
+            sut.ContentHash(emptyWithBom).Should().Be("f1945cd6c19e56b3c1c78943ef5ec18116907a4ca1efc40a57d48ab1db7adfc5");
+            sut.ContentHash(emptyNoBom).Should().Be("e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855");
+            sut.ContentHash(codeWithBom).Should().Be("b98aaf2ce5a3f9cdf8ab785563951f2309d577baa6351098f78908300fdc610a");
+            sut.ContentHash(codeNoBom).Should().Be("8c7535a8e3679bf8cc241b5749cef5fc38243401556f2b7869495c7b48ee4980");
+            sut.ContentHash(utf8).Should().Be("13aa54e315a806270810f3a91501f980a095a2ef1bcc53167d4c750a1b78684d");
+            sut.ContentHash(utf16).Should().Be("a9b3c4402770855d090ba4b49adeb5ad601cb3bbd6de18495302f45f242ef932");
+            sut.ContentHash(ansi).Should().Be("b965073262109da4f106cd90a5eeea025e2441c244af272537afa2cfb03c3ab8");
+        }
+
+        [TestMethod]
+        public void ContentHash_IsDeterministic()
+        {
+            using var sut = new CacheProcessor(Mock.Of<ISonarQubeServer>(), CreateProcessedArgs(), Mock.Of<ILogger>());
+            var root = TestUtils.CreateTestSpecificFolderWithSubPaths(TestContext);
+            var path = CreateFile(root, "File.txt", "Lorem ipsum", Encoding.UTF8);
+            var hash1 = sut.ContentHash(path);
+            var hash2 = sut.ContentHash(path);
+            var hash3 = sut.ContentHash(path);
+            hash2.Should().Be(hash1);
+            hash3.Should().Be(hash1);
+        }
+
+        private static string CreateFile(string root, string fileName, string content, Encoding encoding)
+        {
+            var path = Path.Combine(root, fileName);
+            File.WriteAllText(path, content, encoding);
+            return path;
         }
 
         private static ProcessedArgs CreateProcessedArgs()
