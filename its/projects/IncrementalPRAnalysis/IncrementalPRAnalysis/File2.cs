d7efde3e661099fb61f860701dffc059b592d964
Pavel Mikula <pavel.mikula@sonarsource.com>
2022-11-28 15:38:40 +0100

    Add sample project

---
+++ its/projects/IncrementalPRAnalysis/IncrementalPRAnalysis/File2.cs
@@ -0,0 +1,7 @@
+﻿namespace IncrementalPRAnalysis
+{
+    public class File2
+    {
+        // FIXME: S1134 in second unchanged file
+    }
+}
---
+++ its/projects/IncrementalPRAnalysis/IncrementalPRAnalysis/File3.cs
@@ -0,0 +1,7 @@
+﻿namespace IncrementalPRAnalysis
+{
+    public class File3
+    {
+        // FIXME: S1134 in third file that will have changes on PR
+    }
+}
