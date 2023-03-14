c97a1763c400f3e5eb739df87c37238dcf69c3ed
Sebastien Marichal <sebastien.marichal@sonarsource.com>
2023-03-14 17:34:16 +0100

    Code improvment

--- Tests/SonarScanner.MSBuild.PreProcessor.Test/Infrastructure/MockIDownloaderHelper.cs
+++ Tests/SonarScanner.MSBuild.PreProcessor.Test/Infrastructure/MockIDownloaderHelper.cs
@@ -34,31 +34,15 @@ namespace SonarScanner.MSBuild.PreProcessor.Test.Infrastructure
 
         public static Mock<IDownloader> SetupTryDownloadIfExists(this Mock<IDownloader> mock, Uri requestUri = null, string response = null)
         {
-            if (requestUri is null)
-            {
-                mock.Setup(x => x.TryDownloadIfExists(It.IsAny<Uri>(), It.IsAny<bool>()))
-                    .ReturnsAsync(Tuple.Create(response != null, response));
-            }
-            else
-            {
-                mock.Setup(x => x.TryDownloadIfExists(It.Is<Uri>(downloadUri => downloadUri == requestUri), It.IsAny<bool>()))
-                    .ReturnsAsync(Tuple.Create(response != null, response));
-            }
+            mock.Setup(x => x.TryDownloadIfExists(It.Is<Uri>(downloadUri => requestUri == null || downloadUri == requestUri), It.IsAny<bool>()))
+                .ReturnsAsync(Tuple.Create(response != null, response));
             return mock;
         }
 
         public static Mock<IDownloader> SetupDownload(this Mock<IDownloader> mock, Uri requestUri, string response)
         {
-            if (requestUri is null)
-            {
-                mock.Setup(x => x.Download(It.IsAny<Uri>(), It.IsAny<bool>()))
-                    .ReturnsAsync(response);
-            }
-            else
-            {
-                mock.Setup(x => x.Download(It.Is<Uri>(downloadUri => downloadUri == requestUri), It.IsAny<bool>()))
-                    .ReturnsAsync(response);
-            }
+            mock.Setup(x => x.Download(It.Is<Uri>(downloadUri => requestUri == null || downloadUri == requestUri), It.IsAny<bool>()))
+                .ReturnsAsync(response);
             return mock;
         }
     }
