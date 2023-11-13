473f61edd4988bb282064a11a76188d7b2e5c3c9
Costin Zaharia <56015273+costin-zaharia-sonarsource@users.noreply.github.com>
2023-11-13 11:51:38 +0100

    Implement ITs for code coverage (#1755)

---
+++ its/projects/CodeCoverage/CodeCoverage/Calculator.cs
@@ -0,0 +1,16 @@
+﻿namespace CodeCoverage;
+
+public class Calculator
+{
+    public int ConditionalAdd(int a, int b, bool condition)
+    {
+        if (condition)
+        {
+            return a + b;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
