55006c56db1119b79bc4ee20572ba1854d2d5735
Costin Zaharia <56015273+costin-zaharia-sonarsource@users.noreply.github.com>
2023-11-13 16:47:23 +0100

    Re-enable signing (#1758)

--- scripts/sign.ps1
+++ scripts/sign.ps1
@@ -1,31 +1,25 @@
-﻿Write-Host "Signing .NET 5.0 assemblies"
-Get-ChildItem "build\sonarscanner-msbuild-net5.0\Sonar*.dll" | 
-Foreach-Object {
-    & $env:SIGNTOOL_PATH sign /fd SHA256 /f $env:PFX_PATH /p $env:PFX_PASSWORD /tr http://timestamp.digicert.com?alg=sha256 $_.FullName
+﻿function Sign-Assemblies {
+    param (
+        [string]$Pattern
+    )
+    Get-ChildItem $Pattern |
+    Foreach-Object {
+        & signtool sign /du https://www.sonarsource.com/ /tr http://timestamp.digicert.com /td SHA256 /fd SHA256 /csp "DigiCert Signing Manager KSP" /kc "$env:SM_KP" /f "$env:SM_CLIENT_CRT_FILE" $_.FullName
+    }
 }
+
+Write-Host "Signing .NET 5.0 assemblies"
+Sign-Assemblies "build\sonarscanner-msbuild-net5.0\Sonar*.dll"
 Write-Host "[Completed] Signing .NET 5.0 assemblies"
 
 Write-Host "Signing .NET 4.6 assemblies"
-Get-ChildItem "build\sonarscanner-msbuild-net46\Sonar*.dll" | 
-Foreach-Object {
-    & $env:SIGNTOOL_PATH sign /fd SHA256 /f $env:PFX_PATH /p $env:PFX_PASSWORD /tr http://timestamp.digicert.com?alg=sha256 $_.FullName
-}
-Get-ChildItem "build\sonarscanner-msbuild-net46\Sonar*.exe" | 
-Foreach-Object {
-    & $env:SIGNTOOL_PATH sign /fd SHA256 /f $env:PFX_PATH /p $env:PFX_PASSWORD /tr http://timestamp.digicert.com?alg=sha256 $_.FullName
-}
+Sign-Assemblies "build\sonarscanner-msbuild-net46\Sonar*.dll"
 Write-Host "[Completed]Signing .NET 4.6 assemblies"
 
 Write-Host "Signing .NET Core 2 assemblies"
-Get-ChildItem "build\sonarscanner-msbuild-netcoreapp2.0\Sonar*.dll" | 
-Foreach-Object {
-    & $env:SIGNTOOL_PATH sign /fd SHA256 /f $env:PFX_PATH /p $env:PFX_PASSWORD /tr http://timestamp.digicert.com?alg=sha256 $_.FullName
-}
+Sign-Assemblies "build\sonarscanner-msbuild-netcoreapp2.0\Sonar*.dll"
 Write-Host "[Completed] Signing .NET Core 2 assemblies"
 
 Write-Host "Signing .NET Core 3 assemblies"
-Get-ChildItem "build\sonarscanner-msbuild-netcoreapp3.0\Sonar*.dll" | 
-Foreach-Object {
-    & $env:SIGNTOOL_PATH sign /fd SHA256 /f $env:PFX_PATH /p $env:PFX_PASSWORD /tr http://timestamp.digicert.com?alg=sha256 $_.FullName
-}
+Sign-Assemblies "build\sonarscanner-msbuild-netcoreapp3.0\Sonar*.dll"
 Write-Host "[Completed] Signing .NET Core 3 assemblies"
