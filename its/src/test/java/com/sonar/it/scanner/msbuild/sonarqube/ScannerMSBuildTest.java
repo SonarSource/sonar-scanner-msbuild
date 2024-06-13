70cdb8eba58a9d0b297ff76e5a118e675d3a15f4
Cristian Ambrosini <cristian.ambrosini@sonarsource.com>
2024-06-13 10:08:58 +0200

    Remove comment

--- its/src/test/java/com/sonar/it/scanner/msbuild/sonarqube/ScannerMSBuildTest.java
+++ its/src/test/java/com/sonar/it/scanner/msbuild/sonarqube/ScannerMSBuildTest.java
@@ -1016,7 +1016,6 @@ class ScannerMSBuildTest {
     BuildResult result = runBeginBuildAndEndForStandardProject(folderName, "");
     assertTrue(result.isSuccess());
 
-    // Outside.js is not detected: projectBaseDir is at .csproj level
     List<Issue> issues = TestUtils.allIssues(ORCHESTRATOR);
     assertThat(issues).hasSize(2)
       .extracting(Issue::getRule, Issue::getComponent)
