9a5982dae505dcff3542a74bc4ba399b796fd761
Cristian Ambrosini <cristian.ambrosini@sonarsource.com>
2024-06-21 09:50:05 +0200

    Exclude via Compile Remove

--- its/src/test/java/com/sonar/it/scanner/msbuild/sonarqube/ScannerMSBuildTest.java
+++ its/src/test/java/com/sonar/it/scanner/msbuild/sonarqube/ScannerMSBuildTest.java
@@ -1022,10 +1022,9 @@ class ScannerMSBuildTest {
     // Outside.js, Outside.sql are not detected: projectBaseDir is at .csproj level
     // Excluded.js, Excluded.sql, Excluded.cs are excluded from the .csproj with the Remove attribute
     List<Issue> issues = TestUtils.allIssues(ORCHESTRATOR);
-    assertThat(issues).hasSize(4)
+    assertThat(issues).hasSize(3)
       .extracting(Issue::getRule, Issue::getComponent)
       .containsExactlyInAnyOrder(
-        tuple("csharpsquid:S1134", folderName + ":Excluded.cs"), // FP: Excluded.cs should be excluded
         tuple("csharpsquid:S1134", folderName + ":Program.cs"),
         tuple("javascript:S1529", folderName + ":JavaScript.js"),
         tuple("plsql:S1134", folderName + ":plsql.sql"));
