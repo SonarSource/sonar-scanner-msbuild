fc1bf1f7397997e7f78653cdd0d685cfa704cdd2
Martin Strecker <martin.strecker@sonarsource.com>
2024-06-26 13:39:31 +0200

    Unused using

--- src/SonarScanner.MSBuild.PreProcessor/ArgumentProcessor.cs
+++ src/SonarScanner.MSBuild.PreProcessor/ArgumentProcessor.cs
@@ -23,7 +23,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Text.RegularExpressions;
 using SonarScanner.MSBuild.Common;
 using static SonarScanner.MSBuild.Common.CommandLine.CommandLineFlagPrefix;
 
