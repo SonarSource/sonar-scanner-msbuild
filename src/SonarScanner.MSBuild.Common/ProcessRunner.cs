294b8f85425b78b24a544aadb4178aef06a0ad0c
costin-zaharia-sonarsource <costin.zaharia@sonarsource.com>
2024-07-04 10:16:53 +0200

    Cleanup: ProcessRunner

--- src/SonarScanner.MSBuild.Common/ProcessRunner.cs
+++ src/SonarScanner.MSBuild.Common/ProcessRunner.cs
@@ -23,165 +23,148 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 
-namespace SonarScanner.MSBuild.Common
+namespace SonarScanner.MSBuild.Common;
+
+/// <summary>
+/// Helper class to run an executable and capture the output.
+/// </summary>
+public sealed class ProcessRunner(ILogger logger) : IProcessRunner
 {
+    public const int ErrorCode = 1;
+
+    private readonly ILogger logger = logger ?? throw new ArgumentNullException(nameof(logger));
+
+    public int ExitCode { get; private set; }
+
     /// <summary>
-    /// Helper class to run an executable and capture the output
+    /// Runs the specified executable and returns a boolean indicating success or failure.
     /// </summary>
-    public sealed class ProcessRunner : IProcessRunner
+    /// <remarks>The standard and error output will be streamed to the logger. Child processes do not inherit the env variables from the parent automatically.</remarks>
+    public bool Execute(ProcessRunnerArguments runnerArgs)
     {
-        public const int ErrorCode = 1;
-
-        private readonly ILogger logger;
-
-        public ProcessRunner(ILogger logger)
+        if (runnerArgs is null)
         {
-            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            throw new ArgumentNullException(nameof(runnerArgs));
+        }
+        Debug.Assert(!string.IsNullOrWhiteSpace(runnerArgs.ExeName), "Process runner exe name should not be null/empty");
+
+        if (!File.Exists(runnerArgs.ExeName))
+        {
+            logger.LogError(Resources.ERROR_ProcessRunner_ExeNotFound, runnerArgs.ExeName);
+            ExitCode = ErrorCode;
+            return false;
         }
 
-        #region Public methods
-
-        public int ExitCode { get; private set; }
-
-        /// <summary>
-        /// Runs the specified executable and returns a boolean indicating success or failure
-        /// </summary>
-        /// <remarks>The standard and error output will be streamed to the logger. Child processes do not inherit the env variables from the parent automatically</remarks>
-        public bool Execute(ProcessRunnerArguments runnerArgs)
+        var psi = new ProcessStartInfo
         {
-            if (runnerArgs == null)
-            {
-                throw new ArgumentNullException(nameof(runnerArgs));
-            }
-            Debug.Assert(!string.IsNullOrWhiteSpace(runnerArgs.ExeName), "Process runner exe name should not be null/empty");
+            FileName = runnerArgs.ExeName,
+            RedirectStandardError = true,
+            RedirectStandardOutput = true,
+            UseShellExecute = false, // required if we want to capture the error output
+            ErrorDialog = false,
+            CreateNoWindow = true,
+            Arguments = runnerArgs.GetEscapedArguments(),
+            WorkingDirectory = runnerArgs.WorkingDirectory
+        };
 
-            if (!File.Exists(runnerArgs.ExeName))
-            {
-                logger.LogError(Resources.ERROR_ProcessRunner_ExeNotFound, runnerArgs.ExeName);
-                ExitCode = ErrorCode;
-                return false;
-            }
+        SetEnvironmentVariables(psi, runnerArgs.EnvironmentVariables);
 
-            var psi = new ProcessStartInfo()
-            {
-                FileName = runnerArgs.ExeName,
-                RedirectStandardError = true,
-                RedirectStandardOutput = true,
-                UseShellExecute = false, // required if we want to capture the error output
-                ErrorDialog = false,
-                CreateNoWindow = true,
-                Arguments = runnerArgs.GetEscapedArguments(),
-                WorkingDirectory = runnerArgs.WorkingDirectory
-            };
+        using var process = new Process();
+        process.StartInfo = psi;
+        process.ErrorDataReceived += OnErrorDataReceived;
+        process.OutputDataReceived += OnOutputDataReceived;
 
-            SetEnvironmentVariables(psi, runnerArgs.EnvironmentVariables);
+        process.Start();
+        process.BeginErrorReadLine();
+        process.BeginOutputReadLine();
 
-            bool succeeded;
-            using (var process = new Process())
-            {
-                process.StartInfo = psi;
-                process.ErrorDataReceived += OnErrorDataReceived;
-                process.OutputDataReceived += OnOutputDataReceived;
+        // Warning: do not log the raw command line args as they
+        // may contain sensitive data
+        logger.LogDebug(Resources.MSG_ExecutingFile,
+            runnerArgs.ExeName,
+            runnerArgs.AsLogText(),
+            runnerArgs.WorkingDirectory,
+            runnerArgs.TimeoutInMilliseconds,
+            process.Id);
 
-                process.Start();
-                process.BeginErrorReadLine();
-                process.BeginOutputReadLine();
-
-                // Warning: do not log the raw command line args as they
-                // may contain sensitive data
-                logger.LogDebug(Resources.MSG_ExecutingFile,
-                    runnerArgs.ExeName,
-                    runnerArgs.AsLogText(),
-                    runnerArgs.WorkingDirectory,
-                    runnerArgs.TimeoutInMilliseconds,
-                    process.Id);
-
-                succeeded = process.WaitForExit(runnerArgs.TimeoutInMilliseconds);
-                if (succeeded)
-                {
-                    process.WaitForExit(); // Give any asynchronous events the chance to complete
-                }
-
-                // false means we asked the process to stop but it didn't.
-                // true: we might still have timed out, but the process ended when we asked it to
-                if (succeeded)
-                {
-                    logger.LogDebug(Resources.MSG_ExecutionExitCode, process.ExitCode);
-                    ExitCode = process.ExitCode;
-                }
-                else
-                {
-                    ExitCode = ErrorCode;
-
-                    try
-                    {
-                        process.Kill();
-                        logger.LogWarning(Resources.WARN_ExecutionTimedOutKilled, runnerArgs.TimeoutInMilliseconds, runnerArgs.ExeName);
-                    }
-                    catch
-                    {
-                        logger.LogWarning(Resources.WARN_ExecutionTimedOutNotKilled, runnerArgs.TimeoutInMilliseconds, runnerArgs.ExeName);
-                    }
-                }
-
-                succeeded = succeeded && (ExitCode == 0);
-            }
-
-            return succeeded;
+        var succeeded = process.WaitForExit(runnerArgs.TimeoutInMilliseconds);
+        if (succeeded)
+        {
+            process.WaitForExit(); // Give any asynchronous events the chance to complete
         }
 
-        #endregion Public methods
-
-        #region Private methods
-
-        private void SetEnvironmentVariables(ProcessStartInfo psi, IDictionary<string, string> envVariables)
+        // false means we asked the process to stop but it didn't.
+        // true: we might still have timed out, but the process ended when we asked it to
+        if (succeeded)
         {
-            if (envVariables == null)
+            logger.LogDebug(Resources.MSG_ExecutionExitCode, process.ExitCode);
+            ExitCode = process.ExitCode;
+        }
+        else
+        {
+            ExitCode = ErrorCode;
+
+            try
             {
-                return;
+                process.Kill();
+                logger.LogWarning(Resources.WARN_ExecutionTimedOutKilled, runnerArgs.TimeoutInMilliseconds, runnerArgs.ExeName);
             }
-
-            foreach (var envVariable in envVariables)
+            catch
             {
-                Debug.Assert(!string.IsNullOrEmpty(envVariable.Key), "Env variable name cannot be null or empty");
-
-                if (psi.EnvironmentVariables.ContainsKey(envVariable.Key))
-                {
-                    logger.LogDebug(Resources.MSG_Runner_OverwritingEnvVar, envVariable.Key, psi.EnvironmentVariables[envVariable.Key], envVariable.Value);
-                }
-                else
-                {
-                    logger.LogDebug(Resources.MSG_Runner_SettingEnvVar, envVariable.Key, envVariable.Value);
-                }
-                psi.EnvironmentVariables[envVariable.Key] = envVariable.Value;
+                logger.LogWarning(Resources.WARN_ExecutionTimedOutNotKilled, runnerArgs.TimeoutInMilliseconds, runnerArgs.ExeName);
             }
         }
 
-        private void OnOutputDataReceived(object sender, DataReceivedEventArgs e)
+        succeeded = succeeded && (ExitCode == 0);
+
+        return succeeded;
+    }
+
+    private void SetEnvironmentVariables(ProcessStartInfo psi, IDictionary<string, string> envVariables)
+    {
+        if (envVariables is null)
         {
-            if (e.Data != null)
-            {
-                // It's important to log this as an important message because
-                // this the log redirection pipeline of the child process
-                logger.LogInfo(e.Data);
-            }
+            return;
         }
 
-        private void OnErrorDataReceived(object sender, DataReceivedEventArgs e)
+        foreach (var envVariable in envVariables)
         {
-            if (e.Data != null)
+            Debug.Assert(!string.IsNullOrEmpty(envVariable.Key), "Env variable name cannot be null or empty");
+
+            if (psi.EnvironmentVariables.ContainsKey(envVariable.Key))
             {
-                if (e.Data.StartsWith("WARN"))
-                {
-                    logger.LogWarning(e.Data);
-                }
-                else
-                {
-                    logger.LogError(e.Data);
-                }
+                logger.LogDebug(Resources.MSG_Runner_OverwritingEnvVar, envVariable.Key, psi.EnvironmentVariables[envVariable.Key], envVariable.Value);
+            }
+            else
+            {
+                logger.LogDebug(Resources.MSG_Runner_SettingEnvVar, envVariable.Key, envVariable.Value);
+            }
+            psi.EnvironmentVariables[envVariable.Key] = envVariable.Value;
+        }
+    }
+
+    private void OnOutputDataReceived(object sender, DataReceivedEventArgs e)
+    {
+        if (e.Data is not null)
+        {
+            // It's important to log this as an important message because
+            // this the log redirection pipeline of the child process
+            logger.LogInfo(e.Data);
+        }
+    }
+
+    private void OnErrorDataReceived(object sender, DataReceivedEventArgs e)
+    {
+        if (e.Data is not null)
+        {
+            if (e.Data.StartsWith("WARN"))
+            {
+                logger.LogWarning(e.Data);
+            }
+            else
+            {
+                logger.LogError(e.Data);
             }
         }
-
-        #endregion Private methods
     }
 }
