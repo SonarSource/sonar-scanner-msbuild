2075e0f482bc188998599d999c6045e45930a943
Gregory Paidis <gregory.paidis@sonarsource.com>
2024-08-30 11:33:13 +0200

    Enable rc releasing from infrastructure

--- scripts/promote-project-version.ps1
+++ scripts/promote-project-version.ps1
@@ -4,7 +4,7 @@ Write-Host "Reading the Sonar project version from '${versionFilePath}' ..."
 
 # Read the version from the file
 [xml]$versionProps = Get-Content "$versionFilePath"
-$sonarProjectVersion = $versionProps.Project.PropertyGroup.MainVersion + "-rc"
+$sonarProjectVersion = $versionProps.Project.PropertyGroup.MainVersion + $versionProps.Project.PropertyGroup.PrereleaseSuffix
 Write-Host "Sonar project version is '${sonarProjectVersion}'"
 # Set the variable to it can be used by other tasks
 Write-Host "##vso[task.setvariable variable=SONAR_PROJECT_VERSION]$sonarProjectVersion"
--- scripts/version/set-version.ps1
+++ scripts/version/set-version.ps1
@@ -9,7 +9,11 @@ This script allows to set the specified version in all required files.
 Param(
     [Parameter(Mandatory = $True, Position = 1)]
     [ValidatePattern("^\d{1,3}\.\d{1,3}\.\d{1,3}$")]
-    [string]$version
+    [string]$version,
+
+    [Parameter(Mandatory = $False, Position = 1)]
+    [ValidatePattern("^alpha|beta|rc$")] # see https://learn.microsoft.com/en-us/nuget/concepts/package-versioning?tabs=semver20sort#pre-release-versions
+    [string]$prereleaseSuffix = ""
 )
 
 Set-StrictMode -version 2.0
@@ -22,6 +26,13 @@ function Set-VersionForDotNet() {
         $versionPropsFile = Resolve-Path "Version.props"
         $xml = [xml](Get-Content $versionPropsFile)
         $xml.Project.PropertyGroup.MainVersion = $version
+        if (-not [string]::IsNullOrWhiteSpace($prereleaseSuffix)) {
+            $xml.Project.PropertyGroup.PrereleaseSuffix = "-" + $prereleaseSuffix
+        }
+        else {
+            $xml.Project.PropertyGroup.PrereleaseSuffix = ""
+        }
+
         $xml.Save($versionPropsFile)
         msbuild "ChangeVersion.proj"
         Test-ExitCode "ERROR: Change version FAILED."
@@ -37,7 +48,7 @@ try {
     if ($fixedVersion.EndsWith(".0")) {
         $fixedVersion = $version.Substring(0, $version.Length - 2)
     }
-    
+
     Set-VersionForDotNet
 
     exit 0
