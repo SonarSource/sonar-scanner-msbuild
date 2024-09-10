8da265911b8049c33c9453604c969cc51d0d99f8
Cristian Ambrosini <cristian.ambrosini@sonarsource.com>
2024-09-10 14:36:34 +0200

    First implementation

--- src/SonarScanner.MSBuild.PreProcessor/AnalysisConfigGenerator.cs
+++ src/SonarScanner.MSBuild.PreProcessor/AnalysisConfigGenerator.cs
@@ -27,6 +27,13 @@ namespace SonarScanner.MSBuild.PreProcessor;
 
 public static class AnalysisConfigGenerator
 {
+    public static HashSet<string> CoveragePropertyNames =
+        [
+            "sonar.cs.vscoveragexml.reportsPaths",
+            "sonar.cs.dotcover.reportsPaths",
+            "sonar.cs.opencover.reportsPaths",
+        ];
+
     /// <summary>
     /// Combines the various configuration options into the AnalysisConfig file
     /// used by the build and post-processor. Saves the file and returns the config instance.
@@ -82,7 +89,7 @@ public static class AnalysisConfigGenerator
         {
             AddSetting(config.ServerSettings, property.Key, property.Value);
         }
-        foreach (var property in GetCommandLineProperties(localSettings))    // Only those from command line
+        foreach (var property in GetCommandLineProperties(localSettings, serverProperties))    // Only those from command line
         {
             AddSetting(config.LocalSettings, property.Id, property.Value);
         }
@@ -98,6 +105,36 @@ public static class AnalysisConfigGenerator
         return config;
     }
 
+    // See https://sonarsource.atlassian.net/browse/SCAN4NET-29
+    // This method is a hack and should be removed when we properly support excluding coverage files in the scanner-engine.
+    // Instead, it should be replaced at the call site by "localSettings.CmdLineProperties.GetAllProperties()".
+    // The idea is that we are manually adding the coverage paths to the exclusions, so that they do not appear on the analysis.
+    private static List<Property> GetCommandLineProperties(ProcessedArgs localSettings, IDictionary<string, string> serverProperties)
+    {
+        var commandLineProperties = localSettings.CmdLineProperties.GetAllProperties().ToList();
+        var coveragePaths = string.Join(",", commandLineProperties.Where(x => CoveragePropertyNames.Contains(x.Id)).Select(x => x.Value));
+        if (!localSettings.ScanAllAnalysis      // if scanAll analysis is disabled, we will not pick up the coverage files anyways
+            || coveragePaths.Length == 0)       // if there are no coverage files, there is nothing to exclude
+        {
+            return commandLineProperties;
+        }
+
+        if (commandLineProperties.Find(x => x.Id == "sonar.exclusions") is { } localExclusions)
+        {
+            localExclusions.Value += "," + coveragePaths;
+        }
+        else if (serverProperties.TryGetValue("sonar.exclusions", out var serverExclusions))
+        {
+            commandLineProperties.Add(new("sonar.exclusions", serverExclusions + "," + coveragePaths));
+        }
+        else
+        {
+            commandLineProperties.Add(new("sonar.exclusions", coveragePaths));
+        }
+
+        return commandLineProperties;
+    }
+
     private static void AddSetting(AnalysisProperties properties, string id, string value)
     {
         var property = new Property(id, value);
@@ -107,37 +144,5 @@ public static class AnalysisConfigGenerator
         {
             properties.Add(new(id, value));
         }
-    }
-
-    // See https://sonarsource.atlassian.net/browse/SCAN4NET-29
-    // This method is a hack and should be removed when we properly support excluding coverage files in the scanner-engine.
-    // Instead, it should be replaced at the call site by "localSettings.CmdLineProperties.GetAllProperties()".
-    // The idea is that we are manually adding the coverage paths to the exclusions, so that they do not appear on the analysis.
-    private static List<Property> GetCommandLineProperties(ProcessedArgs localSettings)
-    {
-        HashSet<string> coveragePropertyNames =
-        [
-            "sonar.cs.vscoveragexml.reportsPaths",
-            "sonar.cs.dotcover.reportsPaths",
-            "sonar.cs.opencover.reportsPaths",
-        ];
-        var allProperties = localSettings.CmdLineProperties.GetAllProperties().ToList();
-        var coveragePaths = string.Join(",", allProperties.Where(x => coveragePropertyNames.Contains(x.Id)).Select(x => x.Value));
-        if (!localSettings.ScanAllAnalysis      // if scanAll analysis is disabled, we will not pick up the coverage files anyways
-            || coveragePaths.Length == 0)       // if there are no coverage files, there is nothing to exclude
-        {
-            return allProperties;
-        }
-
-        if (allProperties.Find(x => x.Id == "sonar.exclusions") is { } exclusionsProperty)
-        {
-            exclusionsProperty.Value = exclusionsProperty.Value + "," + coveragePaths;
-        }
-        else
-        {
-            allProperties.Add(new("sonar.exclusions", coveragePaths));
-        }
-
-        return allProperties;
     }
 }
