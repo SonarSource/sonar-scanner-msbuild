9ace0f0cc0a4e956c283ad2dfb2616feab9864c5
Costin Zaharia <56015273+costin-zaharia-sonarsource@users.noreply.github.com>
2024-09-26 09:27:55 +0200

    SCAN4NET-94 Fix Choco packaging scripts (#2211)

--- scripts/generate-packages.ps1
+++ scripts/generate-packages.ps1
@@ -1,45 +1,65 @@
-﻿param ($sourcesDirectory)
+﻿# This script generates the chocolatey packages for the .NET Scanner and the .NET Framework Scanner and updates the pom.xml file with the new artifacts locations.
 
-[xml]$versionProps = Get-Content "$env:BUILD_SOURCESDIRECTORY\scripts\version\Version.props"
-$version= $versionProps.Project.PropertyGroup.MainVersion + $versionProps.Project.PropertyGroup.PrereleaseSuffix
+# The chocolatey packages are generated from the nuspec files located in the nuspec\chocolatey folder and they point to GitHub artifacts that have the following format:
+# Release candidates:
+# - https://github.com/SonarSource/sonar-scanner-msbuild/releases/download/9.0.0-rc.99116/sonar-scanner-9.0.0-rc.99116-net.zip
+# - https://github.com/SonarSource/sonar-scanner-msbuild/releases/download/9.0.0-rc.99116/sonar-scanner-9.0.0-rc.99116-net-framework.zip
+# Normal releases:
+# - https://github.com/SonarSource/sonar-scanner-msbuild/releases/download/8.0.3.99785/sonar-scanner-8.0.3.99785-net.zip
+# - https://github.com/SonarSource/sonar-scanner-msbuild/releases/download/8.0.3.99785/sonar-scanner-8.0.3.99785-net-framework.zip
 
-$artifactsFolder = "$env:BUILD_SOURCESDIRECTORY\\build"
+# In the case of pre-releases, the full version is following the Sem 2 versioning format: 9.0.0-rc.99116.
+# Unfortunatelly Choco only supports Sem 1 versioning format, which does not allow anything except for [0-9A-Za-z-] after the dash in `-rc`.
+# Due to this, when calling `choco pack` the version should not contain the build number (9.0.0-rc).
+# At the same time the the url inside the ps1 file that downloads the scanner should be correct and contain the build number.
 
-$netFrameworkScannerZipPath = Get-Item "$artifactsFolder\\sonarscanner-net-framework.zip"
-$netScannerZipPath = Get-Item "$artifactsFolder\\sonarscanner-net.zip"
-$netScannerGlobalToolPath = Get-Item "$artifactsFolder\\dotnet-sonarscanner.$version.nupkg"
-$sbomJsonPath = Get-Item "$sourcesDirectory\build\bom.json"
+param (
+  [string] $sourcesDirectory = $env:BUILD_SOURCESDIRECTORY,
+  [string] $buildId = $env:BUILD_BUILDID
+)
 
-Write-Host "Generating the chocolatey packages"
-$netFrameworkZipPath = (Get-FileHash $netFrameworkScannerZipPath -Algorithm SHA256).hash
-$netFrameworkPs1 = "nuspec\chocolatey\chocolateyInstall-net-framework.ps1"
-(Get-Content $netFrameworkPs1) `
-  -Replace '-Checksum "not-set"', "-Checksum $netFrameworkZipPath" `
-  -Replace "__PackageVersion__", "$version" `
-| Set-Content $netFrameworkPs1
+function Update-Choco-Package([string] $scannerZipFileName, [string] $runtimeSuffix) {
+  Write-Host "Generating the chocolatey package from $scannerZipFileName"
 
-$netZipHash = (Get-FileHash $netScannerZipPath -Algorithm SHA256).hash
-$netPs1 = "nuspec\chocolatey\chocolateyInstall-net.ps1"
-(Get-Content $netPs1) `
-  -Replace '-Checksum "not-set"', "-Checksum $netZipHash" `
-  -Replace "__PackageVersion__", "$version" `
-| Set-Content $netPs1
+  $hash = (Get-FileHash $scannerZipFileName -Algorithm SHA256).hash
+  $powershellScriptPath = "nuspec/chocolatey/chocolateyInstall-$runtimeSuffix.ps1"
+  Write-Host (Get-Item $powershellScriptPath).FullName
+  (Get-Content $powershellScriptPath) `
+    -Replace '-Checksum "not-set"', "-Checksum $hash" `
+    -Replace '__PackageVersion__', "$fullVersion" `
+  | Set-Content $powershellScriptPath
 
-choco pack nuspec\chocolatey\sonarscanner-net-framework.nuspec `
-  --outputdirectory $artifactsFolder `
-  --version $version
+  choco pack "nuspec/chocolatey/sonarscanner-$runtimeSuffix.nuspec" --outputdirectory $artifactsFolder --version $shortVersion
+}
 
-choco pack nuspec\chocolatey\sonarscanner-net.nuspec `
-  --outputdirectory $artifactsFolder `
-  --version $version
+function Update-Pom-File() {
+  Write-Host 'Update artifacts locations in pom.xml'
+  $sbomJsonPath = Get-Item "$sourcesDirectory/build/bom.json"
+  $netScannerGlobalToolPath = Get-Item "$artifactsFolder/dotnet-sonarscanner.$shortVersion.nupkg"                         # dotnet-sonarscanner.9.0.0-rc.nupkg or dotnet-sonarscanner.9.0.0.nupkg
+  $pomFile = './pom.xml'
+  (Get-Content $pomFile) `
+    -Replace 'netFrameworkScannerZipPath', "$netFrameworkScannerZipPath" `
+    -Replace 'netScannerZipPath', "$netScannerZipPath" `
+    -Replace 'netScannerGlobalToolPath', "$netScannerGlobalToolPath" `
+    -Replace 'netFrameworkScannerChocoPath', "$artifactsFolder/sonarscanner-net-framework.$shortVersion.nupkg" `
+    -Replace 'netScannerChocoPath', "$artifactsFolder/sonarscanner-net.$shortVersion.nupkg" `
+    -Replace 'sbomPath', "$sbomJsonPath" `
+  | Set-Content $pomFile
+}
 
-Write-Host "Update artifacts locations in pom.xml"
-$pomFile = ".\pom.xml"
-(Get-Content $pomFile) `
-  -Replace 'netFrameworkScannerZipPath', "$netFrameworkScannerZipPath" `
-  -Replace 'netScannerZipPath', "$netScannerZipPath" `
-  -Replace 'netScannerGlobalToolPath', "$netScannerGlobalToolPath" `
-  -Replace 'netFrameworkScannerChocoPath', "$artifactsFolder\\sonarscanner-net-framework.$version.nupkg" `
-  -Replace 'netScannerChocoPath', "$artifactsFolder\\sonarscanner-net.$version.nupkg" `
-  -Replace 'sbomPath', "$sbomJsonPath" `
-| Set-Content $pomFile
+function Run() {
+  Update-Choco-Package $netFrameworkScannerZipPath 'net-framework'
+  Update-Choco-Package $netScannerZipPath 'net'
+  Update-Pom-File
+}
+
+# Read the version from the Version.props file and initialize the global variables.
+$artifactsFolder = "$sourcesDirectory/build"
+$netFrameworkScannerZipPath = Get-Item "$artifactsFolder/sonarscanner-net-framework.zip"
+$netScannerZipPath = Get-Item "$artifactsFolder/sonarscanner-net.zip"
+
+Write-Host 'Reading version information from Version.props file'
+[xml] $versionProps = Get-Content "$sourcesDirectory/scripts/version/Version.props"
+$shortVersion = $versionProps.Project.PropertyGroup.MainVersion + $versionProps.Project.PropertyGroup.PrereleaseSuffix  # 9.0.0-rc       for release candidates or 9.0.0       for normal releases.
+$fullVersion = $shortVersion + '.' + $buildId                                                                           # 9.0.0-rc.99116 for release candidates or 9.0.0.99116 for normal releases.
+Write-Host "Short version is $shortVersion, Full version is $fullVersion"
---
+++ scripts/generate-packages.tests.ps1
@@ -0,0 +1,91 @@
+# Unit tests for the generate-packages.ps1 script.
+# Preconditions:
+# - Install chocolatey: https://docs.chocolatey.org/en-us/choco/setup/#install-with-powershellexe
+# - Install Pester: Install-Module -Name Pester -Force -SkipPublisherCheck (https://pester.dev/docs/introduction/installation#windows)
+# Running:
+# - Open a PowerShell terminal.
+# - Generate binaries: .\scripts\its-build.ps1
+# - Package the scanner: nuget pack nuspec\netcoreglobaltool\dotnet-sonarscanner.nuspec
+# - Run the tests: Invoke-Pester -Output Detailed .\scripts\generate-packages.tests.ps1
+
+BeforeAll {
+    New-Item -Force -Path "$PSScriptRoot" -Name "testcontext" -ItemType Directory
+    New-Item -Force -Path "$PSScriptRoot/testcontext" -Name "nuspec" -ItemType Directory
+    New-Item -Force -Path "$PSScriptRoot/testcontext" -Name "build" -ItemType Directory
+    New-Item -Force -Path "$PSScriptRoot/testcontext/build" -Name "sonarscanner-net.zip" -type File
+    New-Item -Force -Path "$PSScriptRoot/testcontext/build" -Name "sonarscanner-net-framework.zip" -type File
+    New-Item -Force -Path "$PSScriptRoot/testcontext/scripts/version" -Name "Version.props" -type File
+
+    Set-Location "$PSScriptRoot/testcontext"
+
+    function CheckVersion([string] $packageFileName, [string] $nuspecFileName, [string] $expectedVersion) {
+        Rename-Item -Path "build/$packageFileName" -NewName 'temp.zip'
+        Expand-Archive 'build/temp.zip' -DestinationPath 'build/temp'
+
+        Get-Content -Raw "build/temp/$nuspecFileName" | Should -Match $expectedVersion
+
+        Remove-Item -Path 'build/temp.zip'
+        Remove-Item -Path 'build/temp' -Recurse
+    }
+
+    function Set-Version([string] $version, [string] $prereleaseSuffix) {
+        Set-Content -Path 'scripts/version/Version.props' -Value "<Project xmlns=""http://schemas.microsoft.com/developer/msbuild/2003"">
+  <PropertyGroup>
+    <MainVersion>$version</MainVersion>
+    <BuildNumber>123456789</BuildNumber>
+    <PrereleaseSuffix>$prereleaseSuffix</PrereleaseSuffix>
+  </PropertyGroup>
+</Project>"
+    }
+}
+
+AfterAll {
+    Set-Location $PSScriptRoot
+    Remove-Item -Path "$PSScriptRoot/testcontext" -Recurse
+}
+
+Describe 'Choco package generation' {
+    Describe 'Main' {
+        It 'Sets short and long versions' -TestCases @(
+            @{ Version = '1.2.3'; PreReleaseSuffix = '-rc'; ExpectedShortVersion = '1.2.3-rc'; ExpectedFullVersion = '1.2.3-rc.99116' }
+            @{ Version = '1.2.3'; PreReleaseSuffix = ''; ExpectedShortVersion = '1.2.3'; ExpectedFullVersion = '1.2.3.99116' }
+        ) {
+            Set-Version $Version $PreReleaseSuffix
+
+            . $PSScriptRoot/generate-packages.ps1 -sourcesDirectory . -buildId 99116
+
+            $shortVersion | Should -Be $ExpectedShortVersion
+            $fullVersion | Should -Be $ExpectedFullVersion
+        }
+    }
+
+    Describe 'Update-Choco-Package' {
+        BeforeEach {
+            Remove-Item -Path 'nuspec' -Recurse
+            Copy-Item -Path "../../nuspec/chocolatey/" -Destination "nuspec/chocolatey" -Recurse
+        }
+
+        It 'Sets the package name, version and url' -TestCases @(
+            @{ Version = '1.2.3'; PreReleaseSuffix = '-rc'; ExpectedShortVersion = '1.2.3-rc'; ExpectedFullVersion = '1.2.3-rc.99116' }
+            @{ Version = '1.2.3'; PreReleaseSuffix = ''; ExpectedShortVersion = '1.2.3'; ExpectedFullVersion = '1.2.3.99116' }
+        )  {
+            Set-Version $Version $PreReleaseSuffix
+
+            . $PSScriptRoot/generate-packages.ps1 -sourcesDirectory . -buildId 99116
+
+            Update-Choco-Package $netFrameworkScannerZipPath 'net-framework'
+
+            $unzipLocation = '$(Split-Path -parent $MyInvocation.MyCommand.Definition)'
+            Get-Content -Raw 'nuspec/chocolatey/chocolateyInstall-net-framework.ps1' | Should -BeExactly "Install-ChocolateyZipPackage ""sonarscanner-net-framework"" ``
+    -Url ""https://github.com/SonarSource/sonar-scanner-msbuild/releases/download/$ExpectedFullVersion/sonar-scanner-$ExpectedFullVersion-net-framework.zip"" ``
+    -UnzipLocation ""$unzipLocation"" ``
+    -ChecksumType 'sha256' ``
+    -Checksum E3B0C44298FC1C149AFBF4C8996FB92427AE41E4649B934CA495991B7852B855
+"
+
+            "build/sonarscanner-net-framework.$ExpectedShortVersion.nupkg" | Should -Exist
+
+            CheckVersion "sonarscanner-net-framework.$ExpectedShortVersion.nupkg" 'sonarscanner-net-framework.nuspec' "<version>$ExpectedShortVersion</version>"
+        }
+    }
+}
