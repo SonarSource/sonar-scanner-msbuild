9a8f9ef94d1db1d2e081a72f86dbd96f85a5efc7
Costin Zaharia <56015273+costin-zaharia-sonarsource@users.noreply.github.com>
2024-11-07 17:50:35 +0100

    SCAN4NET-159 Set version to 9.0.2 to prepare hotfix (#2262)

--- AssemblyInfo.Shared.cs
+++ AssemblyInfo.Shared.cs
@@ -22,9 +22,9 @@ using System.Reflection;
 using System.Resources;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("9.1.0")]
-[assembly: AssemblyFileVersion("9.1.0.0")]
-[assembly: AssemblyInformationalVersion("Version:9.1.0.0 Branch:not-set Sha1:not-set")]
+[assembly: AssemblyVersion("9.0.2")]
+[assembly: AssemblyFileVersion("9.0.2.0")]
+[assembly: AssemblyInformationalVersion("Version:9.0.2.0 Branch:not-set Sha1:not-set")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("SonarSource and Microsoft")]
 [assembly: AssemblyCopyright("Copyright © SonarSource and Microsoft 2015-2023")]
