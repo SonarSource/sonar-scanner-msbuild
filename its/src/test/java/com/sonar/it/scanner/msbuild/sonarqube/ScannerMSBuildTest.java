a9381e65df9ba4a62070e8af733b97dce23635b9
Cristian Ambrosini <cristian.ambrosini@sonarsource.com>
2024-12-10 15:49:17 +0100

    Update issue size

--- its/src/test/java/com/sonar/it/scanner/msbuild/sonarqube/ScannerMSBuildTest.java
+++ its/src/test/java/com/sonar/it/scanner/msbuild/sonarqube/ScannerMSBuildTest.java
@@ -767,7 +767,7 @@ class ScannerMSBuildTest {
 
     assertUIWarnings(buildResult);
     List<Issue> issues = TestUtils.allIssues(ORCHESTRATOR);
-    assertThat(issues).hasSize(3)
+    assertThat(issues).hasSize(2)
       .extracting(Issue::getRule, Issue::getComponent)
       .containsExactlyInAnyOrder(
         tuple(SONAR_RULES_PREFIX + "S1134", folderName + ":Main/Common.cs"),
