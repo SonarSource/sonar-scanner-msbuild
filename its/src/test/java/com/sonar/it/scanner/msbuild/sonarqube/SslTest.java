fec3cf97485c1031662251fa169ec69f4273063e
Sebastien Marichal <sebastien.marichal@sonarsource.com>
2025-01-29 17:10:30 +0100

    Add missing header

--- its/src/test/java/com/sonar/it/scanner/msbuild/sonarqube/SslTest.java
+++ its/src/test/java/com/sonar/it/scanner/msbuild/sonarqube/SslTest.java
@@ -1,3 +1,23 @@
+/*
+ * SonarScanner for .NET
+ * Copyright (C) 2016-2025 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
 package com.sonar.it.scanner.msbuild.sonarqube;
 
 import com.sonar.it.scanner.msbuild.utils.EnvironmentVariable;
@@ -8,7 +28,6 @@ import com.sonar.orchestrator.util.NetworkUtils;
 import java.net.InetAddress;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.List;
 import org.eclipse.jetty.http.HttpVersion;
 import org.eclipse.jetty.proxy.ProxyServlet;
