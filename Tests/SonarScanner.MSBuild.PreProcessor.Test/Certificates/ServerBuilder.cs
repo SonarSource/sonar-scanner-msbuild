fa45dc9762ffc4d54f45314d5985d754450a1a08
Martin Strecker <103252490+martin-strecker-sonarsource@users.noreply.github.com>
2025-02-06 10:37:43 +0100

    SCAN4NET-236 Multi-target SonarScanner.MSBuild.PreProcessor.Test (#2315)

--- Tests/SonarScanner.MSBuild.PreProcessor.Test/Certificates/ServerBuilder.cs
+++ Tests/SonarScanner.MSBuild.PreProcessor.Test/Certificates/ServerBuilder.cs
@@ -21,6 +21,7 @@
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
+using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 using WireMock.Server;
 using WireMock.Settings;
@@ -68,10 +69,16 @@ internal static class ServerBuilder
     private static X509Certificate2Collection AddCertificatesToStore(X509Certificate2Collection certificates)
     {
         RemoveTestCertificatesFromStores();
-        var newCertificates = new X509Certificate2Collection();
-
+        var certificatesBytes = certificates.Export(X509ContentType.Pfx);
         // Flags are needed because of occasional 0x8009030d errors https://stackoverflow.com/a/46091100
-        newCertificates.Import(certificates.Export(X509ContentType.Pfx), string.Empty, X509KeyStorageFlags.PersistKeySet | X509KeyStorageFlags.MachineKeySet | X509KeyStorageFlags.Exportable);
+        var storageFlags = X509KeyStorageFlags.PersistKeySet | X509KeyStorageFlags.UserKeySet | X509KeyStorageFlags.Exportable;
+        var newCertificates =
+#if NET
+        X509CertificateLoader.LoadPkcs12Collection(certificatesBytes, string.Empty, storageFlags);
+#else
+        new X509Certificate2Collection();
+        newCertificates.Import(certificatesBytes, string.Empty, storageFlags);
+#endif
         foreach (var newCertificate in newCertificates)
         {
             newCertificate.FriendlyName = FriendlyNameIdentifier; // This is used to identify the certificate later so we can remove it
@@ -107,7 +114,14 @@ internal static class ServerBuilder
             using var store = new X509Store(storeName, StoreLocation.CurrentUser);
             store.Open(OpenFlags.ReadWrite);
             var existingCertificates = new X509Certificate2Collection(store.Certificates.Cast<X509Certificate2>().Where(x => x.FriendlyName == FriendlyNameIdentifier).ToArray());
-            store.RemoveRange(existingCertificates);
+            try
+            {
+                store.RemoveRange(existingCertificates);
+            }
+            catch (CryptographicException)
+            {
+                // Sometimes the removal fails with AccessDenied in CI.
+            }
         }
     }
 
