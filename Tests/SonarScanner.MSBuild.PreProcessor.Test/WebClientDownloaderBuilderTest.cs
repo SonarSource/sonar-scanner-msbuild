4e12342f9a841d81de40eeb1462e686164735a1f
Martin Strecker <martin.strecker@sonarsource.com>
2025-02-12 17:10:41 +0100

    Better comment

--- Tests/SonarScanner.MSBuild.PreProcessor.Test/WebClientDownloaderBuilderTest.cs
+++ Tests/SonarScanner.MSBuild.PreProcessor.Test/WebClientDownloaderBuilderTest.cs
@@ -767,7 +767,7 @@ public partial class WebClientDownloaderBuilderTest
         logger.AssertDebugLogged($"""
             The remote server certificate is not trusted by the operating system. The scanner is checking the certificate against the certificates provided by the file '{trustStoreFile.FileName}' (specified via the sonar.scanner.truststorePath parameter or its default value).
             """);
-        // The warning is either WARN_TrustStore_Chain_Invalid or WARN_TrustStore_OtherChainStatus
+        // The warning is either WARN_TrustStore_Chain_Invalid or WARN_TrustStore_OtherChainStatus depending on the environment.
         logger.Warnings.Should().HaveCount(1);
     }
 
