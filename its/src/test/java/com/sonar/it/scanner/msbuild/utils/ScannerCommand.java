4b4926434fb031c4a596316e3e5557db48d93445
Pavel Mikula <pavel.mikula@sonarsource.com>
2025-03-13 18:25:19 +0100

    TF_BUILD reset

--- its/src/test/java/com/sonar/it/scanner/msbuild/utils/ScannerCommand.java
+++ its/src/test/java/com/sonar/it/scanner/msbuild/utils/ScannerCommand.java
@@ -61,6 +61,7 @@ public class ScannerCommand {
     // Because the QA runs in AZD, the surrounding environment makes S4NET think it's inside normal AZD run.
     // Therefore it is picking up paths to .sonarqube folder from C:\sonar-ci\_work\1\.sonarqube\conf\SonarQubeAnalysisConfig.xml
     // This can be removed once we move our CI out of Azure DevOps.
+    setEnvironmentVariable("TF_BUILD", "");
     setEnvironmentVariable("AGENT_BUILDDIRECTORY", "");
     setEnvironmentVariable("BUILD_SOURCESDIRECTORY", "");
   }
