72940d99fe8934ef15db39fdeeca708320148efd
Pavel Mikula <pavel.mikula@sonarsource.com>
2025-03-19 12:10:42 +0100

    Run only incremental

--- its/src/test/java/com/sonar/it/scanner/msbuild/sonarcloud/JreProvisioningTest.java
+++ its/src/test/java/com/sonar/it/scanner/msbuild/sonarcloud/JreProvisioningTest.java
@@ -19,20 +19,10 @@
  */
 package com.sonar.it.scanner.msbuild.sonarcloud;
 
-import com.sonar.it.scanner.msbuild.utils.TestUtils;
-import com.sonar.orchestrator.util.Command;
-import com.sonar.orchestrator.util.CommandExecutor;
-import com.sonar.orchestrator.util.StreamConsumer;
-import java.io.File;
-import java.io.IOException;
-import java.io.StringWriter;
 import java.nio.file.Path;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 class JreProvisioningTest {
   private static final Logger LOG = LoggerFactory.getLogger(JreProvisioningTest.class);
@@ -41,133 +31,133 @@ class JreProvisioningTest {
 
   @TempDir
   public Path basePath;
-
-  @Test
-  void different_hostUrl_sonarcloudUrl_logsAndExitsEarly() {
-    var logWriter = new StringWriter();
-    StreamConsumer.Pipe logsConsumer = new StreamConsumer.Pipe(logWriter);
-
-    var beginCommand = Command.create(new File(Constants.SCANNER_PATH).getAbsolutePath())
-      .addArgument("begin")
-      .addArgument("/o:org")
-      .addArgument("/k:project")
-      .addArgument("/d:sonar.host.url=http://localhost:4242")
-      .addArgument("/d:sonar.scanner.sonarcloudUrl=" + Constants.SONARCLOUD_URL);
-
-    LOG.info("Scanner path: {}", Constants.SCANNER_PATH);
-    LOG.info("Command line: {}", beginCommand.toCommandLine());
-    var beginResult = CommandExecutor.create().execute(beginCommand, logsConsumer, Constants.COMMAND_TIMEOUT);
-    assertThat(beginResult).isOne();
-
-    assertThat(logWriter.toString()).contains(
-      "The arguments 'sonar.host.url' and 'sonar.scanner.sonarcloudUrl' are both set and are different. Please set either 'sonar.host" +
-        ".url' for SonarQube or 'sonar.scanner.sonarcloudUrl' for SonarCloud.");
-  }
-
-  @Test
-  void jreProvisioning_skipProvisioning_doesNotDownloadJre() throws IOException {
-    var projectDir = TestUtils.projectDir(basePath, PROJECT_NAME);
-    var logWriter = new StringWriter();
-    StreamConsumer.Pipe logsConsumer = new StreamConsumer.Pipe(logWriter);
-
-    SonarCloudUtils.runBeginStep(projectDir, SONARCLOUD_PROJECT_KEY, logsConsumer, "/d:sonar.scanner.skipJreProvisioning=true");
-
-    assertThat(logWriter.toString()).contains(
-      "JreResolver: Resolving JRE path.",
-      "JreResolver: sonar.scanner.skipJreProvisioning is set, skipping JRE provisioning.");
-    assertThat(logWriter.toString()).doesNotContain(
-      "JreResolver: Cache miss.",
-      "JreResolver: Cache hit",
-      "JreResolver: Cache failure.");
-  }
-
-  @Test
-  void jreProvisioning_endToEnd_cacheMiss_downloadsJre() throws IOException {
-    var projectDir = TestUtils.projectDir(basePath, PROJECT_NAME);
-
-    var logs = SonarCloudUtils.runAnalysis(projectDir, SONARCLOUD_PROJECT_KEY, "/d:sonar.userHome=" + projectDir.toAbsolutePath());
-
-    var root = projectDir.toAbsolutePath().toString().replace("\\", "\\\\");
-    // begin step
-    assertThat(logs).contains(
-      "JreResolver: Resolving JRE path.",
-      "Downloading from " + Constants.SONARCLOUD_API_URL + "/analysis/jres?os=windows&arch=x64...",
-      "Response received from " + Constants.SONARCLOUD_API_URL + "/analysis/jres?os=windows&arch=x64...",
-      "JreResolver: Cache miss. Attempting to download JRE.",
-      "Starting the Java Runtime Environment download.");
-    TestUtils.matchesSingleLine(logs, "Downloading Java JRE from https://.+/jres/.+.zip");
-    TestUtils.matchesSingleLine(logs, "The checksum of the downloaded file is '.+' and the expected checksum is '.+'");
-    TestUtils.matchesSingleLine(logs,
-      "Starting extracting the Java runtime environment from archive '" + root + "\\\\cache.+' to folder '" + root +
-        "\\\\cache.+'");
-    TestUtils.matchesSingleLine(logs,
-      "Moving extracted Java runtime environment from '" + root + "\\\\cache.+' to '" + root + "\\\\cache" +
-        ".+_extracted'");
-    TestUtils.matchesSingleLine(logs, "The Java runtime environment was successfully added to '" + root + "\\\\cache.+_extracted'");
-    TestUtils.matchesSingleLine(logs, "JreResolver: Download success. JRE can be found at '" + root + "\\\\cache.+_extracted.+java.exe'");
-    // end step
-    TestUtils.matchesSingleLine(logs, "Setting the JAVA_HOME for the scanner cli to " + root + "\\\\cache.+_extracted.+");
-    TestUtils.matchesSingleLine(logs, "Overwriting the value of environment variable 'JAVA_HOME'. Old value: .+, new value: " + root +
-      "\\\\cache.+extracted.+");
-  }
-
-  @Test
-  void jreProvisioning_endToEnd_cacheHit_reusesJre() throws IOException {
-    var projectDir = TestUtils.projectDir(basePath, PROJECT_NAME);
-    var root = projectDir.toAbsolutePath().toString().replace("\\", "\\\\");
-    var extraParameters = "/d:sonar.userHome=" + projectDir.toAbsolutePath();
-
-    // first analysis, cache misses and downloads the JRE
-    var cacheMissLogs = SonarCloudUtils.runAnalysis(projectDir, SONARCLOUD_PROJECT_KEY, extraParameters);
-    assertThat(cacheMissLogs).contains(
-      "JreResolver: Cache miss",
-      "Starting the Java Runtime Environment download.");
-    assertThat(cacheMissLogs).doesNotContain(
-      "JreResolver: Cache hit",
-      "JreResolver: Cache failure");
-
-    // second analysis, cache hits and does not download the JRE
-    var cacheHitLogs = SonarCloudUtils.runAnalysis(projectDir, SONARCLOUD_PROJECT_KEY, extraParameters);
-    TestUtils.matchesSingleLine(cacheHitLogs,
-      "JreResolver: Cache hit '" + root + "\\\\cache.+_extracted.+java.exe'");
-    assertThat(cacheHitLogs).doesNotContain(
-      "JreResolver: Cache miss",
-      "Starting the Java Runtime Environment download.");
-  }
-
-  @Test
-  void jreProvisioning_endToEnd_parameters_propagated() throws IOException {
-    var projectDir = TestUtils.projectDir(basePath, PROJECT_NAME);
-
-    SonarCloudUtils.runBeginStep(
-      projectDir,
-      SONARCLOUD_PROJECT_KEY,
-      new StreamConsumer.Pipe(new StringWriter()),
-      "/d:sonar.scanner.os=windows",
-      "/d:sonar.scanner.arch=x64",
-      "/d:sonar.scanner.skipJreProvisioning=true",
-      "/d:sonar.scanner.connectTimeout=42",
-      "/d:sonar.scanner.socketTimeout=100",
-      "/d:sonar.scanner.responseTimeout=500",
-      "/d:sonar.userHome=" + projectDir.toAbsolutePath());
-
-    SonarCloudUtils.runBuild(projectDir);
-
-    var logWriter = new StringWriter();
-    StreamConsumer.Pipe logConsumer = new StreamConsumer.Pipe(logWriter);
-    SonarCloudUtils.runEndStep(projectDir, logConsumer);
-
-    var logs = logWriter.toString();
-    assertThat(logs).contains(
-      "Dumping content of sonar-project.properties",
-      "sonar.scanner.sonarcloudUrl=" + Constants.SONARCLOUD_URL,
-      "sonar.scanner.apiBaseUrl=" + Constants.SONARCLOUD_API_URL,
-      "sonar.scanner.os=windows",
-      "sonar.scanner.arch=x64",
-      "sonar.scanner.skipJreProvisioning=true",
-      "sonar.scanner.connectTimeout=42",
-      "sonar.scanner.socketTimeout=100",
-      "sonar.scanner.responseTimeout=500",
-      "sonar.userHome=" + projectDir.toAbsolutePath().toString().replace("\\", "\\\\"));
-  }
+//
+//  @Test
+//  void different_hostUrl_sonarcloudUrl_logsAndExitsEarly() {
+//    var logWriter = new StringWriter();
+//    StreamConsumer.Pipe logsConsumer = new StreamConsumer.Pipe(logWriter);
+//
+//    var beginCommand = Command.create(new File(Constants.SCANNER_PATH).getAbsolutePath())
+//      .addArgument("begin")
+//      .addArgument("/o:org")
+//      .addArgument("/k:project")
+//      .addArgument("/d:sonar.host.url=http://localhost:4242")
+//      .addArgument("/d:sonar.scanner.sonarcloudUrl=" + Constants.SONARCLOUD_URL);
+//
+//    LOG.info("Scanner path: {}", Constants.SCANNER_PATH);
+//    LOG.info("Command line: {}", beginCommand.toCommandLine());
+//    var beginResult = CommandExecutor.create().execute(beginCommand, logsConsumer, Constants.COMMAND_TIMEOUT);
+//    assertThat(beginResult).isOne();
+//
+//    assertThat(logWriter.toString()).contains(
+//      "The arguments 'sonar.host.url' and 'sonar.scanner.sonarcloudUrl' are both set and are different. Please set either 'sonar.host" +
+//        ".url' for SonarQube or 'sonar.scanner.sonarcloudUrl' for SonarCloud.");
+//  }
+//
+//  @Test
+//  void jreProvisioning_skipProvisioning_doesNotDownloadJre() throws IOException {
+//    var projectDir = TestUtils.projectDir(basePath, PROJECT_NAME);
+//    var logWriter = new StringWriter();
+//    StreamConsumer.Pipe logsConsumer = new StreamConsumer.Pipe(logWriter);
+//
+//    SonarCloudUtils.runBeginStep(projectDir, SONARCLOUD_PROJECT_KEY, logsConsumer, "/d:sonar.scanner.skipJreProvisioning=true");
+//
+//    assertThat(logWriter.toString()).contains(
+//      "JreResolver: Resolving JRE path.",
+//      "JreResolver: sonar.scanner.skipJreProvisioning is set, skipping JRE provisioning.");
+//    assertThat(logWriter.toString()).doesNotContain(
+//      "JreResolver: Cache miss.",
+//      "JreResolver: Cache hit",
+//      "JreResolver: Cache failure.");
+//  }
+//
+//  @Test
+//  void jreProvisioning_endToEnd_cacheMiss_downloadsJre() throws IOException {
+//    var projectDir = TestUtils.projectDir(basePath, PROJECT_NAME);
+//
+//    var logs = SonarCloudUtils.runAnalysis(projectDir, SONARCLOUD_PROJECT_KEY, "/d:sonar.userHome=" + projectDir.toAbsolutePath());
+//
+//    var root = projectDir.toAbsolutePath().toString().replace("\\", "\\\\");
+//    // begin step
+//    assertThat(logs).contains(
+//      "JreResolver: Resolving JRE path.",
+//      "Downloading from " + Constants.SONARCLOUD_API_URL + "/analysis/jres?os=windows&arch=x64...",
+//      "Response received from " + Constants.SONARCLOUD_API_URL + "/analysis/jres?os=windows&arch=x64...",
+//      "JreResolver: Cache miss. Attempting to download JRE.",
+//      "Starting the Java Runtime Environment download.");
+//    TestUtils.matchesSingleLine(logs, "Downloading Java JRE from https://.+/jres/.+.zip");
+//    TestUtils.matchesSingleLine(logs, "The checksum of the downloaded file is '.+' and the expected checksum is '.+'");
+//    TestUtils.matchesSingleLine(logs,
+//      "Starting extracting the Java runtime environment from archive '" + root + "\\\\cache.+' to folder '" + root +
+//        "\\\\cache.+'");
+//    TestUtils.matchesSingleLine(logs,
+//      "Moving extracted Java runtime environment from '" + root + "\\\\cache.+' to '" + root + "\\\\cache" +
+//        ".+_extracted'");
+//    TestUtils.matchesSingleLine(logs, "The Java runtime environment was successfully added to '" + root + "\\\\cache.+_extracted'");
+//    TestUtils.matchesSingleLine(logs, "JreResolver: Download success. JRE can be found at '" + root + "\\\\cache.+_extracted.+java.exe'");
+//    // end step
+//    TestUtils.matchesSingleLine(logs, "Setting the JAVA_HOME for the scanner cli to " + root + "\\\\cache.+_extracted.+");
+//    TestUtils.matchesSingleLine(logs, "Overwriting the value of environment variable 'JAVA_HOME'. Old value: .+, new value: " + root +
+//      "\\\\cache.+extracted.+");
+//  }
+//
+//  @Test
+//  void jreProvisioning_endToEnd_cacheHit_reusesJre() throws IOException {
+//    var projectDir = TestUtils.projectDir(basePath, PROJECT_NAME);
+//    var root = projectDir.toAbsolutePath().toString().replace("\\", "\\\\");
+//    var extraParameters = "/d:sonar.userHome=" + projectDir.toAbsolutePath();
+//
+//    // first analysis, cache misses and downloads the JRE
+//    var cacheMissLogs = SonarCloudUtils.runAnalysis(projectDir, SONARCLOUD_PROJECT_KEY, extraParameters);
+//    assertThat(cacheMissLogs).contains(
+//      "JreResolver: Cache miss",
+//      "Starting the Java Runtime Environment download.");
+//    assertThat(cacheMissLogs).doesNotContain(
+//      "JreResolver: Cache hit",
+//      "JreResolver: Cache failure");
+//
+//    // second analysis, cache hits and does not download the JRE
+//    var cacheHitLogs = SonarCloudUtils.runAnalysis(projectDir, SONARCLOUD_PROJECT_KEY, extraParameters);
+//    TestUtils.matchesSingleLine(cacheHitLogs,
+//      "JreResolver: Cache hit '" + root + "\\\\cache.+_extracted.+java.exe'");
+//    assertThat(cacheHitLogs).doesNotContain(
+//      "JreResolver: Cache miss",
+//      "Starting the Java Runtime Environment download.");
+//  }
+//
+//  @Test
+//  void jreProvisioning_endToEnd_parameters_propagated() throws IOException {
+//    var projectDir = TestUtils.projectDir(basePath, PROJECT_NAME);
+//
+//    SonarCloudUtils.runBeginStep(
+//      projectDir,
+//      SONARCLOUD_PROJECT_KEY,
+//      new StreamConsumer.Pipe(new StringWriter()),
+//      "/d:sonar.scanner.os=windows",
+//      "/d:sonar.scanner.arch=x64",
+//      "/d:sonar.scanner.skipJreProvisioning=true",
+//      "/d:sonar.scanner.connectTimeout=42",
+//      "/d:sonar.scanner.socketTimeout=100",
+//      "/d:sonar.scanner.responseTimeout=500",
+//      "/d:sonar.userHome=" + projectDir.toAbsolutePath());
+//
+//    SonarCloudUtils.runBuild(projectDir);
+//
+//    var logWriter = new StringWriter();
+//    StreamConsumer.Pipe logConsumer = new StreamConsumer.Pipe(logWriter);
+//    SonarCloudUtils.runEndStep(projectDir, logConsumer);
+//
+//    var logs = logWriter.toString();
+//    assertThat(logs).contains(
+//      "Dumping content of sonar-project.properties",
+//      "sonar.scanner.sonarcloudUrl=" + Constants.SONARCLOUD_URL,
+//      "sonar.scanner.apiBaseUrl=" + Constants.SONARCLOUD_API_URL,
+//      "sonar.scanner.os=windows",
+//      "sonar.scanner.arch=x64",
+//      "sonar.scanner.skipJreProvisioning=true",
+//      "sonar.scanner.connectTimeout=42",
+//      "sonar.scanner.socketTimeout=100",
+//      "sonar.scanner.responseTimeout=500",
+//      "sonar.userHome=" + projectDir.toAbsolutePath().toString().replace("\\", "\\\\"));
+//  }
 }
--- its/src/test/java/com/sonar/it/scanner/msbuild/sonarcloud/RegionTest.java
+++ its/src/test/java/com/sonar/it/scanner/msbuild/sonarcloud/RegionTest.java
@@ -19,20 +19,10 @@
  */
 package com.sonar.it.scanner.msbuild.sonarcloud;
 
-import com.sonar.it.scanner.msbuild.utils.TestUtils;
-import com.sonar.orchestrator.util.Command;
-import com.sonar.orchestrator.util.CommandExecutor;
-import com.sonar.orchestrator.util.StreamConsumer;
-import java.io.File;
-import java.io.IOException;
-import java.io.StringWriter;
 import java.nio.file.Path;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 public class RegionTest {
   private static final Logger LOG = LoggerFactory.getLogger(RegionTest.class);
@@ -41,33 +31,33 @@ public class RegionTest {
 
   @TempDir
   public Path basePath;
-
-  @Test
-  void region_us() throws IOException {
-    var projectDir = TestUtils.projectDir(basePath, PROJECT_NAME);
-    var logWriter = new StringWriter();
-    StreamConsumer.Pipe logsConsumer = new StreamConsumer.Pipe(logWriter);
-
-    var beginCommand = Command.create(new File(Constants.SCANNER_PATH).getAbsolutePath())
-      .setDirectory(projectDir.toFile())
-      .addArgument("begin")
-      .addArgument("/o:" + Constants.SONARCLOUD_ORGANIZATION)
-      .addArgument("/k:" + SONARCLOUD_PROJECT_KEY)
-      .addArgument("/d:sonar.region=us")
-      .addArgument("/d:sonar.verbose=true");
-
-    var beginResult = CommandExecutor.create().execute(beginCommand, logsConsumer, Constants.COMMAND_TIMEOUT);
-    assertThat(beginResult).isOne(); // Indicates error
-    assertThat(logWriter.toString()).containsSubsequence(
-      "Server Url: https://sonarqube.us",
-      "Api Url: https://api.sonarqube.us",
-      "Is SonarCloud: True",
-      "Downloading from https://sonarqube.us/api/settings/values?component=unknown",
-      "Downloading from https://api.sonarqube.us/analysis/version",
-      "Using SonarCloud.",
-      "JreResolver: Resolving JRE path.",
-      "Downloading from https://sonarqube.us/api/settings/values?component=team-lang-dotnet_region-parameter...",
-      "Cannot download quality profile. Check scanner arguments and the reported URL for more information.",
-      "Pre-processing failed. Exit code: 1");
-  }
+//
+//  @Test
+//  void region_us() throws IOException {
+//    var projectDir = TestUtils.projectDir(basePath, PROJECT_NAME);
+//    var logWriter = new StringWriter();
+//    StreamConsumer.Pipe logsConsumer = new StreamConsumer.Pipe(logWriter);
+//
+//    var beginCommand = Command.create(new File(Constants.SCANNER_PATH).getAbsolutePath())
+//      .setDirectory(projectDir.toFile())
+//      .addArgument("begin")
+//      .addArgument("/o:" + Constants.SONARCLOUD_ORGANIZATION)
+//      .addArgument("/k:" + SONARCLOUD_PROJECT_KEY)
+//      .addArgument("/d:sonar.region=us")
+//      .addArgument("/d:sonar.verbose=true");
+//
+//    var beginResult = CommandExecutor.create().execute(beginCommand, logsConsumer, Constants.COMMAND_TIMEOUT);
+//    assertThat(beginResult).isOne(); // Indicates error
+//    assertThat(logWriter.toString()).containsSubsequence(
+//      "Server Url: https://sonarqube.us",
+//      "Api Url: https://api.sonarqube.us",
+//      "Is SonarCloud: True",
+//      "Downloading from https://sonarqube.us/api/settings/values?component=unknown",
+//      "Downloading from https://api.sonarqube.us/analysis/version",
+//      "Using SonarCloud.",
+//      "JreResolver: Resolving JRE path.",
+//      "Downloading from https://sonarqube.us/api/settings/values?component=team-lang-dotnet_region-parameter...",
+//      "Cannot download quality profile. Check scanner arguments and the reported URL for more information.",
+//      "Pre-processing failed. Exit code: 1");
+//  }
 }
