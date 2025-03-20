1053bf920b54b4bb344dccd3dbbe41b0612fa3aa
Martin Strecker <martin.strecker@sonarsource.com>
2025-03-20 20:59:46 +0100

    Remove unrelated issues from search result including component

--- its/src/test/java/com/sonar/it/scanner/msbuild/utils/TestUtils.java
+++ its/src/test/java/com/sonar/it/scanner/msbuild/utils/TestUtils.java
@@ -45,6 +45,7 @@ import javax.annotation.Nullable;
 import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.sonar.api.internal.apachecommons.lang3.StringUtils;
 import org.sonarqube.ws.Ce;
 import org.sonarqube.ws.Components;
 import org.sonarqube.ws.Issues;
@@ -389,7 +390,7 @@ public class TestUtils {
       results.addAll(issues.getIssuesList());
       page++;
     } while (results.size() < issues.getPaging().getTotal());
-    results.removeIf(x -> !x.getProject().equals(projectKey));
+    results.removeIf(x -> !StringUtils.equalsAny(projectKey, x.getProject(), x.getComponent()));
     return results;
   }
 
