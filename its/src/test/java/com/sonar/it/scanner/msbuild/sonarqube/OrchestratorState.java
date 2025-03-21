2730643dd6cafddfefd42dd2aff03daad7b39dd7
Pavel Mikula <57188685+pavel-mikula-sonarsource@users.noreply.github.com>
2025-03-21 14:49:01 +0100

    SCAN4NET-344 Improve OrchestratorState startup (#2395)

--- its/src/test/java/com/sonar/it/scanner/msbuild/sonarqube/OrchestratorState.java
+++ its/src/test/java/com/sonar/it/scanner/msbuild/sonarqube/OrchestratorState.java
@@ -32,6 +32,7 @@ public class OrchestratorState {
 
   private final Orchestrator orchestrator;
   private volatile int usageCount;
+  private volatile boolean isStarted;
 
   public OrchestratorState(Orchestrator orchestrator) {
     this.orchestrator = orchestrator;
@@ -45,6 +46,9 @@ public class OrchestratorState {
         TestUtils.getNewToken(orchestrator);  // ToDo: SCAN4NET-293 This is an ugly tangle that should be fixed later
         // To avoid a race condition in scanner file cache mechanism we analyze single project before any test to populate the cache
         analyzeEmptyProject();
+        isStarted = true;
+      } else if (!isStarted) {  // The second, third and any other caller should fail fast if something went wrong for the first one
+        throw new IllegalStateException("Previous OrchestratorState startup failed");
       }
     }
   }
@@ -54,6 +58,7 @@ public class OrchestratorState {
       usageCount -= 1;
       if (usageCount == 0) {
         orchestrator.stop();
+        isStarted = false;
       }
     }
   }
