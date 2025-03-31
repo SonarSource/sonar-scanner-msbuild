d7ef2ea3bee75b57434a6bd1b8347c051b4148ee
Pavel Mikula <57188685+pavel-mikula-sonarsource@users.noreply.github.com>
2025-03-31 13:31:45 +0200

    SCAN4NET-371 Use AnalysisContext in MultiLanguage tests (#2438)

--- its/src/test/java/com/sonar/it/scanner/msbuild/sonarcloud/CloudConstants.java
+++ its/src/test/java/com/sonar/it/scanner/msbuild/sonarcloud/CloudConstants.java
@@ -20,8 +20,6 @@
 package com.sonar.it.scanner.msbuild.sonarcloud;
 
 public class CloudConstants {
-  public static final Integer COMMAND_TIMEOUT = 2 * 60 * 1000;
-
   public static final String SONARCLOUD_ORGANIZATION = System.getenv("SONARCLOUD_ORGANIZATION");
   public static final String SONARCLOUD_URL = System.getenv("SONARCLOUD_URL");
   public static final String SONARCLOUD_API_URL = System.getenv("SONARCLOUD_API_URL");
