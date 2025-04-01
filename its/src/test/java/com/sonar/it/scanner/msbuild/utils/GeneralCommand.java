12e2c49218ee4bd8f39ee71b379858bad5f8fc1c
Martin Strecker <martin.strecker@sonarsource.com>
2025-04-01 10:27:03 +0200

    Hide Command

--- its/src/test/java/com/sonar/it/scanner/msbuild/utils/GeneralCommand.java
+++ its/src/test/java/com/sonar/it/scanner/msbuild/utils/GeneralCommand.java
@@ -23,26 +23,37 @@ import com.sonar.orchestrator.build.BuildResult;
 import com.sonar.orchestrator.util.Command;
 import com.sonar.orchestrator.util.CommandExecutor;
 import com.sonar.orchestrator.util.StreamConsumer;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import static com.sonar.it.scanner.msbuild.utils.TestUtils.LOG;
 import static com.sonar.it.scanner.msbuild.utils.TestUtils.TIMEOUT_LIMIT;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class GeneralCommand extends BaseCommand<GeneralCommand> {
   private long timeout = TIMEOUT_LIMIT;
   public final Command command;
 
-  private GeneralCommand(Command command) {
-    super(Paths.get(""));
-    this.command = command;
+  private GeneralCommand(Path workingDirectory, String command) {
+    super(workingDirectory);
+    this.command = Command.create(command);
   }
 
-  public static GeneralCommand create(Command command) {
-    return new GeneralCommand(command);
+  public static GeneralCommand create(String command) {
+    return new GeneralCommand(Paths.get(""), command);
+  }
+
+  public static GeneralCommand create(String command, Path workingDirectory) {
+    return new GeneralCommand(workingDirectory, command);
   }
 
   public GeneralCommand setTimeout(long timeout) {
     this.timeout = timeout;
+    return this;
+  }
+
+  public GeneralCommand addArgument(String arg) {
+    command.addArgument(arg);
     return this;
   }
 
@@ -52,13 +63,7 @@ public class GeneralCommand extends BaseCommand<GeneralCommand> {
     var result = new BuildResult();
     var returnCode = CommandExecutor.create().execute(command, new StreamConsumer.Pipe(result.getLogsWriter()), timeout);
     result.addStatus(returnCode);
-    if (returnCode == 0)
-    {
-      LOG.info("Command line success: {}", commandLine);
-    } else
-    {
-      LOG.error("Command line failure: '{}'\nReturn code: {}\nLogs:{}", commandLine, returnCode, result.getLogs());
-    }
+    assertThat(result.isSuccess()).describedAs("Command '" + commandLine + "' failed with logs: " + result.getLogs()).isTrue();
     return result;
   }
 
--- its/src/test/java/com/sonar/it/scanner/msbuild/utils/TestUtils.java
+++ its/src/test/java/com/sonar/it/scanner/msbuild/utils/TestUtils.java
@@ -151,18 +151,17 @@ public class TestUtils {
 
   public static void createVirtualDrive(String drive, Path projectDir, String subDirectory) {
     var target = projectDir.resolve(subDirectory).toAbsolutePath().toString();
-    var result = GeneralCommand.create(Command.create("SUBST")
-        .addArguments(drive)
-        .addArguments(target)
-        .setDirectory(projectDir.toFile()))
+    var result = GeneralCommand.create("SUBST", projectDir)
+      .addArgument(drive)
+      .addArgument(target)
       .execute();
     assertThat(result.isSuccess()).withFailMessage("SUBST create failed. Most likely flakiness from a previous canceled run that did not clean-up the drive. " +
       "The mapping of drive %s to folder '%s' failed with logs:%n%s", drive, target, result.getLogs()).isTrue();
   }
 
   public static void deleteVirtualDrive(String drive) {
-    var result = GeneralCommand.create(Command.create("SUBST")
-        .addArguments(drive).addArguments("/D"))
+    var result = GeneralCommand.create("SUBST")
+      .addArgument(drive).addArgument("/D")
       .execute();
     assertThat(result.isSuccess()).withFailMessage("SUBST delete failed for drive %s with logs:%n%s", drive, result.getLogs()).isTrue();
   }
