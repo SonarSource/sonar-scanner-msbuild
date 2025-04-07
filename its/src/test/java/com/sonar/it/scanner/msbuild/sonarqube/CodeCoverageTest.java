fa0203250375e9969dafb331c659376442f2bd7d
mary-georgiou-sonarsource <89914005+mary-georgiou-sonarsource@users.noreply.github.com>
2025-04-07 15:53:53 +0200

    remove net framework from coverage ITs

--- its/src/test/java/com/sonar/it/scanner/msbuild/sonarqube/CodeCoverageTest.java
+++ its/src/test/java/com/sonar/it/scanner/msbuild/sonarqube/CodeCoverageTest.java
@@ -154,7 +154,7 @@ class CodeCoverageTest {
   }
 
   private AnalysisContext createContextWithCoverage(TempDirectory buildDirectory) {
-    var context = AnalysisContext.forServer("CodeCoverage", ScannerClassifier.NET_FRAMEWORK);
+    var context = AnalysisContext.forServer("CodeCoverage");
     context.begin.setDebugLogs(); // For assertions
     // --collect "Code Coverage" parameter produces a binary coverage file ".coverage" that needs to be converted to an XML ".coveragexml" file by the end step
     context.build.useDotNet("test").addArgument("--collect", "Code Coverage", "--logger", "trx", "--results-directory", buildDirectory + "\\TestResults");
