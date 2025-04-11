b29e7a19f8ab588df4657fa548c4f2b853ccde27
Sebastien Marichal <sebastien.marichal@sonarsource.com>
2025-04-11 15:00:26 +0200

    Cleanup

--- its/src/test/java/com/sonar/it/scanner/msbuild/sonarqube/AzureTest.java
+++ its/src/test/java/com/sonar/it/scanner/msbuild/sonarqube/AzureTest.java
@@ -21,9 +21,7 @@ package com.sonar.it.scanner.msbuild.sonarqube;
 
 import com.sonar.it.scanner.msbuild.utils.AnalysisContext;
 import com.sonar.it.scanner.msbuild.utils.AzureDevOps;
-import com.sonar.it.scanner.msbuild.utils.BuildCommand;
 import com.sonar.it.scanner.msbuild.utils.ContextExtension;
-import com.sonar.it.scanner.msbuild.utils.GeneralCommand;
 import com.sonar.it.scanner.msbuild.utils.OSPlatform;
 import com.sonar.it.scanner.msbuild.utils.ScannerCommand;
 import com.sonar.it.scanner.msbuild.utils.Timeout;
@@ -31,20 +29,14 @@ import com.sonar.orchestrator.Orchestrator;
 import com.sonar.orchestrator.util.Command;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
-import org.apache.commons.exec.DefaultExecutor;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import static com.sonar.it.scanner.msbuild.sonarqube.ServerTests.ORCHESTRATOR;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -53,8 +45,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith({ServerTests.class, ContextExtension.class})
 class AzureTest {
-  private static final Logger LOG = LoggerFactory.getLogger(AzureTest.class);
-
   @ParameterizedTest
   @ValueSource(strings = {"TF_Build", "tf_build", "tf_BUILD"})
   void AzureEnvVariables_WrongCase_FailInUnix_SucceedsInWindows(String tfBuild) throws IOException {
@@ -64,9 +54,6 @@ class AzureTest {
     assertThat(Files.exists(sonarConfigFile)).isTrue();
     var content = Files.readString(sonarConfigFile);
     if (OSPlatform.isWindows()) {
-      LOG.info("############## ENVIRONMENT VARIABLES ##############");
-      new GeneralCommand("set", Path.of(".")).execute();
-      LOG.info("############ END ENVIRONMENT VARIABLES ############");
       assertThat(content).contains("<SourcesDirectory>" + sourceDir + "</SourcesDirectory>");
     } else {
       assertThat(content).doesNotMatch("<SourcesDirectory>.+</SourcesDirectory>");
@@ -99,7 +86,7 @@ class AzureTest {
     try {
       var method = ScannerCommand.class.getDeclaredMethod("createCommand", Orchestrator.class);
       method.setAccessible(true);
-      var beginCommand = (Command)method.invoke(context.begin, ORCHESTRATOR);
+      var beginCommand = (Command) method.invoke(context.begin, ORCHESTRATOR);
 
       method = Command.class.getDeclaredMethod("toStrings");
       method.setAccessible(true);
@@ -119,8 +106,8 @@ class AzureTest {
 
     try {
       var process = beginProcess.start();
-      boolean finished = process.waitFor(Timeout.TWO_MINUTES.miliseconds, TimeUnit.MILLISECONDS);
-      assertTrue(finished, "Process did not finish in time");
+      boolean finished = process.waitFor(Timeout.ONE_MINUTE.miliseconds, TimeUnit.MILLISECONDS);
+      assertTrue(finished, "Begin step timeout");
     } catch (IOException | InterruptedException e) {
       throw new RuntimeException(e);
     }
