40cec0e0b8cd2467c8ec23cb09dcf45a3412ec3d
Martin Strecker <martin.strecker@sonarsource.com>
2025-05-27 10:44:06 +0200

    Simplify and just follow ECMA 404

--- src/SonarScanner.MSBuild.Tasks/HttpUtility.cs
+++ src/SonarScanner.MSBuild.Tasks/HttpUtility.cs
@@ -24,63 +24,35 @@ namespace SonarScanner.MSBuild.Tasks;
 
 internal static class HttpUtility
 {
-    // Based in parts on the implementation in the mono-project
-    // https://github.com/mono/mono/blob/0f53e9e151d92944cacab3e24ac359410c606df6/mcs/class/System.Web/System.Web/HttpUtility.cs#L528
-    // Adopted to comply with ecma-404
+    // ecma-404 conformant JSON string encoder
     // https://ecma-international.org/publications-and-standards/standards/ecma-404/
     public static string JavaScriptStringEncode(string value)
     {
-        if (string.IsNullOrEmpty(value))
+        if (value is null)
         {
-            return "\"\"";
+            return "null"; // JSON null literal
         }
 
-        var len = value.Length;
-        var needEncode = false;
-        char c;
-        for (var i = 0; i < len; i++)
-        {
-            c = value[i];
+        var sb = new StringBuilder(value.Length + 2);
+        sb.Append('\"');
 
-            if ((c >= 0 && c <= 0x1F) || c == '"' || c == '\\')
+        foreach (var c in value)
+        {
+            sb.Append(c switch
             {
-                needEncode = true;
-                break;
-            }
+                '\"' => "\\\"",
+                '\\' => "\\\\",
+                '\b' => "\\b",
+                '\f' => "\\f",
+                '\n' => "\\n",
+                '\r' => "\\r",
+                '\t' => "\\t",
+                _ when c < 0x20 => $@"\u{(int)c:X4}",
+                _ => c.ToString()
+            });
         }
 
-        if (!needEncode)
-        {
-            return "\"" + value + "\"";
-        }
-
-        var sb = new StringBuilder();
-        sb.Append('"');
-
-        for (var i = 0; i < len; i++)
-        {
-            c = value[i];
-            if ((c >= 0 && c <= 7) || c == 11 || (c >= 14 && c <= 31))
-            {
-                sb.AppendFormat("\\u{0:x4}", (int)c);
-            }
-            else
-            {
-                sb.Append((int)c switch
-                {
-                    8 => "\\b",
-                    9 => "\\t",
-                    10 => "\\n",
-                    12 => "\\f",
-                    13 => "\\r",
-                    34 => "\\\"",
-                    92 => "\\\\",
-                    _ => c,
-                });
-            }
-        }
-
-        sb.Append('"');
+        sb.Append('\"');
         return sb.ToString();
     }
 }
