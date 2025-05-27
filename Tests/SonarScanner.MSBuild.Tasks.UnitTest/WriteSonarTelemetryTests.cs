9a02c0c9a348f20d479e41b4c49a55213e20ff65
Martin Strecker <martin.strecker@sonarsource.com>
2025-05-27 10:52:03 +0200

    Better test

--- Tests/SonarScanner.MSBuild.Tasks.UnitTest/WriteSonarTelemetryTests.cs
+++ Tests/SonarScanner.MSBuild.Tasks.UnitTest/WriteSonarTelemetryTests.cs
@@ -45,7 +45,7 @@ public class WriteSonarTelemetryTests
                 TelemetryTaskItem("key4", $"""
                     Special value with
                     NewLines, <Html Attribute="Value"> tags </Html>
-                    Special characters {'\t'} {'\b'} {'\f'} " \ {'\u0007'} '
+                    Special characters {'\t'} {'\b'} {'\f'} ' " \ {'\u0007'} {'\u001F'} {'\u0020'}
                     """),
             ]
         };
@@ -59,7 +59,7 @@ public class WriteSonarTelemetryTests
                 """{"key2":"value2"}""",
                 """{"key3":"value3"}""",
                 """{"key3":"duplicate"}""",
-                """{"key4":"Special value with\r\nNewLines, <Html Attribute=\"Value\"> tags </Html>\r\nSpecial characters \t \b \f \" \\ \u0007 '"}""",
+                """{"key4":"Special value with\r\nNewLines, <Html Attribute=\"Value\"> tags </Html>\r\nSpecial characters \t \b \f ' \" \\ \u0007 \u001F  "}""",
             })),
             Encoding.UTF8);
         fileWrapper.DidNotReceive().CreateNewAllLines(Arg.Any<string>(), Arg.Any<IEnumerable<string>>(), Arg.Any<Encoding>());
