b5976b08600e503887cc965439c716f25e4e8123
Martin Strecker <martin.strecker@sonarsource.com>
2025-05-27 14:48:48 +0200

    SelectMany from aggregated arguments

--- src/SonarScanner.MSBuild.Common/Telemetry/TelemetryUtils.cs
+++ src/SonarScanner.MSBuild.Common/Telemetry/TelemetryUtils.cs
@@ -22,17 +22,74 @@ namespace SonarScanner.MSBuild.Common;
 
 public static class TelemetryUtils
 {
-    private static string[] telemetryProperties = [SonarProperties.ScanAllAnalysis];
-
     public static void AddTelemetry(ILogger logger, AggregatePropertiesProvider aggregatedProperties)
     {
-        foreach (var propertyWithProvider in aggregatedProperties.GetAllPropertiesWithProvider().Where(x => !x.Key.ContainsSensitiveData() && telemetryProperties.Contains(x.Key.Id)))
+        foreach (var kvp in aggregatedProperties.GetAllPropertiesWithProvider().SelectMany(SelectMany))
         {
-            logger.AddTelemetryMessage($"dotnetenterprise.s4net.params.{ToTelemetryId(propertyWithProvider.Key.Id)}.value", propertyWithProvider.Key.Value);
-            logger.AddTelemetryMessage($"dotnetenterprise.s4net.params.{ToTelemetryId(propertyWithProvider.Key.Id)}.source", propertyWithProvider.Value.ProviderType.ToString());
+            logger.AddTelemetryMessage(kvp.Key, kvp.Value);
         }
     }
 
+    private static IEnumerable<KeyValuePair<string, string>> SelectMany(KeyValuePair<Property, IAnalysisPropertyProvider> argument)
+    {
+        var property = argument.Key;
+        if (property.ContainsSensitiveData()
+            // Further senstive parameters
+            || property.IsKey(SonarProperties.Organization)
+            || property.IsKey(SonarProperties.ProjectKey)
+            // Should be extracted from ServerInfo
+            || property.IsKey(SonarProperties.HostUrl)
+            || property.IsKey(SonarProperties.ApiBaseUrl)
+            || property.IsKey(SonarProperties.SonarcloudUrl)
+            || property.IsKey(SonarProperties.Region))
+        {
+            return [];
+        }
+        var value = argument.Key.Value;
+        var provider = argument.Value;
+        if ((property.IsKey(SonarProperties.ClientCertPath)
+            || property.IsKey(SonarProperties.TruststorePath)
+            || property.IsKey(SonarProperties.JavaExePath)) && value is { } filePath)
+        {
+            // Don't put the file path in the telemetry. The file extension is indicator enough
+            return MessagePair(provider, property, Path.GetExtension(filePath));
+        }
+        else if ((property.IsKey(SonarProperties.PullRequestCacheBasePath)
+            || property.IsKey(SonarProperties.VsCoverageXmlReportsPaths)
+            || property.IsKey(SonarProperties.VsTestReportsPaths)
+            || property.IsKey(SonarProperties.PluginCacheDirectory)
+            || property.IsKey(SonarProperties.ProjectBaseDir)
+            || property.IsKey(SonarProperties.UserHome)
+            || property.IsKey(SonarProperties.WorkingDirectory)) && value is { } directoryPath)
+        {
+            // Don't write directories to telemetry. Just specify if the path was absolute or relative
+            return MessagePair(provider, property, Path.IsPathRooted(directoryPath) ? "rooted" : "relative");
+        }
+        else if (property.IsKey(SonarProperties.ProjectBranch)
+            || property.IsKey(SonarProperties.ProjectName)
+            || property.IsKey(SonarProperties.ProjectVersion)
+            || property.IsKey(SonarProperties.Sources)
+            || property.IsKey(SonarProperties.Tests))
+        {
+            // Long values or senstive data. Just specify the source where the parameter came from.
+            return [new($"{ToTelemetryId(property.Id)}.source", provider.ProviderType.ToString())];
+        }
+        else
+        {
+            // Default: Write the value specified by the user to the telemetry.
+            return MessagePair(provider, property);
+        }
+    }
+
+    private static IEnumerable<KeyValuePair<string, string>> MessagePair(IAnalysisPropertyProvider source, Property property, string value)
+    {
+        yield return new($"{ToTelemetryId(property.Id)}.value", value);
+        yield return new($"{ToTelemetryId(property.Id)}.source", source.ProviderType.ToString());
+    }
+
+    private static IEnumerable<KeyValuePair<string, string>> MessagePair(IAnalysisPropertyProvider source, Property property) =>
+        MessagePair(source, property, property.Value);
+
     private static string ToTelemetryId(string property) =>
-        property.ToLower().Replace('.', '_');
+        $"dotnetenterprise.s4net.params.{property.ToLower().Replace('.', '_')}";
 }
