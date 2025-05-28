b0d28fd67a9410a7b5ea0c15874926cc80694f5f
Mary Georgiou <89914005+mary-georgiou-sonarsource@users.noreply.github.com>
2025-05-28 16:11:53 +0200

    SCAN4NET-496 Add telemetry IT (#2516)

---
+++ its/projects/Telemetry/Telemetry/Telemetry.cs
@@ -0,0 +1,15 @@
+﻿namespace Telemetry
+{
+    public class Telemetry
+    {
+        public void Method()
+        {
+            // empty body
+        }
+
+        private class InnerClass
+        {
+            public string Name { get; set; } = "InnerClass";
+        }
+    }
+}
