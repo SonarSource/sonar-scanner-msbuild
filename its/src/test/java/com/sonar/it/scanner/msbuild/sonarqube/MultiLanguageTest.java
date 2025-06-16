83a6a9c54165bc2cd256a7f922a8171bc556e460
Tim Pohlmann <tim.pohlmann@sonarsource.com>
2025-06-16 15:08:37 +0200

    SCAN4NET-624 Make MultiLanguageTest.angular more stable (#2559)

--- its/src/test/java/com/sonar/it/scanner/msbuild/sonarqube/MultiLanguageTest.java
+++ its/src/test/java/com/sonar/it/scanner/msbuild/sonarqube/MultiLanguageTest.java
@@ -290,7 +290,7 @@ class MultiLanguageTest {
         tuple("python:S5754", context.projectKey + ":ClientApp/node_modules/flatted/python/flatted.py")
       )
       .size()
-      .isIn(1053, 1210, 1212, 1234); // 8.9 = 1053, 9.9 = 1210, 2025.1 = 1234
+      .isGreaterThan(1000);
 
     assertThat(issues)
       .filteredOn(x -> x.getRule().startsWith("php"))
@@ -299,7 +299,7 @@ class MultiLanguageTest {
         tuple("php:S121", context.projectKey + ":ClientApp/node_modules/flatted/php/flatted.php")
       )
       .size()
-      .isIn(6, 9, 28);
+      .isGreaterThan(5);
 
     if (ORCHESTRATOR.getServer().version().getMajor() == 8) {
       // In version 8.9 css files are handled by a dedicated plugin and node_modules are not filtered in that plugin.
