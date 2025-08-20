a6b5fc61c1d4e8602fc62fcb64d746c57a767a15
Alex Meseldzija <alexander.meseldzija@sonarsource.com>
2025-08-20 14:08:51 +0200

    SCAN4NET-773 Update PostProcessor InternalsVisibleTo with PublicKey when Signed (#2688)

--- src/SonarScanner.MSBuild.PostProcessor/Properties/AssemblyInfo.cs
+++ src/SonarScanner.MSBuild.PostProcessor/Properties/AssemblyInfo.cs
@@ -22,4 +22,8 @@ using System.Runtime.CompilerServices;
 
 [assembly: AssemblyTitle("SonarScanner.MSBuild.PostProcessor")]
 [assembly: AssemblyProduct("SonarScanner.MSBuild.PostProcessor")]
+#if SignAssembly
+[assembly: InternalsVisibleTo("SonarScanner.MSBuild.PostProcessor.Test,PublicKey=002400000480000094000000060200000024000052534131000400000100010081b4345a022cc0f4b42bdc795a5a7a1623c1e58dc2246645d751ad41ba98f2749dc5c4e0da3a9e09febcb2cd5b088a0f041f8ac24b20e736d8ae523061733782f9c4cd75b44f17a63714aced0b29a59cd1ce58d8e10ccdb6012c7098c39871043b7241ac4ab9f6b34f183db716082cd57c1ff648135bece256357ba735e67dc6")]
+#else
 [assembly: InternalsVisibleTo("SonarScanner.MSBuild.PostProcessor.Test")]
+#endif
