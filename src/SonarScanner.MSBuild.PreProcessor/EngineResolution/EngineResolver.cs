249e5957ba49b151eaa526df7fde2215684f5767
Alex Meseldzija <alexander.meseldzija@sonarsource.com>
2025-08-25 09:33:47 +0200

    Download + retry logic

--- src/SonarScanner.MSBuild.PreProcessor/EngineResolution/EngineResolver.cs
+++ src/SonarScanner.MSBuild.PreProcessor/EngineResolution/EngineResolver.cs
@@ -19,28 +19,40 @@
  */
 
 using SonarScanner.MSBuild.PreProcessor.Caching;
+using SonarScanner.MSBuild.PreProcessor.Interfaces;
 
 namespace SonarScanner.MSBuild.PreProcessor.EngineResolution;
 
 public class EngineResolver : IEngineResolver
 {
     private readonly ISonarWebServer server;
-    private readonly CachedDownloader cachedDownloader;
     private readonly ILogger logger;
+    private readonly IChecksum checksum;
+    private readonly string sonarUserHome;
+    private readonly IDirectoryWrapper directoryWrapper;
+    private readonly IFileWrapper fileWrapper;
 
-    public EngineResolver(ISonarWebServer server, CachedDownloader cachedDownloader, ILogger logger)
+    public EngineResolver(ISonarWebServer server,
+                          ILogger logger,
+                          string sonarUserHome,
+                          IChecksum checksum = null,
+                          IDirectoryWrapper directoryWrapper = null,
+                          IFileWrapper fileWrapper = null)
     {
         this.server = server;
-        this.cachedDownloader = cachedDownloader;
         this.logger = logger;
+        this.sonarUserHome = sonarUserHome;
+        this.checksum = checksum ?? ChecksumSha256.Instance;
+        this.directoryWrapper = directoryWrapper ?? DirectoryWrapper.Instance;
+        this.fileWrapper = fileWrapper ?? FileWrapper.Instance;
     }
 
     public async Task<string> ResolveEngine(ProcessedArgs args)
     {
-        if (args.EngineJarPath is { } enginePath)
+        if (args.EngineJarPath is { } localEngine)
         {
-            logger.LogDebug(Resources.MSG_EngineResolver_UsingLocalEngine, enginePath);
-            return enginePath;
+            logger.LogDebug(Resources.MSG_EngineResolver_UsingLocalEngine, localEngine);
+            return localEngine;
         }
         if (!server.SupportsJreProvisioning) // JRE and sonar engine provisioning were introduced by the same version of SQ  S
         {
@@ -52,10 +64,50 @@ public class EngineResolver : IEngineResolver
             logger.LogDebug(Resources.MSG_EngineResolver_MetadataFailure);
             return null;
         }
-        return cachedDownloader.IsFileCached() switch
+
+        if (await ResolveEnginePath(metadata) is { } enginePath)
         {
-            CacheHit hit => hit.FilePath,
-            _ => throw new NotImplementedException("Not yet implemented"),
-        };
+            return enginePath;
+        }
+        else
+        {
+            logger.LogDebug(Resources.MSG_JreResolver_Resolving, " Retrying...");
+            return await ResolveEnginePath(metadata);
+        }
+    }
+
+    public async Task<string> ResolveEnginePath(EngineMetadata metadata)
+    {
+        var cachedDownloader = new CachedDownloader(logger, directoryWrapper, fileWrapper, checksum, metadata.ToDescriptor(), sonarUserHome);
+        switch (cachedDownloader.IsFileCached())
+        {
+            case CacheHit hit:
+                logger.LogDebug(Resources.MSG_JreResolver_CacheHit, hit.FilePath);
+                return hit.FilePath;
+            case CacheMiss:
+                logger.LogDebug(Resources.MSG_JreResolver_CacheMiss);
+                return await DownloadEngine(cachedDownloader, metadata);
+            case CacheError error:
+                logger.LogDebug(Resources.MSG_JreResolver_CacheFailure, error.Message);
+                return null;
+            default:
+                throw new NotSupportedException("File Resolution is expected to be CacheHit, CacheMiss, or CacheError.");
+        }
+    }
+
+    public async Task<string> DownloadEngine(CachedDownloader cachedDownloader, EngineMetadata metadata)
+    {
+        var result = await cachedDownloader.DownloadFileAsync(() => server.DownloadEngineAsync(metadata));
+        if (result is DownloadSuccess success)
+        {
+            logger.LogDebug(Resources.MSG_JreResolver_DownloadSuccess, success.FilePath);
+            return success.FilePath;
+        }
+        else if (result is DownloadError error)
+        {
+            logger.LogDebug(Resources.MSG_JreResolver_DownloadFailure, error.Message);
+            return null;
+        }
+        throw new NotSupportedException("Download result is expected to be DownloadSuccess or DownloadError.");
     }
 }
