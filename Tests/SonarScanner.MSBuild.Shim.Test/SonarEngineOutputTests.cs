f96607a31b992d1b196ca586641fab9934737898
Martin Strecker <103252490+martin-strecker-sonarsource@users.noreply.github.com>
2025-08-27 14:59:54 +0200

    SCAN4NET-817 SonarEngineOutput parser (#2737)

---
+++ Tests/SonarScanner.MSBuild.Shim.Test/SonarEngineOutputTests.cs
@@ -0,0 +1,83 @@
+﻿/*
+ * SonarScanner for .NET
+ * Copyright (C) 2016-2025 SonarSource SA
+ * mailto: info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+namespace SonarScanner.MSBuild.Shim.Test;
+
+[TestClass]
+public class SonarEngineOutputTests
+{
+    [TestMethod]
+    [DataRow("""{"message":"A trace","level":"TRACE"}""", LogLevel.Info, "A trace")]
+    [DataRow("""{"message":"A debug","level":"DEBUG"}""", LogLevel.Info, "A debug")]
+    [DataRow("""{"message":"An info","level":"INFO"}""", LogLevel.Info, "An info")]
+    [DataRow("""{"message":"A warning","level":"WARN"}""", LogLevel.Warning, "A warning")]
+    [DataRow("""{"message":"An error","level":"ERROR"}""", LogLevel.Error, "An error")]
+    [DataRow("""{"MESSAGE":"An error","Level":"ERROR"}""", LogLevel.Error, "An error")]
+    public void OutputToLogMessage_ParsesJsonAndMapsLevel(string json, LogLevel expectedLevel, string expectedMessage)
+    {
+        var result = SonarEngineOutput.OutputToLogMessage(true, json);
+
+        var logMessage = result.Should().NotBeNull().And.BeAssignableTo<LogMessage>().Which;
+        logMessage.Level.Should().Be(expectedLevel);
+        logMessage.Message.Should().Be(expectedMessage);
+    }
+
+    [TestMethod]
+    public void OutputToLogMessage_ParsesStacktrace()
+    {
+        var result = SonarEngineOutput.OutputToLogMessage(true, """{"Message":"Error with stack","Level":"ERROR","Stacktrace":"stacktrace details"}""");
+
+        var logMessage = result.Should().NotBeNull().And.BeAssignableTo<LogMessage>().Which;
+        logMessage.Level.Should().Be(LogLevel.Error);
+        logMessage.Message.Should().Be("""
+            Error with stack
+            stacktrace details
+            """.ToEnvironmentLineEndings());
+    }
+
+    [TestMethod]
+    [DataRow("""not a json""")]
+    [DataRow("""{}""")]
+    [DataRow("""{OtherProp: 1}""")]
+    [DataRow("""{Message: "Message"}""")]
+    [DataRow("""{Level: "WARN"}""")]
+    [DataRow("""{Message: "Message", Level: "UNKNOWN"}""")]
+    [DataRow("""{Message: null, Level: "WARN"}""")]
+    public void OutputToLogMessage_ReturnsInfoOnJsonException(string invalidJson)
+    {
+        var result = SonarEngineOutput.OutputToLogMessage(true, invalidJson);
+
+        var logMessage = result.Should().NotBeNull().And.BeAssignableTo<LogMessage>().Which;
+        logMessage.Level.Should().Be(LogLevel.Info);
+        logMessage.Message.Should().Be(invalidJson);
+    }
+
+    [TestMethod]
+    [DataRow("Just some text")]
+    [DataRow("""{"Message":"Error with stack","Level":"ERROR","Stacktrace":"stacktrace details"}""")]
+    public void OutputToLogMessage_StdErr_ReturnsErrorDoesNotParseJson(string line)
+    {
+        var result = SonarEngineOutput.OutputToLogMessage(false, line);
+
+        var logMessage = result.Should().NotBeNull().And.BeAssignableTo<LogMessage>().Which;
+        logMessage.Level.Should().Be(LogLevel.Error);
+        logMessage.Message.Should().Be(line);
+    }
+}
---
+++ src/SonarScanner.MSBuild.Shim/SonarEngineOutput.cs
@@ -0,0 +1,76 @@
+﻿/*
+ * SonarScanner for .NET
+ * Copyright (C) 2016-2025 SonarSource SA
+ * mailto: info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using Newtonsoft.Json;
+
+namespace SonarScanner.MSBuild.Shim;
+
+public class SonarEngineOutput
+{
+    private enum EngineLevel
+    {
+        TRACE,
+        DEBUG,
+        INFO,
+        WARN,
+        ERROR
+    }
+
+    public static LogMessage? OutputToLogMessage(bool stdOut, string outputLine)
+    {
+        if (stdOut)
+        {
+            try
+            {
+                var engineOutput = JsonConvert.DeserializeObject<EngineOutput>(outputLine);
+                var logLevel = engineOutput.Level switch
+                {
+                    EngineLevel.WARN => LogLevel.Warning,
+                    EngineLevel.ERROR => LogLevel.Error,
+                    _ => LogLevel.Info
+                };
+                var message = engineOutput.Message;
+                if (!string.IsNullOrWhiteSpace(engineOutput.Stacktrace))
+                {
+                    message += Environment.NewLine + engineOutput.Stacktrace;
+                }
+                return new(logLevel, message);
+            }
+            catch (JsonException)
+            {
+                return new(LogLevel.Info, outputLine);
+            }
+        }
+        return new(LogLevel.Error, outputLine);
+    }
+
+    // https://xtranet-sonarsource.atlassian.net/wiki/spaces/CodeOrches/pages/3155001372/Scanner+Bootstrapping#Scanner-Engine-contract
+    private sealed record EngineOutput
+    {
+        [JsonProperty(Required = Required.Always)]
+        public string Message { get; set; }
+
+        [JsonProperty(Required = Required.Always)]
+        public EngineLevel Level { get; set; }
+
+        [JsonProperty(Required = Required.Default)]
+        public string Stacktrace { get; set; }
+    }
+}
