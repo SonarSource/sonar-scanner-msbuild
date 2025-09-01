67ff0a2b3d11ce344c4a5b1a20eb38aaf69cecd4
Pavel Mikula <57188685+pavel-mikula-sonarsource@users.noreply.github.com>
2025-09-01 10:35:01 +0200

    SCAN4NET-852 Use IRuntime in SummaryReportBuilder (#2773)

--- Tests/SonarScanner.MSBuild.TFS.Test/ProgramTests.cs
+++ Tests/SonarScanner.MSBuild.TFS.Test/ProgramTests.cs
@@ -23,62 +23,66 @@ namespace SonarScanner.MSBuild.TFS.Classic.Tests;
 [TestClass]
 public class ProgramTests
 {
+    private readonly TestRuntime runtime = new TestRuntime();
+
     [TestMethod]
     public void Execute_WhenNoArgs_ShouldLogError()
     {
-        var logger = new TestLogger();
-        var result = Program.Execute(new string[] { }, logger);
+        var result = Program.Execute([], runtime);
 
         result.Should().Be(1);
-        logger.Warnings.Should().BeEmpty();
-        logger.Errors.Should().HaveCount(1);
-        logger.Errors.Should().Contain(new string[] { "No argument found. Exiting..." });
+        runtime.Logger.Warnings.Should().BeEmpty();
+        runtime.Logger.Errors.Should().BeEquivalentTo("No argument found. Exiting...");
+    }
+
+    [TestMethod]
+    public void Execute_ExceptionThrown_LogsError()
+    {
+        var loggerRuntime = new MockedLoggerRuntime();
+        loggerRuntime.Logger.When(x => x.LogError("No argument found. Exiting...")).Do(x => { throw new Exception("Mock Exception"); });
+        var result = Program.Execute([], loggerRuntime);
+
+        result.Should().Be(1);
+        loggerRuntime.Logger.Received().LogError("An exception occurred while executing the process: Mock Exception");
     }
 
     [TestMethod]
     public void Execute_Method_Is_Uknown_Should_Log_Error()
     {
-        var logger = new TestLogger();
-        var result = Program.Execute(new string[] { "MockMethod" }, logger);
+        var result = Program.Execute(["MockMethod"], runtime);
 
         result.Should().Be(1);
-        logger.Errors.Should().HaveCount(1);
-        logger.Errors.Should().Contain(new string[] { "Failed to parse or retrieve arguments for command line." });
+        runtime.Logger.Errors.Should().BeEquivalentTo("Failed to parse or retrieve arguments for command line.");
     }
 
     [TestMethod]
     public void Execute_ShouldExecute_CoverageConverter_ShouldSucceeed()
     {
-        var logger = new TestLogger();
         var tempDir = Directory.CreateDirectory(Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString())).FullName;
-        var text = @"<?xml version=""1.0"" encoding=""utf-8""?>
-                        <AnalysisConfig xmlns:xsd=""http://www.w3.org/2001/XMLSchema""
-                        xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns=""http://www.sonarsource.com/msbuild/integration/2015/1"">
-                        </AnalysisConfig>";
-
+        var text = """
+            <?xml version="1.0" encoding="utf-8"?>
+            <AnalysisConfig xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://www.sonarsource.com/msbuild/integration/2015/1">
+            </AnalysisConfig>
+            """;
         File.WriteAllText(Path.Combine(tempDir, "temp.xml"), text);
         File.WriteAllText(Path.Combine(tempDir, "sonar-project.properties"), string.Empty);
-
         using var scope = new EnvironmentVariableScope();
-
         // Faking TeamBuild
         scope.SetVariable(EnvironmentVariables.IsInTeamFoundationBuild, "true");
         scope.SetVariable(EnvironmentVariables.BuildUriTfs2015, "vsts://test/test");
         scope.SetVariable(EnvironmentVariables.BuildDirectoryTfs2015, tempDir);
         scope.SetVariable(EnvironmentVariables.SourcesDirectoryTfs2015, tempDir);
-
-        var result = Program.Execute(new string[] { "ConvertCoverage", Path.Combine(tempDir, "temp.xml"), Path.Combine(tempDir, "sonar-project.properties") }, logger);
+        var result = Program.Execute(["ConvertCoverage", Path.Combine(tempDir, "temp.xml"), Path.Combine(tempDir, "sonar-project.properties")], runtime);
 
         result.Should().Be(0);
-        logger.Errors.Should().HaveCount(0);
-        logger.InfoMessages.Should().NotContain("Coverage report conversion completed successfully.");
-        logger.InfoMessages.Should().NotContain("Coverage report conversion has failed. Skipping...");
+        runtime.Logger.Errors.Should().BeEmpty();
+        runtime.Logger.InfoMessages.Should().NotContain("Coverage report conversion completed successfully.");
+        runtime.Logger.InfoMessages.Should().NotContain("Coverage report conversion has failed. Skipping...");
     }
 
     [TestMethod]
     public void Execute_ShouldExecute_SummaryReportBuilder_ShouldSucceed()
     {
-        var logger = new TestLogger();
         var tempDir = Directory.CreateDirectory(Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString())).FullName;
         var text = $"""
             <?xml version="1.0" encoding="utf-8"?>
@@ -87,26 +91,29 @@ public class ProgramTests
                 <SonarQubeHostUrl>http://localhost/</SonarQubeHostUrl>
             </AnalysisConfig>
             """;
-
         File.WriteAllText(Path.Combine(tempDir, "temp.xml"), text);
         File.WriteAllText(Path.Combine(tempDir, "sonar-project.properties"), string.Empty);
-
         // Faking TeamBuild
         Environment.SetEnvironmentVariable("TF_Build", "true");
         Environment.SetEnvironmentVariable("BUILD_BUILDURI", "vsts://test/test");
         Environment.SetEnvironmentVariable("AGENT_BUILDDIRECTORY", tempDir);
         Environment.SetEnvironmentVariable("BUILD_SOURCESDIRECTORY", tempDir);
-
         using var scope = new EnvironmentVariableScope();
-
         // Faking TeamBuild
         scope.SetVariable(EnvironmentVariables.IsInTeamFoundationBuild, "true");
         scope.SetVariable(EnvironmentVariables.BuildUriTfs2015, "vsts://test/test");
         scope.SetVariable(EnvironmentVariables.BuildDirectoryTfs2015, tempDir);
         scope.SetVariable(EnvironmentVariables.SourcesDirectoryTfs2015, tempDir);
-
-        var result = Program.Execute(["SummaryReportBuilder", Path.Combine(tempDir, "temp.xml"), Path.Combine(tempDir, "sonar-project.properties"), "true"], logger);
+        var result = Program.Execute(["SummaryReportBuilder", Path.Combine(tempDir, "temp.xml"), Path.Combine(tempDir, "sonar-project.properties"), "true"], runtime);
 
         result.Should().Be(0);
+    }
+
+    private sealed class MockedLoggerRuntime : IRuntime
+    {
+        public ILogger Logger { get; } = Substitute.For<ILogger>();
+        public OperatingSystemProvider OperatingSystem => throw new NotImplementedException();
+        public IDirectoryWrapper Directory => throw new NotImplementedException();
+        public IFileWrapper File => throw new NotImplementedException();
     }
 }
