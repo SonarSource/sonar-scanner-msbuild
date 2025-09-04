50e3f037f9725a295b4adc08358df211909780d8
Tim Pohlmann <tim.pohlmann@sonarsource.com>
2025-09-04 12:00:29 +0200

    Removed AssertInfoLogs

--- Tests/SonarScanner.MSBuild.Shim.Test/SonarEngineWrapperTest.cs
+++ Tests/SonarScanner.MSBuild.Shim.Test/SonarEngineWrapperTest.cs
@@ -65,7 +65,7 @@ public class SonarEngineWrapperTest
             CmdLineArgs = (string[])["-jar", "engine.jar"],
             StandardInput = SampleInput,
         });
-        context.Runtime.Logger.AssertInfoLogs(
+        context.Runtime.Logger.AssertInfoLogged(
             "The scanner engine has finished successfully",
             $"Using Java found in Analysis Config: {context.ResolvedJavaExe}");
     }
@@ -82,7 +82,7 @@ public class SonarEngineWrapperTest
         context.Execute().Should().BeTrue();
 
         context.Runner.SuppliedArguments.ExeName.Should().Be(context.JavaFileName);
-        context.Runtime.Logger.AssertInfoLogs(
+        context.Runtime.Logger.AssertInfoLogged(
             $"Could not find Java in Analysis Config: {context.ResolvedJavaExe}",
             "'JAVA_HOME' environment variable not set",
             $"Could not find Java, falling back to using PATH: {context.JavaFileName}");
@@ -102,7 +102,7 @@ public class SonarEngineWrapperTest
         context.Execute().Should().BeTrue();
 
         context.Runner.SuppliedArguments.ExeName.Should().Be(context.JavaHomeExePath);
-        context.Runtime.Logger.AssertInfoLogs(
+        context.Runtime.Logger.AssertInfoLogged(
             $"Could not find Java in Analysis Config: {context.ResolvedJavaExe}",
             $"Found 'JAVA_HOME': {context.JavaHome}",
             $"Using Java found in JAVA_HOME: {context.JavaHomeExePath}");
@@ -120,7 +120,7 @@ public class SonarEngineWrapperTest
         context.Execute().Should().BeTrue();
 
         context.Runner.SuppliedArguments.ExeName.Should().Be(context.JavaFileName);
-        context.Runtime.Logger.AssertInfoLogs(
+        context.Runtime.Logger.AssertInfoLogged(
             $"Could not find Java in Analysis Config: {context.ResolvedJavaExe}",
             $"Found 'JAVA_HOME': {context.JavaHome}",
             $"Could not find Java in JAVA_HOME: {context.JavaHomeExePath}",
