49a1a2e656dafb84e159d1ad28da49e5268a2be7
Alex Meseldzija <alexander.meseldzija@sonarsource.com>
2025-09-05 15:37:54 +0200

    not basedir

--- its/src/test/java/com/sonar/it/scanner/msbuild/sonarqube/BaseDirTest.java
+++ its/src/test/java/com/sonar/it/scanner/msbuild/sonarqube/BaseDirTest.java
@@ -74,6 +74,7 @@ class BaseDirTest {
   @EnabledOnOs(OS.WINDOWS)
   void whenMajorityOfProjectsIsOnSameDrive_AnalysisSucceeds() {
     var context = createContextWithoutProjectBasedDir("TwoDrivesThreeProjects");
+    context.begin.setProperty("sonar.scanner.useSonarScannerCLI", "true"); // TODO: remove this in SCAN4NET-861
     try {
       TestUtils.createVirtualDrive("Y:", context.projectDir, "DriveY");
       var logs = context.runAnalysis().end().getLogs();
@@ -148,7 +149,8 @@ class BaseDirTest {
     // projectBaseDir = "..\.." is relative to the projectDir. That is "C:\Windows\Temp\", so component keys should start with tempDirectoryName.
     var tempDirectoryName = context.projectDir.getParent().getFileName().toString();
     context.begin
-      .setProperty("sonar.projectBaseDir", Paths.get("..", "..").toString());  // Relative from scanner working directory
+      .setProperty("sonar.projectBaseDir", Paths.get("..", "..").toString())  // Relative from scanner working directory
+      .setProperty("sonar.scanner.useSonarScannerCLI", "true");   // TODO: remove in SCAN4NET-861
     context.runAnalysis();
 
     assertThat(TestUtils.listComponents(ORCHESTRATOR, context.projectKey))
