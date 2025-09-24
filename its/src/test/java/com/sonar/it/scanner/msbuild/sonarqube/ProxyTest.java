3c9f397a2ee4956af9308a8eaf5a6974593da9ac
Martin Strecker <martin.strecker@sonarsource.com>
2025-09-24 17:57:26 +0200

    Fix assertion in proxyAuth

--- its/src/test/java/com/sonar/it/scanner/msbuild/sonarqube/ProxyTest.java
+++ its/src/test/java/com/sonar/it/scanner/msbuild/sonarqube/ProxyTest.java
@@ -23,7 +23,6 @@ import com.sonar.it.scanner.msbuild.utils.AnalysisContext;
 import com.sonar.it.scanner.msbuild.utils.ContextExtension;
 import com.sonar.it.scanner.msbuild.utils.ProxyAuthenticator;
 import com.sonar.it.scanner.msbuild.utils.QualityProfile;
-import com.sonar.it.scanner.msbuild.utils.SonarAssertions;
 import com.sonar.it.scanner.msbuild.utils.TestUtils;
 import com.sonar.orchestrator.util.NetworkUtils;
 import jakarta.servlet.ServletException;
@@ -58,7 +57,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.sonarqube.ws.Issues.Issue;
 
 import static com.sonar.it.scanner.msbuild.sonarqube.ServerTests.ORCHESTRATOR;
-import static org.assertj.core.api.Assertions.assertThat;
+import static com.sonar.it.scanner.msbuild.utils.SonarAssertions.assertThat;
 
 @ExtendWith({ServerTests.class, ContextExtension.class})
 class ProxyTest {
@@ -107,7 +106,7 @@ class ProxyTest {
     assertThat(TestUtils.getMeasureAsInteger(fileKey, "ncloc", ORCHESTRATOR)).isEqualTo(25);
     assertThat(TestUtils.getMeasureAsInteger(fileKey, "lines", ORCHESTRATOR)).isEqualTo(52);
     assertThat(seenByProxy).isNotEmpty();
-    SonarAssertions.assertThat(TestUtils.scannerEngineInputJson(context)).hasAllSecretsRedacted();
+    assertThat(TestUtils.scannerEngineInputJson(context)).hasAllSecretsRedacted();
   }
 
   private static void startProxy(boolean needProxyAuth) throws Exception {
