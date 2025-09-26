f216ee2b63fd7dc450059d6dd5e0d430a43a3004
Pavel Mikula <57188685+pavel-mikula-sonarsource@users.noreply.github.com>
2025-09-26 14:30:46 +0200

    SCAN4NET-923 License Packaging Standard (#2849)

--- scripts/package-artifacts.ps1
+++ scripts/package-artifacts.ps1
@@ -3,35 +3,42 @@
         [Bool]$SignAssemblies = $false
     )
 
-    $destination = "$fullBuildOutputDir\sonarscanner-net-framework"
-    $destinationTargets = "$destination\Targets"
-    $sourceRoot = "$PSScriptRoot\..\src\SonarScanner.MSBuild.TFS.Classic\bin\Release\net462"
+    $Destination = "$FullBuildOutputDir\sonarscanner-net-framework"
+    $DestinationTargets = "$Destination\Targets"
+    $DestinationLicenses = "$Destination\licenses"
+    $DestinationThirdPartyLicenses = "$DestinationLicenses\THIRD_PARTY_LICENSES"
 
-    if (!(Test-Path -Path $destination)) { New-Item $destination -Type Directory }
-    if (!(Test-Path -Path $destinationTargets)) { New-Item $destinationTargets -Type Directory }
+    $SourceRoot = "$PSScriptRoot\..\src\SonarScanner.MSBuild.TFS.Classic\bin\Release\net462"
 
-    Copy-Item -Path "$sourceRoot\Microsoft.CodeCoverage.IO.dll" -Destination $destination
-    Copy-Item -Path "$sourceRoot\Newtonsoft.Json.dll" -Destination $destination
-    Copy-Item -Path "$sourceRoot\SonarScanner.MSBuild.Common.dll" -Destination $destination
-    Copy-Item -Path "$sourceRoot\SonarScanner.MSBuild.Shim.dll" -Destination $destination
-    Copy-Item -Path "$sourceRoot\SonarScanner.MSBuild.TFS.dll" -Destination $destination
-    Copy-Item -Path "$sourceRoot\SonarScanner.MSBuild.TFSProcessor.exe" -Destination $destination
-    Copy-Item -Path "$sourceRoot\SonarScanner.MSBuild.TFSProcessor.exe.config" -Destination $destination
-    Copy-Item -Path "$sourceRoot\System.Runtime.InteropServices.RuntimeInformation.dll" -Destination $destination
-    Copy-Item -Path "$sourceRoot\System.ValueTuple.dll" -Destination $destination
-    Copy-Item -Path "$PSScriptRoot\..\src\SonarScanner.MSBuild.Tasks\Targets\*" -Destination $destinationTargets -Recurse
-    Copy-Item -Path "$PSScriptRoot\..\src\SonarScanner.MSBuild\bin\Release\net462\*" -Exclude "*.pdb" -Destination $destination -Recurse
-    Copy-Item -Path "$PSScriptRoot\..\src\SonarScanner.MSBuild.Tasks\bin\Release\net462\SonarScanner.MSBuild.Tasks.dll" -Destination $destination
+    if (!(Test-Path -Path $Destination)) { New-Item $Destination -Type Directory }
+    if (!(Test-Path -Path $DestinationTargets)) { New-Item $DestinationTargets -Type Directory }
+    if (!(Test-Path -Path $DestinationLicenses)) { New-Item $DestinationLicenses -Type Directory }
+    if (!(Test-Path -Path $DestinationThirdPartyLicenses)) { New-Item $DestinationThirdPartyLicenses -Type Directory }
 
-    Expand-Archive -Path "$scannerCliDownloadDir\$scannerCliArtifact" -DestinationPath $destination -Force
+    Copy-Item -Path "$SourceRoot\Microsoft.CodeCoverage.IO.dll" -Destination $Destination
+    Copy-Item -Path "$SourceRoot\Newtonsoft.Json.dll" -Destination $Destination
+    Copy-Item -Path "$SourceRoot\SonarScanner.MSBuild.Common.dll" -Destination $Destination
+    Copy-Item -Path "$SourceRoot\SonarScanner.MSBuild.Shim.dll" -Destination $Destination
+    Copy-Item -Path "$SourceRoot\SonarScanner.MSBuild.TFS.dll" -Destination $Destination
+    Copy-Item -Path "$SourceRoot\SonarScanner.MSBuild.TFSProcessor.exe" -Destination $Destination
+    Copy-Item -Path "$SourceRoot\SonarScanner.MSBuild.TFSProcessor.exe.config" -Destination $Destination
+    Copy-Item -Path "$SourceRoot\System.Runtime.InteropServices.RuntimeInformation.dll" -Destination $Destination
+    Copy-Item -Path "$SourceRoot\System.ValueTuple.dll" -Destination $Destination
+    Copy-Item -Path "$PSScriptRoot\..\src\SonarScanner.MSBuild.Tasks\Targets\*" -Destination $DestinationTargets -Recurse
+    Copy-Item -Path "$PSScriptRoot\..\src\SonarScanner.MSBuild\bin\Release\net462\*" -Exclude "*.pdb" -Destination $Destination -Recurse
+    Copy-Item -Path "$PSScriptRoot\..\src\SonarScanner.MSBuild.Tasks\bin\Release\net462\SonarScanner.MSBuild.Tasks.dll" -Destination $Destination
+    Copy-Item -Path "$PSScriptRoot\..\LICENSE.txt" -Destination $DestinationLicenses
+    Copy-Item -Path "$PSScriptRoot\..\Licenses\THIRD_PARTY_LICENSES\*" -Destination $DestinationThirdPartyLicenses
+
+    Expand-Archive -Path "$scannerCliDownloadDir\$scannerCliArtifact" -DestinationPath $Destination -Force
 
     if ($SignAssemblies) {
-        Sign-Assemblies -Pattern "$destination\Sonar*" -TargetName ".NET Framework assemblies"
+        Sign-Assemblies -Pattern "$Destination\Sonar*" -TargetName ".NET Framework assemblies"
     }
 
     # Don't use Compress-Archive because https://github.com/SonarSource/sonar-scanner-msbuild/issues/2086
     # This is propably fixed in Powershell 7
-    tar -c -a -C "$destination" --options "zip:compression-level=9" -f "$destination.zip" *
+    tar -c -a -C "$Destination" --options "zip:compression-level=9" -f "$Destination.zip" *
 }
 
 function Package-NetScanner {
@@ -39,36 +46,44 @@ function Package-NetScanner {
         [Bool]$SignAssemblies = $false
     )
 
-    $sourceRoot = "$PSScriptRoot\..\src\SonarScanner.MSBuild\bin\Release\netcoreapp3.1"
-    $destination = "$fullBuildOutputDir\sonarscanner-net"
-    $destinationTargets = "$destination\Targets"
+    $SourceRoot = "$PSScriptRoot\..\src\SonarScanner.MSBuild\bin\Release\netcoreapp3.1"
+    $Destination = "$FullBuildOutputDir\sonarscanner-net"
+    $DestinationTargets = "$Destination\Targets"
+    $DestinationLicenses = "$Destination\licenses"
+    $DestinationThirdPartyLicenses = "$DestinationLicenses\THIRD_PARTY_LICENSES"
 
-    if (!(Test-Path -Path $destination)) { New-Item $destination -Type Directory }
-    if (!(Test-Path -Path $destinationTargets)) { New-Item $destinationTargets -Type Directory }
+    if (!(Test-Path -Path $Destination)) { New-Item $Destination -Type Directory }
+    if (!(Test-Path -Path $DestinationTargets)) { New-Item $DestinationTargets -Type Directory }
+    if (!(Test-Path -Path $DestinationLicenses)) { New-Item $DestinationLicenses -Type Directory }
+    if (!(Test-Path -Path $DestinationThirdPartyLicenses)) { New-Item $DestinationThirdPartyLicenses -Type Directory }
 
-    Copy-Item -Path "$PSScriptRoot\..\src\SonarScanner.MSBuild.Tasks\Targets\*" -Destination $destinationTargets -Recurse
-    Copy-Item -Path "$sourceRoot\SonarQube.Analysis.xml" -Destination $destination
-    Copy-Item -Path "$sourceRoot\SonarScanner.MSBuild.Common.dll" -Destination $destination
-    Copy-Item -Path "$sourceRoot\SonarScanner.MSBuild.dll" -Destination $destination
-    Copy-Item -Path "$sourceRoot\SonarScanner.MSBuild.PostProcessor.dll" -Destination $destination
-    Copy-Item -Path "$sourceRoot\SonarScanner.MSBuild.PreProcessor.dll" -Destination $destination
-    Copy-Item -Path "$sourceRoot\SonarScanner.MSBuild.runtimeconfig.json" -Destination $destination
-    Copy-Item -Path "$sourceRoot\SonarScanner.MSBuild.Shim.dll" -Destination $destination
-    Copy-Item -Path "$sourceRoot\SonarScanner.MSBuild.TFS.dll" -Destination $destination
-    Copy-Item -Path "$sourceRoot\Google.Protobuf.dll" -Destination $destination
-    Copy-Item -Path "$sourceRoot\Newtonsoft.Json.dll" -Destination $destination
-    Copy-Item -Path "$sourceRoot\ICSharpCode.SharpZipLib.dll" -Destination $destination
-    Copy-Item -Path "$PSScriptRoot\..\src\SonarScanner.MSBuild.Tasks\bin\Release\netstandard2.0\SonarScanner.MSBuild.Tasks.dll" -Destination $destination
+    Copy-Item -Path "$PSScriptRoot\..\src\SonarScanner.MSBuild.Tasks\Targets\*" -Destination $DestinationTargets -Recurse
+    Copy-Item -Path "$SourceRoot\SonarQube.Analysis.xml" -Destination $Destination
+    Copy-Item -Path "$SourceRoot\SonarScanner.MSBuild.Common.dll" -Destination $Destination
+    Copy-Item -Path "$SourceRoot\SonarScanner.MSBuild.dll" -Destination $Destination
+    Copy-Item -Path "$SourceRoot\SonarScanner.MSBuild.PostProcessor.dll" -Destination $Destination
+    Copy-Item -Path "$SourceRoot\SonarScanner.MSBuild.PreProcessor.dll" -Destination $Destination
+    Copy-Item -Path "$SourceRoot\SonarScanner.MSBuild.runtimeconfig.json" -Destination $Destination
+    Copy-Item -Path "$SourceRoot\SonarScanner.MSBuild.Shim.dll" -Destination $Destination
+    Copy-Item -Path "$SourceRoot\SonarScanner.MSBuild.TFS.dll" -Destination $Destination
+    Copy-Item -Path "$SourceRoot\Google.Protobuf.dll" -Destination $Destination
+    Copy-Item -Path "$SourceRoot\Newtonsoft.Json.dll" -Destination $Destination
+    Copy-Item -Path "$SourceRoot\ICSharpCode.SharpZipLib.dll" -Destination $Destination
+    Copy-Item -Path "$PSScriptRoot\..\src\SonarScanner.MSBuild.Tasks\bin\Release\netstandard2.0\SonarScanner.MSBuild.Tasks.dll" -Destination $Destination
+    Copy-Item -Path "$PSScriptRoot\..\LICENSE.txt" -Destination $DestinationLicenses
+    Copy-Item -Path "$PSScriptRoot\..\Licenses\THIRD_PARTY_LICENSES\Google.Protobuf-LICENSE.txt" -Destination $DestinationThirdPartyLicenses
+    Copy-Item -Path "$PSScriptRoot\..\Licenses\THIRD_PARTY_LICENSES\Newtonsoft.Json-LICENSE.txt" -Destination $DestinationThirdPartyLicenses
+    Copy-Item -Path "$PSScriptRoot\..\Licenses\THIRD_PARTY_LICENSES\SharpZipLib-LICENSE.txt" -Destination $DestinationThirdPartyLicenses
 
-    Expand-Archive -Path "$scannerCliDownloadDir\$scannerCliArtifact" -DestinationPath $destination -Force
+    Expand-Archive -Path "$scannerCliDownloadDir\$scannerCliArtifact" -DestinationPath $Destination -Force
 
     if ($SignAssemblies) {
-        Sign-Assemblies -Pattern "$destination\Sonar*" -TargetName ".NET assemblies"
+        Sign-Assemblies -Pattern "$Destination\Sonar*" -TargetName ".NET assemblies"
     }
-    
+
     # Don't use Compress-Archive because https://github.com/SonarSource/sonar-scanner-msbuild/issues/2086
     # This is propably fixed in Powershell 7
-    tar -c -a -C "$destination" --options "zip:compression-level=9" -f "$destination.zip" *
+    tar -c -a -C "$Destination" --options "zip:compression-level=9" -f "$Destination.zip" *
 }
 
 function Sign-Assemblies {
@@ -85,19 +100,19 @@ function Sign-Assemblies {
 }
 
 function Download-ScannerCli {
-    $artifactoryUrlEnv = "ARTIFACTORY_URL"
+    $ArtifactoryUrlEnv = "ARTIFACTORY_URL"
 
-    $artifactoryUrl = [environment]::GetEnvironmentVariable($artifactoryUrlEnv, "Process")
-    if (!$artifactoryUrl) {
+    $ArtifactoryUrl = [environment]::GetEnvironmentVariable($ArtifactoryUrlEnv, "Process")
+    if (!$ArtifactoryUrl) {
         Write-Host "Could not find ARTIFACTORY_URL variable, defaulting to repox URL.";
-        $artifactoryUrl = "https://repox.jfrog.io/repox";
+        $ArtifactoryUrl = "https://repox.jfrog.io/repox";
     }
 
     if (!(Test-Path -LiteralPath $scannerCliDownloadDir)) {
         New-Item -Path $scannerCliDownloadDir -ItemType Directory -ErrorAction Stop -Force
     }
 
-    mvn org.apache.maven.plugins:maven-dependency-plugin:3.2.0:get -DremoteRepositories=$artifactoryUrl -Dartifact="org.sonarsource.scanner.cli:sonar-scanner-cli:${scannerCliVersion}:zip" -Dtransitive=false
+    mvn org.apache.maven.plugins:maven-dependency-plugin:3.2.0:get -DremoteRepositories=$ArtifactoryUrl -Dartifact="org.sonarsource.scanner.cli:sonar-scanner-cli:${scannerCliVersion}:zip" -Dtransitive=false
 
     mvn org.apache.maven.plugins:maven-dependency-plugin:3.2.0:copy -Dartifact="org.sonarsource.scanner.cli:sonar-scanner-cli:${scannerCliVersion}:zip" -DoutputDirectory="${scannerCliDownloadDir}"
 }
