2b8bda9e450e0dc3533b7c49526029a088011996
Pavel Mikula <pavel.mikula@sonarsource.com>
2025-10-02 19:40:42 +0200

    Switch to AltCover

--- build.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-setlocal ENABLEDELAYEDEXPANSION
-
-echo "Build has been moved to Azure pipeline, link is here : https://dev.azure.com/sonarsource/DotNetTeam%20Project/_build?definitionId=64&_a=summary"
-exit !errorlevel!
