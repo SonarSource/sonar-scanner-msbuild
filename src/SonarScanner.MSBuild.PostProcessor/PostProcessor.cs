5a3ef30cfb06fcdc740c17d8839afdf8c36ba925
Pavel Mikula <pavel.mikula@sonarsource.com>
2025-10-10 08:16:33 +0200

    QG cleanup

--- src/SonarScanner.MSBuild.PostProcessor/PostProcessor.cs
+++ src/SonarScanner.MSBuild.PostProcessor/PostProcessor.cs
@@ -18,7 +18,6 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-using System.Runtime;
 using SonarScanner.MSBuild.Common.Interfaces;
 using SonarScanner.MSBuild.Common.TFS;
 using SonarScanner.MSBuild.Shim;
