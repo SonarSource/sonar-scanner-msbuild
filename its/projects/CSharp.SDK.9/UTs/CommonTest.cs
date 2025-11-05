89d1596274b54a11179e8772ebf0300aa312717b
Alex Meseldzija <alexander.meseldzija@sonarsource.com>
2025-11-05 14:31:09 +0100

    Add CSharp.SDK.9 IT

---
+++ its/projects/CSharp.SDK.9/UTs/CommonTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSharp.SDKs.Test
+{
+    [TestClass]
+    public class CommonTest
+    {
+        // FIXME: This line contains S1134 warning in SL, but not in S4NET context due to current test-code support
+
+        [TestMethod]
+        public void TestMethodWithNoAssertion() // S2699: Add at least one assertion to this test case. This rule has test-only scope.
+        {
+            Common.IsGreaterThanZero(1);
+        }
+    }
+}
