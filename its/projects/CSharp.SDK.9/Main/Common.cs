4acc954ea6adcd035df1e3c3a6b36bb38f31875f
Alex Meseldzija <alexander.meseldzija@sonarsource.com>
2025-11-11 11:29:16 +0100

    SCAN4NET-1003 Add CSharp.SDK.9 IT (#2922)

---
+++ its/projects/CSharp.SDK.9/Main/Common.cs
@@ -0,0 +1,8 @@
+﻿namespace CSharp.SDKs
+{
+    public static class Common
+    {
+        // FIXME: This line contains S1134 warning
+        public static bool IsGreaterThanZero(int value) => value > 0;
+    }
+}
