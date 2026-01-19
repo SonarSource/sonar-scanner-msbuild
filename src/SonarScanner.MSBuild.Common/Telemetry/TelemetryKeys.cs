73dabe211315b73c71ada5993a904f4d75faecb7
Alex Meseldzija <alexander.meseldzija@sonarsource.com>
2026-01-19 15:56:18 +0100

    Add Telemetry Key/Value

--- src/SonarScanner.MSBuild.Common/Telemetry/TelemetryKeys.cs
+++ src/SonarScanner.MSBuild.Common/Telemetry/TelemetryKeys.cs
@@ -37,4 +37,5 @@ public static class TelemetryKeys
     public const string ServerInfoVersion           = "dotnetenterprise.s4net.serverInfo.version";
     // EndStep
     public const string EndstepLegacyTFS            = "dotnetenterprise.s4net.endstep.legacyTFS";
+    public const string EndstepIsRoslynV1Report       = "dotnetenterprise.s4net.endstep.isRoslynV1Report";
 }
--- src/SonarScanner.MSBuild.Common/Telemetry/TelemetryValues.cs
+++ src/SonarScanner.MSBuild.Common/Telemetry/TelemetryValues.cs
@@ -67,6 +67,12 @@ public static class TelemetryValues
         public static readonly string NotCalled = nameof(NotCalled);
     }
 
+    public static class EndstepIsRoslynV1Report
+    {
+        public static readonly string True = nameof(True);
+        public static readonly string False = nameof(False);
+    }
+
     public static class Product
     {
         public static readonly string Server = "SQ_Server";
