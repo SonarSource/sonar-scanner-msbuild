367b5c0d6db1a42cabe31d53526c65bc71ac6392
Alex Meseldzija <alexander.meseldzija@sonarsource.com>
2026-01-22 11:34:16 +0100

    Add Telemetry Project

---
+++ its/projects/Telemetry/Telemetry/SQMSBuildPropTelemetry/Telemetry.cs
@@ -0,0 +1,15 @@
+﻿namespace SQMSBuildPropTelemetry
+{
+    public class SQMSBuildPropTelemetry
+    {
+        public void Method()
+        {
+            // empty body
+        }
+
+        private class InnerClass
+        {
+            public string Name { get; set; } = "InnerClass";
+        }
+    }
+}
