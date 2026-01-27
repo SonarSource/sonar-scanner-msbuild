814c156c2d3ed737ff5c3a3d7bcd8e89de326ae3
Alex Meseldzija <alexander.meseldzija@sonarsource.com>
2026-01-27 16:19:59 +0100

    SCAN4NET-463 Telemetry for csproj exclusion via SonarQubeExclude (#2981)

--- its/src/test/java/com/sonar/it/scanner/msbuild/sonarqube/TelemetryTest.java
+++ its/src/test/java/com/sonar/it/scanner/msbuild/sonarqube/TelemetryTest.java
@@ -65,6 +65,8 @@ class TelemetryTest {
       x -> assertThat(x).isEqualTo("dotnetenterprise.s4net.build.override_warnings_as_errors.true=3"),
       x -> assertThat(x).isEqualTo("dotnetenterprise.s4net.build.nuget_project_style.packagereference=3"),
       x -> assertThat(x).isEqualTo("dotnetenterprise.s4net.build.using_microsoft_net_sdk.true=3"),
+      x -> assertThat(x).isEqualTo("dotnetenterprise.s4net.build.exclusion_proj.true=1"),
+      x -> assertThat(x).isEqualTo("dotnetenterprise.s4net.build.exclusion_proj.false=2"),
       x -> assertThat(x).isEqualTo("dotnetenterprise.s4net.build.deterministic.true=3"));
   }
 
