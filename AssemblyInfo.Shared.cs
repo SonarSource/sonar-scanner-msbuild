e51e60108c6f53165867dcc4349af1add21de11b
Martin Strecker <103252490+martin-strecker-sonarsource@users.noreply.github.com>
2026-02-03 09:46:37 +0100

    SCAN4NET-1082 Bump version to 11.2.0 (#3012)

--- AssemblyInfo.Shared.cs
+++ AssemblyInfo.Shared.cs
@@ -22,9 +22,9 @@ using System.Reflection;
 using System.Resources;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("11.1.0")]
-[assembly: AssemblyFileVersion("11.1.0.0")]
-[assembly: AssemblyInformationalVersion("Version:11.1.0.0 Branch:not-set Sha1:not-set")]
+[assembly: AssemblyVersion("11.2.0")]
+[assembly: AssemblyFileVersion("11.2.0.0")]
+[assembly: AssemblyInformationalVersion("Version:11.2.0.0 Branch:not-set Sha1:not-set")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("SonarSource")]
 [assembly: AssemblyCopyright("Copyright © SonarSource 2015-2025")]
