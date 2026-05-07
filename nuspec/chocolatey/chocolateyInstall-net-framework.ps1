d0e00a26ca5d75c826890d8133fd75da97969f0c
Pavel Mikula <pavel.mikula@sonarsource.com>
2026-05-07 16:44:58 +0200

    Update files

--- nuspec/chocolatey/chocolateyInstall-net-framework.ps1
+++
@@ -1,5 +0,0 @@
-﻿Install-ChocolateyZipPackage "sonarscanner-net-framework" `
-    -Url "https://github.com/SonarSource/sonar-scanner-msbuild/releases/download/__PackageVersion__/sonar-scanner-__PackageVersion__-net-framework.zip" `
-    -UnzipLocation "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" `
-    -ChecksumType 'sha256' `
-    -Checksum "not-set"
