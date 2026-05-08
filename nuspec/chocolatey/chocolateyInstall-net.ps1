a9cf1c20c124a993b7072853ae0c7caec834024b
Pavel Mikula <57188685+pavel-mikula-sonarsource@users.noreply.github.com>
2026-05-08 18:11:51 +0200

    SCAN4NET-1539 Move Chocolatey build to Packaging (#3144)

--- nuspec/chocolatey/chocolateyInstall-net.ps1
+++
@@ -1,5 +0,0 @@
-﻿Install-ChocolateyZipPackage "sonarscanner-net" `
-    -Url "https://github.com/SonarSource/sonar-scanner-msbuild/releases/download/__PackageVersion__/sonar-scanner-__PackageVersion__-net.zip" `
-    -UnzipLocation "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" `
-    -ChecksumType 'sha256' `
-    -Checksum "not-set"
---
+++ scripts/create-choco-packages.ps1
@@ -0,0 +1,20 @@
+﻿# This script generates the chocolatey packages for the .NET Scanner and the .NET Framework Scanner.
+
+function Create-Choco-Package([string] $runtime) {
+    $Packaging = "$PSScriptRoot\..\Packaging"
+    $Zip = "$PSScriptRoot\..\build\sonar-scanner-$env:FULL_VERSION-$runtime.zip"
+    Write-Host "Generating the '$runtime' chocolatey package for $Zip"
+    $Hash = (Get-FileHash $Zip -Algorithm SHA256).Hash
+    $Content = "Install-ChocolateyZipPackage ""sonarscanner-$runtime"" ``
+        -Url ""https://github.com/SonarSource/sonar-scanner-msbuild/releases/download/$env:FULL_VERSION/sonar-scanner-$env:FULL_VERSION-$runtime.zip"" ``
+        -UnzipLocation ""`$(Split-Path -parent `$MyInvocation.MyCommand.Definition)"" ``
+        -ChecksumType ""sha256"" ``
+        -Checksum ""$Hash""
+    "
+    New-Item -ItemType Directory -Path "$Packaging\Binaries\Chocolatey\" -Force | Out-Null
+    Set-Content -Path "$Packaging\Binaries\Chocolatey\chocolateyInstall-$runtime.ps1" -Value $Content
+    choco pack "$Packaging\Chocolatey\sonarscanner-$runtime.nuspec" --outputdirectory "$Packaging\Binaries\Chocolatey" --version $env:PATCH_VERSION
+}
+
+Create-Choco-Package "net-framework"
+Create-Choco-Package "net"
