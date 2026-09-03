01b2a7796cbd66cd304efe4f5ec9583929e825b8
Pavel Mikula <pavel.mikula@sonarsource.com>
2026-09-03 11:17:07 +0200

    SCAN4NET-1730 Remove Analysis-cache availability check

--- src/SonarScanner.MSBuild.PreProcessor/Resources.Designer.cs
+++ src/SonarScanner.MSBuild.PreProcessor/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace SonarScanner.MSBuild.PreProcessor {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "18.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -790,15 +790,6 @@ namespace SonarScanner.MSBuild.PreProcessor {
         internal static string MSG_GeneratingRulesets {
             get {
                 return ResourceManager.GetString("MSG_GeneratingRulesets", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Incremental PR analysis is available starting with SonarQube 9.9 or later..
-        /// </summary>
-        internal static string MSG_IncrementalPRAnalysisUpdateSonarQube {
-            get {
-                return ResourceManager.GetString("MSG_IncrementalPRAnalysisUpdateSonarQube", resourceCulture);
             }
         }
         
