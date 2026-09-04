7ded59ec293b1c33c2291157e4df539fc8e4890c
Tim Pohlmann <tim.pohlmann@sonarsource.com>
2026-09-04 08:17:21 +0200

    SCAN4NET-1742 Fix WebClientDownloaderBuilderTest (Cloudflare status page renamed) (#3354)

--- Tests/SonarScanner.MSBuild.PreProcessor.Test/WebClientDownloaderBuilderTest.cs
+++ Tests/SonarScanner.MSBuild.PreProcessor.Test/WebClientDownloaderBuilderTest.cs
@@ -388,7 +388,7 @@ public partial class WebClientDownloaderBuilderTest
         var result = await client.Download(new("https://www.cloudflarestatus.com/api/v2/status.json"));
 
         // Assert
-        var expected = new { Page = new { Name = "Cloudflare" } };
+        var expected = new { Page = new { Name = "Cloudflare Status" } };
         var converted = JsonConvert.DeserializeAnonymousType(result, expected);
         converted.Should().BeEquivalentTo(expected);
         callbackWasCalled.Should().BeTrue();
